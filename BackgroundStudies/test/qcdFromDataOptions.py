a4df5c6fc4df168937efdeca3d6eb5f5a0a28bbe
bing <bingxuan.liu@cern.ch>
2013-10-01 09:23:48 -0400

    Added impurities.

--- BackgroundStudies/test/qcdFromDataOptions.py
+++ BackgroundStudies/test/qcdFromDataOptions.py
@@ -3,17 +3,18 @@
 datasets = [
     'MuEG_22Jan2013'
 ]
-
+impurities = [
+]
 qcd_from_data = {
-    'scale_factor' : 0.0001,
-    'scale_factor_error' : (0.0001 * 5),
+    'scale_factor' : 0.02209,
+    'scale_factor_error' : (0),
 
     # This is a map from channels in the output from data to the channels you
     # want in QCDFromData.root; e.g., below, the histograms in the
     # "AntiIsolated" channel will be used to create an "Isolated" and
     # "AntiIsolated" channel in QCDFromData.root.
     'channel_map' : {
-        'Preselection_AntiIso' : ['Preselection','Blinded_Preselection'],
+        'Anti_Iso_Blinded_Preselection' : ['Blinded_Preselection'],
 #        'AntiIsolated' : ['Isolated', 'AntiIsolated'],
 #        'AntiIsolated_Prompt' : ['Isolated_Prompt', 'AntiIsolated_Prompt'],
 #        'AntiIsolated_NonPrompt' : ['Isolated_NonPrompt', 'AntiIsolated_NonPrompt'],
