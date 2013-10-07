12e23a69cdd69615e799ab36cc01ae59b1d2915a
Jamie Antonelli <lantonel@cern.ch>
2013-10-07 10:00:25 -0400

    removed d0 requirement on lepton of interest

--- StandardAnalysis/python/Preselection_D0_Sidebands.py
+++ StandardAnalysis/python/Preselection_D0_Sidebands.py
@@ -3,25 +3,15 @@ import copy
 
 #################################################################
 
-prompt_muon_d0_cut = cms.PSet (
-    inputCollection = cms.string("muons"),
-    cutString = cms.string("abs(correctedD0) < 0.02"),
-    numberRequired = cms.string("== 1")
-)
-displaced_muon_d0_cut = cms.PSet (
-    inputCollection = cms.string("muons"),
-    cutString = cms.string("abs(correctedD0) > 0.02"),
-    numberRequired = cms.string("== 1")
-)
-
 prompt_electron_d0_cut = cms.PSet (
     inputCollection = cms.string("electrons"),
     cutString = cms.string("abs(correctedD0) < 0.02"),
     numberRequired = cms.string("== 1")
 )
-displaced_electron_d0_cut = cms.PSet (
-    inputCollection = cms.string("electrons"),
-    cutString = cms.string("abs(correctedD0) > 0.02"),
+
+prompt_muon_d0_cut = cms.PSet (
+    inputCollection = cms.string("muons"),
+    cutString = cms.string("abs(correctedD0) < 0.02"),
     numberRequired = cms.string("== 1")
 )
 
@@ -31,24 +21,22 @@ from DisplacedSUSY.StandardAnalysis.Preselection import *
 
 #################################################################
 
-Preselection_Displaced_Electron = cms.PSet(
-    name = cms.string("Preselection_Displaced_Electron"),
+Preselection_Prompt_Electron = cms.PSet(
+    name = cms.string("Preselection_Prompt_Electron"),
     triggers = copy.deepcopy(Preselection.triggers),
     cuts = cms.VPSet ()
 )
-Preselection_Displaced_Electron.cuts.extend(copy.deepcopy(Preselection.cuts))
-Preselection_Displaced_Electron.cuts.append(prompt_muon_d0_cut)
-Preselection_Displaced_Electron.cuts.append(displaced_electron_d0_cut)
+Preselection_Prompt_Electron.cuts.extend(copy.deepcopy(Preselection.cuts))
+Preselection_Prompt_Electron.cuts.append(prompt_electron_d0_cut)
 
 #################################################################
 
-Preselection_Displaced_Muon = cms.PSet(
-    name = cms.string("Preselection_Displaced_Muon"),
+Preselection_Prompt_Muon = cms.PSet(
+    name = cms.string("Preselection_Prompt_Muon"),
     triggers = copy.deepcopy(Preselection.triggers),
     cuts = cms.VPSet ()
 )
-Preselection_Displaced_Muon.cuts.extend(copy.deepcopy(Preselection.cuts))
-Preselection_Displaced_Muon.cuts.append(displaced_muon_d0_cut)
-Preselection_Displaced_Muon.cuts.append(prompt_electron_d0_cut)
+Preselection_Prompt_Muon.cuts.extend(copy.deepcopy(Preselection.cuts))
+Preselection_Prompt_Muon.cuts.append(prompt_muon_d0_cut)
 
 #################################################################
