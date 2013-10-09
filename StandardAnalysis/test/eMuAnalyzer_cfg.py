2ba77f74605988ea9873c4574fc8e7db2c2274f2
Andrew Hart <ahart@cern.ch>
2013-10-09 11:43:06 -0400

    Modified the electron scale factor parameters to apply those for the new working point.

--- StandardAnalysis/test/eMuAnalyzer_cfg.py
+++ StandardAnalysis/test/eMuAnalyzer_cfg.py
@@ -7,16 +7,19 @@ from OSUT3Analysis.Configuration.processingUtilities import *
 
 #dir = "/mnt/hadoop/mc/stop200ToBottom_100mm_8TeV-pythia6_Summer12-START52_V9-v2_ahart-stop200ToBottom_100mm_TuneZ2star_8TeV-pythia6_Summer12_DR53X-PU_S10_START53_V7A-v2-3c2c2ea126eaa14f77b77129f5e671ee_USER_STOP2012-v1/"
 #dir = "/store/user/ahart/DYToTauTau_M-20_CT10_TuneZ2star_v2_8TeV-powheg-tauola-pythia6/BEAN2012-v4/4a12a6f1b79fbeb9915fe6064a3d6fce/"
-dir = "/store/user/jbrinson/TTJets_FullLeptMGDecays_8TeV-madgraph/BEAN2012-v4/0ff8045eb3a4a7ce9562dd332df0072c/"
+dir = "/store/user/ahart/eMuMinimal/TTbar_Lep/EMu_Minimal"
 
 for file in os.listdir(dir):
-        process.source.fileNames.extend(cms.untracked.vstring('file:' + dir + file))
+        process.source.fileNames.extend(cms.untracked.vstring('file:' + dir + '/' + file))
 
 ######################################################################
 ##### Overwrite specific variables defined in osuAnalysis_cfi.py #####
 ######################################################################
 
-process.OSUAnalysis.applyTriggerSF = True
+process.OSUAnalysis.electronSFFile  =  cms.string (os.environ['CMSSW_BASE'] + '/src/OSUT3Analysis/Configuration/data/MVANonTrig_HtoZZto4l_IdIsoSip.root')
+process.OSUAnalysis.electronSFID    =  cms.string ('')
+process.OSUAnalysis.electronSF      =  cms.string ('h_electronScaleFactor_IdIsoSip')
+process.OSUAnalysis.applyTriggerSF  =  True
 
 ########################################################################
 ##### Import the information about all the histograms to be filled #####
