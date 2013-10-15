13b456a0d405a2dcfaa06dc9c48ed69ddb4216f9
bing <bingxuan.liu@cern.ch>
2013-10-15 09:12:12 -0400

    Added half blind control region.

--- BackgroundStudies/python/ZTauTauControlRegion.py
+++ BackgroundStudies/python/ZTauTauControlRegion.py
@@ -7,6 +7,12 @@ from DisplacedSUSY.StandardAnalysis.Preselection import *
 
 ##########################################################################
 
+
+
+
+
+
+
 ZTauTauControlRegion = cms.PSet(
     name = cms.string("ZTauTautoEMu"),
     triggers = cms.vstring("HLT_Mu22_Photon22_CaloIdL_v"),
@@ -55,6 +61,47 @@ ZTauTauControlRegion_Prompt.cuts.append(ht_cut)
 
 
 ##########################################################################
+ZTauTauControlRegion_ElectronPrompt = cms.PSet(
+    name = cms.string("ZTauTautoEMu_ElectronPrompt"),
+    triggers = cms.vstring("HLT_Mu22_Photon22_CaloIdL_v"),
+    cuts = cms.VPSet ()
+)
+ZTauTauControlRegion_ElectronPrompt.cuts.extend(copy.deepcopy(ZTauTauControlRegion_Prompt.cuts))
+for cut in ZTauTauControlRegion_ElectronPrompt.cuts:
+	if "(correctedD0) < 0.02" in str(cut.cutString) and cut.inputCollection == cms.string("muons"):
+		ZTauTauControlRegion_ElectronPrompt.cuts.remove(cut) 
 
 
+ZTauTauControlRegion_MuonPrompt = cms.PSet(
+    name = cms.string("ZTauTautoEMu_MuonPrompt"),
+    triggers = cms.vstring("HLT_Mu22_Photon22_CaloIdL_v"),
+    cuts = cms.VPSet ()
+)
+ZTauTauControlRegion_MuonPrompt.cuts.extend(copy.deepcopy(ZTauTauControlRegion_Prompt.cuts))
+for cut in ZTauTauControlRegion_MuonPrompt.cuts:
+	if "(correctedD0) < 0.02" in str(cut.cutString) and cut.inputCollection == cms.string("electrons"):
+		ZTauTauControlRegion_MuonPrompt.cuts.remove(cut) 
+##########################################################################
+QCDinZTauTauControlRegion_ElectronPrompt = cms.PSet(
+    name = cms.string("QCDinZTauTautoEMu_ElectronPrompt"),
+    triggers = cms.vstring("HLT_Mu22_Photon22_CaloIdL_v"),
+    cuts = cms.VPSet ()
+)
+QCDinZTauTauControlRegion_ElectronPrompt.cuts.extend(copy.deepcopy(ZTauTauControlRegion_ElectronPrompt.cuts))
+for cut in QCDinZTauTauControlRegion_ElectronPrompt.cuts:
+	if "Beta" in str(cut.cutString):
+		cut.cutString = cms.string("relPFdBetaIso > 0.12 & relPFdBetaIso < 1.5")
+        if "rho"  in str(cut.cutString):
+		cut.cutString = cms.string("relPFrhoIso > 0.1 & relPFrhoIso < 1")
 
+QCDinZTauTauControlRegion_MuonPrompt = cms.PSet(
+    name = cms.string("QCDinZTauTautoEMu_MuonPrompt"),
+    triggers = cms.vstring("HLT_Mu22_Photon22_CaloIdL_v"),
+    cuts = cms.VPSet ()
+)
+QCDinZTauTauControlRegion_MuonPrompt.cuts.extend(copy.deepcopy(ZTauTauControlRegion_MuonPrompt.cuts))
+for cut in QCDinZTauTauControlRegion_MuonPrompt.cuts:
+	 if "Beta" in str(cut.cutString):
+                cut.cutString = cms.string("relPFdBetaIso > 0.12 & relPFdBetaIso < 1.5")
+         if "rho"  in str(cut.cutString):
+                cut.cutString = cms.string("relPFrhoIso > 0.1 & relPFrhoIso < 1")
