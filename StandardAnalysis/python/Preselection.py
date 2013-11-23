ff28b8140bac1af1c8fd5bbd255e96414febbadc
Andrew Hart <ahart@cern.ch>
2013-11-23 11:13:51 -0500

    Added the missing hits cut to the conversion rejection and added versions of the preselection with |d0| < 100 micron and |d0| < 50 micron cuts.

--- StandardAnalysis/python/Preselection.py
+++ StandardAnalysis/python/Preselection.py
@@ -80,8 +80,9 @@ Preselection = cms.PSet(
       # PHOTON CONVERSION VETO
       cms.PSet (
         inputCollection = cms.string("electrons"),
-        cutString = cms.string("passConvVeto > 0"),
-        numberRequired = cms.string(">= 1")
+        cutString = cms.string("passConvVeto > 0 & numberOfLostHits = 0"),
+        numberRequired = cms.string(">= 1"),
+        alias = cms.string("electron conversion rejection")
       ),
       # ELECTRON ISOLATION
       cms.PSet (
@@ -139,6 +140,32 @@ Preselection = cms.PSet(
         cutString = cms.string("deltaR > 0.5"),
         numberRequired = cms.string("== 1")
       ),
+
+      ########### START OF ADDITIONAL CUTS TO REQUIRE LEPTON IS NOT IN A JET
+      # ONLY CONSIDER 30 GEV JETS
+#      cms.PSet (
+#        inputCollection = cms.string("jets"),
+#        cutString = cms.string("pt > 30"),
+#        numberRequired = cms.string(">= 0")
+#      ),
+#      # ELECTRON NOT OVERLAPPING WITH JET
+#      cms.PSet (
+#        inputCollection = cms.string("electron-jet pairs"),
+#        cutString = cms.string("deltaR < 0.5"),
+#        numberRequired = cms.string("== 0"),
+#        isVeto = cms.bool(True),
+#        alias = cms.string("electron near jet veto"),
+#      ),
+#      # MUON NOT OVERLAPPING WITH JET
+#      cms.PSet (
+#        inputCollection = cms.string("muon-jet pairs"),
+#        cutString = cms.string("deltaR < 0.5"),
+#        numberRequired = cms.string("== 0"),
+#        isVeto = cms.bool(True),
+#        alias = cms.string("muon near jet veto"),
+#      ),
+      ########### END OF ADDITIONAL CUTS TO REQUIRE LEPTON IS NOT IN A JET      
+
       # RESTRICT ELECTRONS TO RECONSTRUCTION ACCEPTANCE
       cms.PSet (
         inputCollection = cms.string("electrons"),
@@ -224,3 +251,82 @@ PreselectionWithGenMatching = cms.PSet(
 )
 PreselectionWithGenMatching.cuts.extend(copy.deepcopy(SignalGenMatching.cuts))
 PreselectionWithGenMatching.cuts.extend(copy.deepcopy(Preselection.cuts))
+
+#################################################################
+
+# PRESELECTION WITH 100 MICRON |D0| CUTS
+
+Preselection_100um = cms.PSet(
+    name = cms.string("Preselection_100um"),
+    triggers = copy.deepcopy(Preselection.triggers),
+    cuts = cms.VPSet ()
+)
+Preselection_100um.cuts.extend(copy.deepcopy(Preselection.cuts))
+
+electron_d0_cut = cms.PSet (
+    inputCollection = cms.string("electrons"),
+    cutString = cms.string("abs(correctedD0) > 0.01"),
+    numberRequired = cms.string("== 1")
+)
+Preselection_100um.cuts.append(electron_d0_cut)
+
+muon_d0_cut = cms.PSet (
+    inputCollection = cms.string("muons"),
+    cutString = cms.string("abs(correctedD0) > 0.01"),
+    numberRequired = cms.string("== 1")
+)
+Preselection_100um.cuts.append(muon_d0_cut)
+
+#################################################################
+
+# PRESELECTION WITH SS LEPTONS AND 100 MICRON |D0| CUTS
+
+Preselection_SS_100um = cms.PSet(
+        name = cms.string("Preselection_SS_100um"),
+            triggers = copy.deepcopy(Preselection_100um.triggers),
+            cuts = cms.VPSet ()
+        )
+Preselection_SS_100um.cuts.extend(copy.deepcopy(Preselection_100um.cuts))
+for cut in Preselection_SS_100um.cuts:
+    if "chargeProduct" in str(cut.cutString):
+        cut.cutString = cms.string('chargeProduct > 0')
+
+#################################################################
+
+# PRESELECTION WITH 50 MICRON |D0| CUTS
+
+Preselection_50um = cms.PSet(
+    name = cms.string("Preselection_50um"),
+    triggers = copy.deepcopy(Preselection.triggers),
+    cuts = cms.VPSet ()
+)
+Preselection_50um.cuts.extend(copy.deepcopy(Preselection.cuts))
+
+electron_d0_cut = cms.PSet (
+    inputCollection = cms.string("electrons"),
+    cutString = cms.string("abs(correctedD0) > 0.005"),
+    numberRequired = cms.string("== 1")
+)
+Preselection_50um.cuts.append(electron_d0_cut)
+
+muon_d0_cut = cms.PSet (
+    inputCollection = cms.string("muons"),
+    cutString = cms.string("abs(correctedD0) > 0.005"),
+    numberRequired = cms.string("== 1")
+)
+Preselection_50um.cuts.append(muon_d0_cut)
+
+#################################################################
+
+# PRESELECTION WITH SS LEPTONS AND 50 MICRON |D0| CUTS
+
+Preselection_SS_50um = cms.PSet(
+        name = cms.string("Preselection_SS_50um"),
+            triggers = copy.deepcopy(Preselection_50um.triggers),
+            cuts = cms.VPSet ()
+        )
+Preselection_SS_50um.cuts.extend(copy.deepcopy(Preselection_50um.cuts))
+for cut in Preselection_SS_50um.cuts:
+    if "chargeProduct" in str(cut.cutString):
+        cut.cutString = cms.string('chargeProduct > 0')
+
