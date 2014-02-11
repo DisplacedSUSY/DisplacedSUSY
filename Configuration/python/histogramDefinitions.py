686e045ff22f3c06e0d60131dcbbad0d83fe1522
bing <bingxuan.liu@cern.ch>
2014-02-11 09:16:35 -0500

    added histograms with variable bins for medium d0 range

--- Configuration/python/histogramDefinitions.py
+++ Configuration/python/histogramDefinitions.py
@@ -42,7 +42,12 @@ MuonD0Histograms = cms.PSet(
         cms.PSet (
             name = cms.string("muonAbsD0BeamspotM"),
             title = cms.string("Muon |d_{0}| wrt Beamspot; |d_{0}| [cm]"),
-            #bins = cms.untracked.vdouble(100, 0, 0.5),
+            bins = cms.untracked.vdouble(100, 0, 0.5),
+            inputVariables = cms.vstring("abs(correctedD0)")
+        ),
+        cms.PSet (
+            name = cms.string("muonAbsD0BeamspotMVariableBins"),
+            title = cms.string("Muon |d_{0}| wrt Beamspot; |d_{0}| [cm]"),
             variableBinsX = cms.untracked.vdouble(0.00,0.01,0.02,0.03,0.04,0.05,0.07,0.09,0.11,0.15,0.20,0.50),
             inputVariables = cms.vstring("abs(correctedD0)")
         ),
@@ -200,7 +205,12 @@ ElectronD0Histograms = cms.PSet(
         cms.PSet (
             name = cms.string("electronAbsD0BeamspotM"),
             title = cms.string("Electron |d_{0}| wrt Beamspot; |d_{0}| [cm]"),
-            #bins = cms.untracked.vdouble(100, 0, 0.5),
+            bins = cms.untracked.vdouble(100, 0, 0.5),
+            inputVariables = cms.vstring("abs(correctedD0)")
+        ),
+        cms.PSet (
+            name = cms.string("electronAbsD0BeamspotMVariableBins"),
+            title = cms.string("Electron |d_{0}| wrt Beamspot; |d_{0}| [cm]"),
             variableBinsX = cms.untracked.vdouble(0.00,0.01,0.02,0.03,0.04,0.05,0.07,0.09,0.11,0.15,0.20,0.50),
             inputVariables = cms.vstring("abs(correctedD0)")
         ),
