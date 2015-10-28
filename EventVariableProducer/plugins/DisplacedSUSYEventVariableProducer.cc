687f546ce98c742cb8d88bf663dec6113cd0d892
bing <bingxuan.liu@cern.ch>
2015-10-28 17:41:21 -0400

    Add jet cuts in the calculation of sumJetPt

--- EventVariableProducer/plugins/DisplacedSUSYEventVariableProducer.cc
+++ EventVariableProducer/plugins/DisplacedSUSYEventVariableProducer.cc
@@ -11,12 +11,32 @@ DisplacedSUSYEventVariableProducer::AddVariables (const edm::Event &event) {
 #if DATA_FORMAT == MINI_AOD_CUSTOM
 
   objectsToGet_.insert ("candjets");
+  objectsToGet_.insert ("primaryvertexs");
   anatools::getRequiredCollections (objectsToGet_, collections_, handles_, event);
-  double value = 0;
+  double sumJetPt = 0;
+  double numLooseBjets = 0; 
+  double numMediumBjets = 0; 
+  double numTightBjets = 0; 
+  double numPV = 0;
   for (const auto &candjet1 : *handles_.candjets) {
-    value = value + anatools::getMember(candjet1, "pt");
+    if(anatools::getMember(candjet1, "pt") >= 25 && abs(anatools::getMember(candjet1, "eta")) <= 2.4 && anatools::getMember(candjet1,"neutralHadronEnergyFraction") < 0.99 && anatools::getMember(candjet1,"chargedEmEnergyFraction") < 0.99 && anatools::getMember(candjet1, "neutralEmEnergyFraction") < 0.99 && anatools::getMember(candjet1, "numberOfDaughters") > 1 && anatools::getMember(candjet1, "chargedHadronEnergyFraction") > 0.0 && anatools::getMember(candjet1, "chargedMultiplicity") > 0.0 )  
+      sumJetPt = sumJetPt + anatools::getMember(candjet1, "pt");
+    if(anatools::getMember(candjet1, "pfCombinedInclusiveSecondaryVertexV2BJetTags") >= 0.605)
+      numLooseBjets = numLooseBjets + 1;
+    if(anatools::getMember(candjet1, "pfCombinedInclusiveSecondaryVertexV2BJetTags") >= 0.89)
+      numMediumBjets = numMediumBjets + 1;
+    if(anatools::getMember(candjet1, "pfCombinedInclusiveSecondaryVertexV2BJetTags") >= 0.97)
+      numTightBjets = numTightBjets + 1;
   }
-  (*eventvariables)["sumJetPt"] = value;
+ for (const auto &pv1 : *handles_.primaryvertexs) {
+    if(anatools::getMember(pv1,"isValid"))
+      numPV = numPV + 1;
+  }
+  (*eventvariables)["sumJetPt"] = sumJetPt;
+  (*eventvariables)["numLooseBjets"] = numLooseBjets;
+  (*eventvariables)["numMediumBjets"] = numMediumBjets;
+  (*eventvariables)["numTightBjets"] = numTightBjets;
+  (*eventvariables)["numPV"] = numPV;
 #  endif
   }  
 #include "FWCore/Framework/interface/MakerMacros.h"
