6a6504d2ec52eae6ec388be88e296faf089a0b9a
bing <bingxuan.liu@cern.ch>
2015-11-19 16:18:15 -0500

    TTBar control region

---
+++ BackgroundStudies/test/TTBarControlRegionOptions.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+# to be run with submitToCondor.py -l protoBatchConfig.py
+
+# import the definitions of all the datasets on the T3
+from OSUT3Analysis.Configuration.configurationOptions_13TeV import *
+
+# specify which config file to pass to cmsRun
+config_file = "TTbarControlRegion_cfg.py"
+
+# choose luminosity used for MC normalization
+intLumi = 2445.23# from 8 TeV MuEG dataset
+
+# create list of datasets to process
+datasets = [
+    'Diboson_MiniAOD',
+    'QCD_MuEnriched_MiniAOD',
+    'WJetsToLNu_MiniAOD',
+    'SingleTop_MiniAOD',
+    'TTJets_Lept_MiniAOD',
+    'DYJetsToLL_50_MiniAOD',
+    'MuonEG_2015D',
+]
+
+InputCondorArguments = {'hold':'true'}
