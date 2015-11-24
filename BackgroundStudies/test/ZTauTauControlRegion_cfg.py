630d5c98375c3f2055a6a07c1d9fa48cdc4f0fa3
bing <bingxuan.liu@cern.ch>
2015-11-24 16:53:12 -0500

    Update the ztatau selections

--- BackgroundStudies/test/ZTauTauControlRegion_cfg.py
+++ BackgroundStudies/test/ZTauTauControlRegion_cfg.py
@@ -62,6 +62,7 @@ collections = miniAOD_collections
 
 variableProducers = []
 variableProducers.append('PUScalingFactorProducer')
+variableProducers.append('DisplacedSUSYEventVariableProducer')
 
 weights = cms.VPSet (
     cms.PSet (
@@ -88,8 +89,9 @@ from DisplacedSUSY.StandardAnalysis.HistogramsDefinitions import *
 add_channels (process, eventSelections, cms.VPSet (muonHistograms,electronHistograms,electronMuonHistograms,eventHistograms, metHistograms), weights, collections,variableProducers, False)
 
 process.PUScalingFactorProducer.dataset = cms.string("TTJets_DiLept_MiniAOD")
-process.PUScalingFactorProducer.PU = cms.string("/data/users/bing/condor/PU2015MC/puMC.root")
+process.PUScalingFactorProducer.PU = cms.string(os.environ['CMSSW_BASE'] + '/src/DisplacedSUSY/StandardAnalysis/data/pu.root')
 #process.PUScalingFactorProducer.type = cms.string("data")
 process.PUScalingFactorProducer.type = cms.string("bgMC")
+process.DisplacedSUSYEventVariableProducer.type = cms.string("bgMC")
 
 #outfile = open('dumpedConfig.py','w'); print >> outfile,process.dumpPython(); outfile.close()
