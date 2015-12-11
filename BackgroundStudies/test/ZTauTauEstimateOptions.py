83b0958478b461fd0b25a2bb4b4bb476b2515b46
bing <bingxuan.liu@cern.ch>
2015-12-11 16:26:28 -0500

    Configuration files to run ztautaut background estimation

---
+++ BackgroundStudies/test/ZTauTauEstimateOptions.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+# to be run with submitToCondor.py -l protoBatchConfig.py
+
+# import the definitions of all the datasets on the T3
+from OSUT3Analysis.Configuration.configurationOptions_13TeV import *
+
+# specify which config file to pass to cmsRun
+config_file = "ZTauTauEstimate_cfg.py"
+
+# choose luminosity used for MC normalization
+intLumi = 2538.43# from 13 TeV MuEG dataset 2015D SilverJson
+
+# create list of datasets to process
+datasets = [
+    'DYJetsToLL_50_MiniAOD',
+]
+
+InputCondorArguments = {'hold':'true'}
