d2dbc436db8d3c92f6fe09bb5a27d1c5dadaf441
bing <bingxuan.liu@cern.ch>
2015-12-14 15:59:30 -0500

    Add configuration files to produce cosmic MC

---
+++ DisplacedTrackingStudies/test/CosmicMuon_GEN_SIM.py
@@ -0,0 +1,165 @@
+# Auto generated configuration file
+# using: 
+# Revision: 1.19 
+# Source: /local/reps/CMSSW/CMSSW/Configuration/Applications/python/ConfigBuilder.py,v 
+# with command line options: UndergroundCosmicMu_cfi.py --conditions auto:run2_mc -n 10 --eventcontent RAWSIM -s GEN,SIM --datatier GEN-SIM --beamspot NominalCollision2015 --customise SLHCUpgradeSimulations/Configuration/postLS1Customs.customisePostLS1 --magField 38T_PostLS1 --no_exec --no_exec
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process('SIM')
+
+# import of standard configurations
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
+process.load('FWCore.MessageService.MessageLogger_cfi')
+process.load('Configuration.EventContent.EventContent_cff')
+process.load('SimGeneral.MixingModule.mixNoPU_cfi')
+process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
+process.load('Configuration.Geometry.GeometrySimDB_cff')
+process.load('Configuration.StandardSequences.MagneticField_38T_PostLS1_cff')
+process.load('Configuration.StandardSequences.Generator_cff')
+process.load('IOMC.EventVertexGenerators.VtxSmearedNominalCollision2015_cfi')
+process.load('GeneratorInterface.Core.genFilterSummary_cff')
+process.load('Configuration.StandardSequences.SimIdeal_cff')
+process.load('Configuration.StandardSequences.EndOfProcess_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_condDBv2_cff')
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(100)
+)
+
+# Input source
+process.source = cms.Source("EmptySource")
+
+process.options = cms.untracked.PSet(
+
+)
+
+# Production Info
+process.configurationMetadata = cms.untracked.PSet(
+    annotation = cms.untracked.string('UndergroundCosmicMu_cfi.py nevts:10'),
+    name = cms.untracked.string('Applications'),
+    version = cms.untracked.string('$Revision: 1.19 $')
+)
+
+# Output definition
+
+process.RAWSIMoutput = cms.OutputModule("PoolOutputModule",
+    SelectEvents = cms.untracked.PSet(
+        SelectEvents = cms.vstring('generation_step')
+    ),
+    dataset = cms.untracked.PSet(
+        dataTier = cms.untracked.string('GEN-SIM'),
+        filterName = cms.untracked.string('')
+    ),
+    eventAutoFlushCompressedSize = cms.untracked.int32(5242880),
+    fileName = cms.untracked.string('CosmicMuonDisplacedSUSY_GEN_SIM.root'),
+    outputCommands = process.RAWSIMEventContent.outputCommands,
+    splitLevel = cms.untracked.int32(0)
+)
+
+# Additional output definition
+
+# Other statements
+process.genstepfilter.triggerConditions=cms.vstring("generation_step")
+from Configuration.AlCa.GlobalTag_condDBv2 import GlobalTag
+process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:run2_mc', '')
+
+process.cosmicInTracker = cms.EDFilter("CosmicGenFilterHelix",
+    charges = cms.vint32(1, -1),
+    doMonitor = cms.untracked.bool(False),
+    maxZ = cms.double(212.0),
+    minP = cms.double(0.0),
+    minPt = cms.double(0.0),
+    minZ = cms.double(-212.0),
+    pdgIds = cms.vint32(-13, 13),
+    propagator = cms.string('SteppingHelixPropagatorAlong'),
+    radius = cms.double(80.0),
+    src = cms.InputTag("generator")
+)
+
+
+process.generator = cms.EDProducer("CosMuoGenProducer",
+    AcptAllMu = cms.bool(False),
+    ClayWidth = cms.double(50000.0),
+    ElossScaleFactor = cms.double(1.0),
+    MTCCHalf = cms.bool(False),
+    MaxEnu = cms.double(10000.0),
+    MaxP = cms.double(3000.0),
+    MaxPhi = cms.double(360.0),
+    MaxT0 = cms.double(12.5),
+    MaxTheta = cms.double(75.0),
+    MinEnu = cms.double(10.0),
+    MinP = cms.double(10.0),
+    MinP_CMS = cms.double(-1.0),
+    MinPhi = cms.double(0.0),
+    MinT0 = cms.double(-12.5),
+    MinTheta = cms.double(0.0),
+    MultiMuon = cms.bool(False),
+    MultiMuonFileFirstEvent = cms.int32(1),
+    MultiMuonFileName = cms.string('CORSIKAmultiMuon.root'),
+    MultiMuonNmin = cms.int32(2),
+    NuProdAlt = cms.double(7500000.0),
+    PlugVx = cms.double(0.0),
+    PlugVz = cms.double(-14000.0),
+    RadiusOfTarget = cms.double(8000.0),
+    RhoAir = cms.double(0.001214),
+    RhoClay = cms.double(2.3),
+    RhoPlug = cms.double(2.5),
+    RhoRock = cms.double(2.5),
+    RhoWall = cms.double(2.5),
+    TIFOnly_constant = cms.bool(False),
+    TIFOnly_linear = cms.bool(False),
+    TrackerOnly = cms.bool(False),
+    Verbosity = cms.bool(False),
+    ZCentrOfTarget = cms.double(0.0),
+    ZDistOfTarget = cms.double(15000.0)
+)
+
+
+process.SteppingHelixPropagatorAlong = cms.ESProducer("SteppingHelixPropagatorESProducer",
+    ApplyRadX0Correction = cms.bool(True),
+    AssumeNoMaterial = cms.bool(False),
+    ComponentName = cms.string('SteppingHelixPropagatorAlong'),
+    NoErrorPropagation = cms.bool(False),
+    PropagationDirection = cms.string('alongMomentum'),
+    SetVBFPointer = cms.bool(False),
+    VBFName = cms.string('VolumeBasedMagneticField'),
+    debug = cms.bool(False),
+    endcapShiftInZNeg = cms.double(0.0),
+    endcapShiftInZPos = cms.double(0.0),
+    returnTangentPlane = cms.bool(True),
+    sendLogWarning = cms.bool(False),
+    useEndcapShiftsInZ = cms.bool(False),
+    useInTeslaFromMagField = cms.bool(False),
+    useIsYokeFlag = cms.bool(True),
+    useMagVolumes = cms.bool(True),
+    useMatVolumes = cms.bool(True),
+    useTuningForL2Speed = cms.bool(False)
+)
+
+
+process.ProductionFilterSequence = cms.Sequence(process.generator+process.cosmicInTracker)
+
+# Path and EndPath definitions
+process.generation_step = cms.Path(process.pgen)
+process.simulation_step = cms.Path(process.psim)
+process.genfiltersummary_step = cms.EndPath(process.genFilterSummary)
+process.endjob_step = cms.EndPath(process.endOfProcess)
+process.RAWSIMoutput_step = cms.EndPath(process.RAWSIMoutput)
+
+# Schedule definition
+process.schedule = cms.Schedule(process.generation_step,process.genfiltersummary_step,process.simulation_step,process.endjob_step,process.RAWSIMoutput_step)
+# filter all path with the production filter sequence
+for path in process.paths:
+	getattr(process,path)._seq = process.ProductionFilterSequence * getattr(process,path)._seq 
+
+# customisation of the process.
+
+# Automatic addition of the customisation function from SLHCUpgradeSimulations.Configuration.postLS1Customs
+from SLHCUpgradeSimulations.Configuration.postLS1Customs import customisePostLS1 
+
+#call to customisation function customisePostLS1 imported from SLHCUpgradeSimulations.Configuration.postLS1Customs
+process = customisePostLS1(process)
+
+# End of customisation functions
+
---
+++ DisplacedTrackingStudies/test/CosmicMuon_RECO.py
@@ -0,0 +1,85 @@
+# Auto generated configuration file
+# using: 
+# Revision: 1.19 
+# Source: /local/reps/CMSSW/CMSSW/Configuration/Applications/python/ConfigBuilder.py,v 
+# with command line options: step3 --customise SLHCUpgradeSimulations/Configuration/postLS1Customs.customisePostLS1 --conditions auto:run2_mc -s RAW2DIGI,L1Reco,RECO --datatier GEN-SIM-RECO -n 10 --magField 38T_PostLS1 --eventcontent RECOSIM --no_exec
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process('RECO')
+
+# import of standard configurations
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
+process.load('FWCore.MessageService.MessageLogger_cfi')
+process.load('Configuration.EventContent.EventContent_cff')
+process.load('SimGeneral.MixingModule.mixNoPU_cfi')
+process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
+process.load('Configuration.StandardSequences.MagneticField_38T_PostLS1_cff')
+process.load('Configuration.StandardSequences.RawToDigi_cff')
+process.load('Configuration.StandardSequences.L1Reco_cff')
+process.load('Configuration.StandardSequences.Reconstruction_cff')
+process.load('Configuration.StandardSequences.EndOfProcess_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_condDBv2_cff')
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(100)
+)
+
+# Input source
+process.source = cms.Source("PoolSource",
+    fileNames = cms.untracked.vstring('file:CosmicMuonDisplacedSUSY_HLT.root'),
+    secondaryFileNames = cms.untracked.vstring()
+)
+
+process.options = cms.untracked.PSet(
+
+)
+
+# Production Info
+process.configurationMetadata = cms.untracked.PSet(
+    annotation = cms.untracked.string('step3 nevts:10'),
+    name = cms.untracked.string('Applications'),
+    version = cms.untracked.string('$Revision: 1.19 $')
+)
+
+# Output definition
+
+process.RECOSIMoutput = cms.OutputModule("PoolOutputModule",
+    dataset = cms.untracked.PSet(
+        dataTier = cms.untracked.string('GEN-SIM-RECO'),
+        filterName = cms.untracked.string('')
+    ),
+    eventAutoFlushCompressedSize = cms.untracked.int32(5242880),
+    fileName = cms.untracked.string('CosmicMuonDisplacedSUSY_RECO.root'),
+    outputCommands = process.RECOSIMEventContent.outputCommands,
+    splitLevel = cms.untracked.int32(0)
+)
+
+process.RECOSIMoutput.outputCommands.append("keep *_genParticles_*_*")
+
+# Additional output definition
+
+# Other statements
+from Configuration.AlCa.GlobalTag_condDBv2 import GlobalTag
+process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:run2_mc', '')
+
+# Path and EndPath definitions
+process.raw2digi_step = cms.Path(process.RawToDigi)
+process.L1Reco_step = cms.Path(process.L1Reco)
+process.reconstruction_step = cms.Path(process.reconstruction)
+process.endjob_step = cms.EndPath(process.endOfProcess)
+process.RECOSIMoutput_step = cms.EndPath(process.RECOSIMoutput)
+
+# Schedule definition
+process.schedule = cms.Schedule(process.raw2digi_step,process.L1Reco_step,process.reconstruction_step,process.endjob_step,process.RECOSIMoutput_step)
+
+# customisation of the process.
+
+# Automatic addition of the customisation function from SLHCUpgradeSimulations.Configuration.postLS1Customs
+from SLHCUpgradeSimulations.Configuration.postLS1Customs import customisePostLS1 
+
+#call to customisation function customisePostLS1 imported from SLHCUpgradeSimulations.Configuration.postLS1Customs
+process = customisePostLS1(process)
+
+# End of customisation functions
+
