6e91b250a6737d23be25f39e7cebf5a8fae1d73e
bing <bingxuan.liu@cern.ch>
2016-01-22 11:32:43 -0500

    change the orders of samples and set hold to be true

--- BackgroundStudies/test/QCDElectronControlRegionOptions.py
+++ BackgroundStudies/test/QCDElectronControlRegionOptions.py
@@ -10,17 +10,17 @@ from OSUT3Analysis.Configuration.miniAODV2Samples import *
 config_file = "QCDElectronControlRegion_cfg.py"
 
 # choose luminosity used for MC normalization
-intLumi = 2556.37 # for 13 TeV Silver Json, 2015D 
+intLumi = 2.502 # for 13 TeV Silver Json, 2015D 
 
 # create list of datasets to process
 datasets = [
     'SingleEle_2015D',
-    'QCD_EMEnriched',
-    'QCD_bcToE',
-    'TTJets_Lept',
-    'SingleTop',
     'DYJetsToLL_50',
     'WJetsToLNu',
     'Diboson',
+    'SingleTop',
+    'TTJets_Lept',
+    'QCD_EMEnriched',
+    'QCD_bcToE',
 ]
 InputCondorArguments = {'hold': 'true'}
