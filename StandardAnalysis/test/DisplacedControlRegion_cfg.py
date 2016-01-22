baaeaca2f12a78095c351269a5de54d230b67fcc
bing <bingxuan.liu@cern.ch>
2016-01-22 11:40:54 -0500

    add the new channels

--- StandardAnalysis/test/DisplacedControlRegion_cfg.py
+++ StandardAnalysis/test/DisplacedControlRegion_cfg.py
@@ -15,8 +15,7 @@ process.MessageLogger.cerr.FwkReport.reportEvery = 100
 process.source = cms.Source ('PoolSource',
   fileNames = cms.untracked.vstring (
     #'root://cmsxrootd.fnal.gov//store/mc/RunIISpring15DR74/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/MINIAODSIM/Asympt25ns_MCRUN2_74_V9-v3/10000/009D49A5-7314-E511-84EF-0025905A605E.root',
-    #'file:/data/users/bing/condor/EMuSkim13TeV/MuonEG_2015D_v3/EMuSKim13TeV/skim_1.root',
-    'file:/data/users/bing/condor/EMuSkim13TeV/TTJets_DiLept_MiniAOD/EMuSKim13TeV/skim_416.root',
+    'file:/data/users/bing/condor/EMuSkimJan14th/MuonEG_2015D_v3/EMuSkimSelection/skim_0.root',
   )
 )
 
@@ -40,20 +39,20 @@ miniAOD_collections = cms.PSet (
   electrons       =  cms.InputTag  ('slimmedElectrons',''),
   genjets         =  cms.InputTag  ('slimmedGenJets',                 ''),
   jets            =  cms.InputTag  ('slimmedJets',                    ''),
-  generatorweights = cms.InputTag  ('generator', ''), 
-  pileupinfos     =  cms.InputTag  ('slimmedAddPileupInfo',  ''),
+  bjets           =  cms.InputTag  ('slimmedJets',                    ''),
+  generatorweights= cms.InputTag  ('generator', ''), 
   mcparticles     =  cms.InputTag  ('packedGenParticles',             ''),
   mets            =  cms.InputTag  ('slimmedMETs',                    ''),
   muons           =  cms.InputTag  ('slimmedMuons',                   ''),
   photons         =  cms.InputTag  ('slimmedPhotons',                 ''),
   primaryvertexs  =  cms.InputTag  ('offlineSlimmedPrimaryVertices',  ''),
+  pileupinfos     =  cms.InputTag  ('slimmedAddPileupInfo',  ''),
   beamspots       =  cms.InputTag  ('offlineBeamSpot',                ''),
   superclusters   =  cms.InputTag  ('reducedEgamma',                  'reducedSuperClusters'),
   taus            =  cms.InputTag  ('slimmedTaus',                    ''),
   triggers        =  cms.InputTag  ('TriggerResults',                 '',  'HLT'),
   trigobjs        =  cms.InputTag  ('selectedPatTrigger',             ''),
 )
-
 collections = miniAOD_collections
 
 ################################################################################
@@ -62,7 +61,6 @@ collections = miniAOD_collections
 
 variableProducers = []
 variableProducers.append('PUScalingFactorProducer')
-#DisplacedSUSYEventVariableProducer can only run over skims
 variableProducers.append('DisplacedSUSYEventVariableProducer')
 
 
@@ -92,7 +90,7 @@ eventSelections = []
 #eventSelections.append(IsoMuAntiIsoEleDisplacedControlRegionInclusiveDisplacedTrigger)
 #eventSelections.append(AntiIsoMuAntiIsoEleDisplacedControlRegionInclusiveDisplacedTrigger)
 #eventSelections.append(AntiIsoMuIsoEleDisplacedControlRegionDisplacedTrigger)
-eventSelections.append(DisplacedControlRegionDisplacedTrigger)
+eventSelections.append(DisplacedControlRegionNoIsoNoOSInclusiveDisplacedTrigger)
 #eventSelections.append(IsoMuAntiIsoEleDisplacedControlRegionDisplacedTrigger)
 #eventSelections.append(AntiIsoMuAntiIsoEleDisplacedControlRegionDisplacedTrigger)
 
@@ -104,15 +102,13 @@ from DisplacedSUSY.StandardAnalysis.HistogramsDefinitions import *
 ##### Attach the channels and histograms to the process ########################
 ################################################################################
 #eventHistograms can only run over skims. 
-add_channels (process, eventSelections, cms.VPSet (muonHistograms,electronHistograms,electronMuonHistograms,eventHistograms,metHistograms),weights, collections,variableProducers, False)
+add_channels (process, eventSelections, cms.VPSet (muonHistograms,electronHistograms,electronMuonHistograms,metHistograms,eventHistograms),weights, collections,variableProducers, False)
 
-process.PUScalingFactorProducer.dataset = cms.string("TTJets_DiLept_MiniAOD")
-process.PUScalingFactorProducer.PU = cms.string("$CMSSW_BASE/src/DisplacedSUSY/StandardAnalysis/data/pu.root")
-#process.PUScalingFactorProducer.type = cms.string("data")
-process.PUScalingFactorProducer.type = cms.string("bgMC")
-#DisplacedSUSYEventVariableProducer can only run over skims.
-process.DisplacedSUSYEventVariableProducer.type = cms.string("bgMC")
+process.PUScalingFactorProducer.dataset = cms.string("MuonEG_2015D")
+process.PUScalingFactorProducer.PU = cms.string(os.environ['CMSSW_BASE'] + '/src/DisplacedSUSY/StandardAnalysis/data/pu.root')
+process.PUScalingFactorProducer.type = cms.string("data")
+process.DisplacedSUSYEventVariableProducer.type = cms.string("data")
 process.DisplacedSUSYEventVariableProducer.triggerPath = cms.string("HLT_Mu38NoFiltersNoVtx_Photon38_CaloIdL_v")
-process.DisplacedSUSYEventVariableProducer.triggerScalingFactor = cms.double(0.9596)
+process.DisplacedSUSYEventVariableProducer.triggerScalingFactor = cms.double(0.962)
 
 #outfile = open('dumpedConfig.py','w'); print >> outfile,process.dumpPython(); outfile.close()
