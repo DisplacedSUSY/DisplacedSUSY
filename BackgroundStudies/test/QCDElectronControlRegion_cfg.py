c2bbe2db44097e937a4eb17afd5362d80465db7c
bing <bingxuan.liu@cern.ch>
2016-01-22 11:25:40 -0500

    Add puScalingFactor and event histograms

--- BackgroundStudies/test/QCDElectronControlRegion_cfg.py
+++ BackgroundStudies/test/QCDElectronControlRegion_cfg.py
@@ -14,13 +14,8 @@ process.load ('FWCore.MessageService.MessageLogger_cfi')
 process.MessageLogger.cerr.FwkReport.reportEvery = 100
 process.source = cms.Source ('PoolSource',
   fileNames = cms.untracked.vstring (
-#        'file:/data/users/lantonel/condor/DEC3__DisplacedControlRegionNoTriggerNoIsoSkim/TTJets_DiLept_MiniAOD/DisplacedControlRegionNoTriggerNoIso/skim_0.root'
-
-#        'file:/data/users/bing/condor/EMuSkim13TeV/TTJets_DiLept_MiniAOD/EMuSKim13TeV/skim_416.root'
-#        'file:/data/users/bing/condor/EMuSkim13TeV/DYJetsToLL_50_MiniAOD/EMuSKim13TeV/skim_0.root',
 #    'root://cmsxrootd.fnal.gov//store/mc/RunIISpring15DR74/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/MINIAODSIM/Asympt25ns_MCRUN2_74_V9-v3/10000/009D49A5-7314-E511-84EF-0025905A605E.root',
- 'file:/home/lantonel/CMSSW_7_4_5_ROOT5/src/DisplacedSUSY/BackgroundStudies/test/QCDElectronControlRegion/skim_QCDElectronControlRegion_2015_12_19_13h09m30s.root'
-#    'file:/data/users/bing/condor/EMuSkim13TeV/MuonEG_2015D_v3/EMuSKim13TeV/skim_1.root',
+     'file:/data/users/bing/condor/QCDElectronSkim/QCD_EMEnriched_170to300/QCDElectronSkim/skim_12.root',
   )
 )
 
@@ -69,9 +64,14 @@ collections = miniAOD_collections
 
 
 variableProducers = []
-#variableProducers.append('PUScalingFactorProducer')
+variableProducers.append('PUScalingFactorProducer')
 
-weights = cms.VPSet ()
+weights = cms.VPSet (
+    cms.PSet (
+        inputCollections = cms.vstring("eventvariables"),
+        inputVariable = cms.string("puScalingFactor")
+    ),
+)
 
 ################################################################################
 ##### Import the channels to be run ############################################
@@ -87,21 +87,29 @@ eventSelections.append(QCDElectronControlRegion)
 ################################################################################
 
 from OSUT3Analysis.Configuration.histogramDefinitions import *
+from DisplacedSUSY.Configuration.histogramDefinitions import ElectronD0Histograms
+from DisplacedSUSY.StandardAnalysis.HistogramsDefinitions import eventHistograms
 
 histograms = cms.VPSet()
-#histograms.append(ElectronHistograms)
-#histograms.append(ElectronIPHistograms)
-#histograms.append(MetHistograms)
-#histograms.append(JetHistograms)
+histograms.append(ElectronHistograms)
+histograms.append(ElectronIPHistograms)
+histograms.append(ElectronD0Histograms)
+histograms.append(MetHistograms)
+histograms.append(JetHistograms)
+histograms.append(BjetHistograms)
+histograms.append(JetBjetHistograms)
+histograms.append(ElectronJetHistograms)
+histograms.append(ElectronBjetHistograms)
+histograms.append(eventHistograms)
 
 ################################################################################
 ##### Attach the channels and histograms to the process ########################
 ################################################################################
 
-add_channels (process, eventSelections, histograms, weights, collections, variableProducers, True)
+add_channels (process, eventSelections, histograms, weights, collections, variableProducers, False)
 
-#process.PUScalingFactorProducer.dataset = cms.string("DYJetsToLL_50_MiniAOD")
-#process.PUScalingFactorProducer.PU = cms.string(os.environ['CMSSW_BASE'] + '/src/DisplacedSUSY/StandardAnalysis/data/pu.root')
-#process.PUScalingFactorProducer.type = cms.string("bgMC")
+process.PUScalingFactorProducer.dataset = cms.string("QCD_EMEnriched_170to300")
+process.PUScalingFactorProducer.PU = cms.string(os.environ['CMSSW_BASE'] + '/src/DisplacedSUSY/StandardAnalysis/data/pu.root')
+process.PUScalingFactorProducer.type = cms.string("bgMC")
 
 #outfile = open('dumpedConfig.py','w'); print >> outfile,process.dumpPython(); outfile.close()
