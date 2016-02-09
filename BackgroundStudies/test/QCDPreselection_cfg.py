6efabbefae6e372f1e3ce7cd4c51612ff8434272
bing <bingxuan.liu@cern.ch>
2016-02-09 14:12:54 -0500

    Add lepton scaling factors and the new channels

--- BackgroundStudies/test/QCDPreselection_cfg.py
+++ BackgroundStudies/test/QCDPreselection_cfg.py
@@ -14,9 +14,9 @@ process.load ('FWCore.MessageService.MessageLogger_cfi')
 process.MessageLogger.cerr.FwkReport.reportEvery = 100
 process.source = cms.Source ('PoolSource',
   fileNames = cms.untracked.vstring (
- #       'file:/data/users/bing/condor/EMuSkim13TeV/TTJets_DiLept_MiniAOD/EMuSKim13TeV/skim_416.root'
-        'file:/data/users/bing/condor/EMuSkim13TeV/DYJetsToLL_50_MiniAOD/EMuSKim13TeV/skim_0.root',
-#    'root://cmsxrootd.fnal.gov//store/mc/RunIISpring15DR74/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/MINIAODSIM/Asympt25ns_MCRUN2_74_V9-v3/10000/009D49A5-7314-E511-84EF-0025905A605E.root',
+#       'file:/data/users/bing/condor/EMuSkim13TeV/TTJets_DiLept_MiniAOD/EMuSKim13TeV/skim_416.root'
+        #'file:/home/bing/CMSSW_7_4_5_ROOT5/src/DisplacedSUSY/BackgroundStudies/test/condor/EMuSkimFinal/MuonEG_2015D_v3/EMuSkimSelection/skim_100.root',
+    'root://cmsxrootd.fnal.gov//store/mc/RunIISpring15DR74/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/MINIAODSIM/Asympt25ns_MCRUN2_74_V9-v3/10000/009D49A5-7314-E511-84EF-0025905A605E.root',
 #    'file:/data/users/bing/condor/EMuSkim13TeV/MuonEG_2015D_v3/EMuSKim13TeV/skim_1.root',
   )
 )
@@ -31,7 +31,7 @@ process.TFileService = cms.Service ('TFileService',
 
 # number of events to process when running interactively
 process.maxEvents = cms.untracked.PSet (
-    input = cms.untracked.int32 (1000)
+    input = cms.untracked.int32 (-1)
 )
 
 ################################################################################
@@ -43,7 +43,8 @@ miniAOD_collections = cms.PSet (
   electrons       =  cms.InputTag  ('slimmedElectrons',''),
   genjets         =  cms.InputTag  ('slimmedGenJets',                 ''),
   jets            =  cms.InputTag  ('slimmedJets',                    ''),
-  generatorweights= cms.InputTag  ('generator', ''), 
+  bjets           =  cms.InputTag  ('slimmedJets',                    ''),
+  generatorweights=  cms.InputTag  ('generator', ''), 
   mcparticles     =  cms.InputTag  ('packedGenParticles',             ''),
   mets            =  cms.InputTag  ('slimmedMETs',                    ''),
   muons           =  cms.InputTag  ('slimmedMuons',                   ''),
@@ -74,42 +75,71 @@ weights = cms.VPSet (
         inputCollections = cms.vstring("eventvariables"),
         inputVariable = cms.string("puScalingFactor")
     ),
+    cms.PSet (
+        inputCollections = cms.vstring("eventvariables"),
+        inputVariable = cms.string("triggerScalingFactor")
+    ),
 )
 
+scalingfactorproducers = []
+#ObjectScalingFactorProducer = {}
+#ObjectScalingFactorProducer['name'] = 'ObjectScalingFactorProducer'
+#ObjectScalingFactorProducer['muonFile'] = cms.string(os.environ['CMSSW_BASE'] + '/src/OSUT3Analysis/AnaTools/data/muonSF.root')
+#ObjectScalingFactorProducer['electronFile'] = cms.string(os.environ['CMSSW_BASE'] + '/src/OSUT3Analysis/AnaTools/data/electronSF.root')
+#ObjectScalingFactorProducer['muonWp'] = cms.string('NUM_TightIDandIPCut_DEN_genTracks_PAR_pt_spliteta_bin1/abseta_vs_pt')
+#ObjectScalingFactorProducer['electronWp'] = cms.string('GlobalSF')
+#ObjectScalingFactorProducer['doEleSF'] = cms.bool(True)
+#ObjectScalingFactorProducer['doMuSF'] = cms.bool(True)
+
+#scalingfactorproducers.append(ObjectScalingFactorProducer)
 ################################################################################
 ##### Import the channels to be run ############################################
 ################################################################################
 
-from DisplacedSUSY.StandardAnalysis.EMuPreselection import *
+from DisplacedSUSY.StandardAnalysis.PromptControlRegionSelection import *
 from DisplacedSUSY.BackgroundStudies.QCDPreselections import *
 
 eventSelections = []
-eventSelections.append(EMuPreselectionInclusiveTrigger)
-eventSelections.append(OSAntiIso)
-eventSelections.append(OSIsoMuAntiIsoEle)
-eventSelections.append(OSAntiIsoMuIsoEle)
+#eventSelections.append(OSAntiIso)
+#eventSelections.append(OSAntiIsoMuIsoEle)
+#eventSelections.append(OSIsoMuAntiIsoEle)
+#eventSelections.append(SSAntiIso)
+#eventSelections.append(SSIso)
+eventSelections.append(AntiIsoBlinded)
+#eventSelections.append(AntiIsoBlindedOS)
+#eventSelections.append(AntiIsoBlindedSS)
+#eventSelections.append(AntiIsoNotBlinded)
+#eventSelections.append(AntiIsoDisplacedBlinded)
+#eventSelections.append(AntiIsoLooseDisplacedBlinded)
 
 ################################################################################
 ##### Import the histograms to be plotted ######################################
 ################################################################################
 
-from DisplacedSUSY.StandardAnalysis.HistogramsDefinitions import *
-#eventHistograms can only run over skims. 
-histograms = cms.VPSet (muonHistograms, electronHistograms, electronMuonHistograms, eventHistograms, metHistograms, electronJetHistograms, muonJetHistograms)
-#histograms = cms.VPSet ()
+from OSUT3Analysis.Configuration.histogramDefinitions import *
+from DisplacedSUSY.Configuration.histogramDefinitions import ElectronD0Histograms,MuonD0Histograms,ElectronMuonD0Histograms
+from DisplacedSUSY.StandardAnalysis.HistogramsDefinitions import eventHistograms
 
 ################################################################################
 ##### Attach the channels and histograms to the process ########################
 ################################################################################
+histograms = cms.VPSet()
+histograms.append(ElectronHistograms)
+histograms.append(ElectronD0Histograms)
+histograms.append(MuonHistograms)
+histograms.append(MuonD0Histograms)
+histograms.append(ElectronMuonD0Histograms)
+histograms.append(ElectronMuonHistograms)
+histograms.append(eventHistograms)
 
-add_channels (process, eventSelections, histograms, weights, collections, variableProducers, False)
+add_channels (process, eventSelections, histograms, weights, scalingfactorproducers, collections,variableProducers, False)
 
-process.PUScalingFactorProducer.dataset = cms.string("DYJetsToLL_50_MiniAOD")
-process.PUScalingFactorProducer.PU = cms.string("/data/users/bing/condor/PU2015MC/puMC.root")
+process.PUScalingFactorProducer.dataset = cms.string("DYJetsToLL_50")
+process.PUScalingFactorProducer.PU = cms.string(os.environ['CMSSW_BASE'] + '/src/DisplacedSUSY/StandardAnalysis/data/pu.root')
 process.PUScalingFactorProducer.type = cms.string("bgMC")
 #DisplacedSUSYEventVariableProducer can only run over skims.
 process.DisplacedSUSYEventVariableProducer.type = cms.string("bgMC")
 process.DisplacedSUSYEventVariableProducer.triggerPath = cms.string("HLT_Mu38NoFiltersNoVtx_Photon38_CaloIdL_v")
-process.DisplacedSUSYEventVariableProducer.triggerScalingFactor = cms.double(1)
+process.DisplacedSUSYEventVariableProducer.triggerScalingFactor = cms.double(0.962)
 
 #outfile = open('dumpedConfig.py','w'); print >> outfile,process.dumpPython(); outfile.close()
