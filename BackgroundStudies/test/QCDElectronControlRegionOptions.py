5a5d561c42a25e86560234f1389708776b761cda
bing <bingxuan.liu@cern.ch>
2016-03-23 14:58:57 -0400

    Update

--- BackgroundStudies/test/QCDElectronControlRegionOptions.py
+++ BackgroundStudies/test/QCDElectronControlRegionOptions.py
@@ -4,18 +4,19 @@
 
 # import the definitions of all the datasets on the T3
 from OSUT3Analysis.Configuration.configurationOptions import *
-from OSUT3Analysis.Configuration.miniAODV2Samples import *
+from OSUT3Analysis.Configuration.miniAODV2_76X_Samples import *
 
 # specify which config file to pass to cmsRun
 config_file = "QCDElectronControlRegion_cfg.py"
 
 # choose luminosity used for MC normalization
-intLumi = 6.5 # for 13 TeV Silver Json, 2015D 
+intLumi = 2613 # for 13 TeV Silver Json, 2015D 
 
 # create list of datasets to process
 datasets = [
-    #'DoubleEG_2015D',
+    'DoubleEG_2015D',
     'SingleEle_2015D',
+    'SinglePhoton_2015D',
     'DYJetsToLL_50',
     'WJetsToLNu',
     'Diboson',
