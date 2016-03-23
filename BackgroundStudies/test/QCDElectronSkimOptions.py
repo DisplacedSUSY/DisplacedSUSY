fa87908f4366133a13885d1bbb1c07441ea99666
bing <bingxuan.liu@cern.ch>
2016-03-23 15:01:10 -0400

    added singlephoton

--- BackgroundStudies/test/QCDElectronSkimOptions.py
+++ BackgroundStudies/test/QCDElectronSkimOptions.py
@@ -4,24 +4,25 @@
 
 # import the definitions of all the datasets on the T3
 from OSUT3Analysis.Configuration.configurationOptions import *
-from OSUT3Analysis.Configuration.miniAODV2Samples import *
+from OSUT3Analysis.Configuration.miniAODV2_76X_Samples import *
 
 # specify which config file to pass to cmsRun
 config_file = "QCDElectronSkim_cfg.py"
 
 # choose luminosity used for MC normalization
-intLumi = 2502 # for 13 TeV Silver Json, 2015D 
+intLumi = 2613 # for 13 TeV Silver Json, 2015D 
 
 # create list of datasets to process
 datasets = [
-    #'Diboson',
-    #'WJetsToLNu',
-    #'DYJetsToLL_50',
-    #'SingleTop',
-    #'TTJets_Lept',
-    #'QCD_EMEnriched',
-    #'QCD_bcToE',
+    'Diboson',
+    'WJetsToLNu',
+    'DYJetsToLL_50',
+    'SingleTop',
+    'TTJets_Lept',
+    'QCD_EMEnriched',
+    'QCD_bcToE',
     'SingleEle_2015D',
     'DoubleEG_2015D',
+    'SinglePhoton_2015D',
 ]
 InputCondorArguments = {'hold': 'true'}
