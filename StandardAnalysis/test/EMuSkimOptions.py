baf81f610043457441d9c10dd9ac4e31b31a0a5a
bing <bingxuan.liu@cern.ch>
2016-03-23 15:15:35 -0400

    added some individual datasets

--- StandardAnalysis/test/EMuSkimOptions.py
+++ StandardAnalysis/test/EMuSkimOptions.py
@@ -2,13 +2,13 @@
 
 # import the definitions of all the datasets on the T3
 from OSUT3Analysis.Configuration.configurationOptions import *
-from OSUT3Analysis.Configuration.miniAODV2Samples import *
+from OSUT3Analysis.Configuration.miniAODV2_76X_Samples import *
 
 # specify which config file to pass to cmsRun
 config_file = "EMuSkim_cfg.py"
 
 # choose luminosity used for MC normalization
-intLumi = 2504 # for 13 TeV Silver Json
+intLumi = 2613 # for 13 TeV Silver Json
 
 # create list of datasets to process
 datasets = [
@@ -17,11 +17,22 @@ datasets = [
     #'DYJetsToLL_50',
     #'SingleTop',
     #'TTJets_Lept',
+    #'TTJets_SingleLeptFromT',
     #'QCD_MuEnriched',
     #'MuonEG_2015D',
-    #'DisplacedSUSYSignal'
+    #'MET_2015D',
+    'DisplacedSUSYSignal'
     #'stop600_1mm',
-    'stop700_1mm',
+    #'stop700_1mm',
+    #'WZToLNu2QorQQ2L',
+    #'WG', 
+    #'WWToLNuLNu',
+    #'ZZToLLNuNu',
+    #'WWToLNuQQ',
+    #'ZZToLLQQ',
+    #'QCD_MuEnriched_170to300',
+    #'ZG',
+    #'WZToLLLNu',
 ]
 
 InputCondorArguments = {}
