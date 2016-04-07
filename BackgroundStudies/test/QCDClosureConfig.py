4f6e5312a90d6f8735c46d49016990e5d4904398
bing <bingxuan.liu@cern.ch>
2016-04-07 13:09:33 -0400

    Not affecting

--- BackgroundStudies/test/QCDClosureConfig.py
+++ BackgroundStudies/test/QCDClosureConfig.py
@@ -1,17 +1,13 @@
-bbbarMuFile = '/data/users/bing/condor/QCDMuon76X/SingleMu_2015D.root'
-bbbarEleFile = '/data/users/bing/condor/QCDElectron76X/SingleEle_2015D.root'
-bbbarMuIPShape = 'QCDMuonControlRegionPlotter/Muon-beamspot Plots/muonAbsD0BeamspotS'
-bbbarEleIPShape = 'QCDElectronControlRegionPlotter/Electron-beamspot Plots/electronAbsD0BeamspotS'
+bbbarMuFile = '/data/users/bing/condor/QCDMuon76X_IsoCorr_April5th/SingleMu_2015D.root'
+bbbarEleFile = '/data/users/bing/condor/QCDElectron76X_IsoCorr_April5th/SingleEle_2015D.root'
+bbbarMuIPShape = 'QCDMuonDisplacedControlRegionPlotter/Muon-beamspot Plots/muonAbsD0BeamspotM'
+bbbarEleIPShape = 'QCDElectronDisplacedControlRegionPlotter/Electron-beamspot Plots/electronAbsD0BeamspotM'
 
 normSource = 'Displaced control region'
 
 dataNormFile = '/data/users/bing/condor/DisplacedControlRegion76X_March9/MuonEG_2015D.root'
 bgNormFile = '/data/users/bing/condor/DisplacedControlRegion76X_March9/Background.root'
 normIPDistribution = 'DisplacedControlRegionNoIsoNoOSInclusiveDisplacedTriggerPlotter/Electron-muon-beamspot Plots/electronIpMuonIpInclusive'
-
-#dataNormFile = '/data/users/bing/condor/PromptControlRegionWithSF_Feb8th/MuonEG_2015D.root'
-#bgNormFile = '/data/users/bing/condor/PromptControlRegionWithSF_Feb8th/Background.root'
-#normIPDistribution = 'MuEleNoIsoPromptControlRegionNoOSInclusiveDisplacedTriggerPlotter/Electron-muon-beamspot Plots/electronIpMuonIpInclusive'
 
 targetSource = 'Displaced control region'
 
@@ -20,17 +16,15 @@ bgTargetFile = '/data/users/bing/condor/DisplacedControlRegion76X_March9/Backgro
 targetNormIPDistribution = 'DisplacedControlRegionNoIsoNoOSInclusiveDisplacedTriggerPlotter/Electron-muon-beamspot Plots/electronIpMuonIpInclusive'
 
 normMuLow = 0.01
-normMuHigh = 0.015
+normMuHigh = 0.02
 normEleLow = 0.01
-normEleHigh = 0.012
+normEleHigh = 0.02
 
-targetMuLow = 0.015
-targetMuHigh = 0.02
-targetEleLow = 0.012
-targetEleHigh = 0.02
+targetMuLow = 0.02
+targetMuHigh = 10
+targetEleLow = 0.02
+targetEleHigh = 10
 
 
-#dataDrivenQCDFile = './condor/SignalSelectionsWithSF_Feb9th/QCDFromData.root'
-#dataDrivenQCDIPDistribution = 'SignalRegionSelectionNoIsoNoOSInclusiveDisplacedTriggerPlotter/Electron-muon-beamspot Plots/electronIpMuonIpInclusive'
 dataDrivenQCDFile = './condor/DisplacedControlRegionWithSF_Feb8th/QCDFromData.root'
-dataDrivenQCDIPDistribution = 'DisplacedControlRegionNoIsoNoOSInclusiveDisplacedTriggerPlotter/Electron-muon-beamspot Plots/electronIpMuonIpInclusive'
+dataDrivenQCDIPDistribution = 'EMuPreselectionInclusiveTriggerPlotter/Electron-muon-beamspot Plots/electronIpMuonIpInclusive'
