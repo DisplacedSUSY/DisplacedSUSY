d252335dcbdba731a6ce937c85a55e688d49ded7
bing <bingxuan.liu@cern.ch>
2016-06-28 14:56:11 -0400

    update qcd closure test scrip

--- BackgroundStudies/test/QCDClosureConfig.py
+++ BackgroundStudies/test/QCDClosureConfig.py
@@ -1,35 +1,130 @@
-bbbarMuFile = '/data/users/bing/condor/QCDMuon76X_IsoCorr_April11thPreApr/SingleMu_2015D.root'
-bbbarEleFile = '/data/users/bing/condor/QCDElectron76X_IsoCorr_April11thPreApr/SingleEle_2015D.root'
-#bbbarMuFile = '/data/users/bing/condor/AntiIsoDisplaced76X_IsoCorr_April11thPreApr/MuonEG_2015D.root'
-#bbbarEleFile = '/data/users/bing/condor/QCDElectron76X_IsoCorr_SinglePhoton_April11thPreApr/SinglePhoton_2015D.root'
-#bbbarEleFile = '/data/users/bing/condor/AntiIsoDisplaced76X_IsoCorr_April11thPreApr/MuonEG_2015D.root'
-bbbarMuIPShape = 'QCDMuonControlRegionPlotter/Muon-beamspot Plots/muonAbsD0BeamspotM'
-bbbarEleIPShape = 'QCDElectronControlRegionPlotter/Electron-beamspot Plots/electronAbsD0BeamspotM'
-#bbbarMuIPShape = 'AntiIsoElectronBlindedMuonDisplacedPlotter/Muon-beamspot Plots/muonAbsD0BeamspotM'
-#bbbarEleIPShape = 'AntiIsoMuonBlindedElectronDisplacedPlotter/Electron-beamspot Plots/electronAbsD0BeamspotM'
+bbbarEleFile = '/data/users/bing/condor/QCDElectronControlRegion_Run2PreApproval/MCSubtracted.root'
+bbbarMuFile = '/data/users/bing/condor/QCDMuonControlRegion_Run2PreApproval/SingleMu_2015D.root'
+bbbarMuIPShape = 'QCDMuonIsoControlRegionPlotter/Muon-beamspot Plots/muonAbsD0BeamspotM'
+bbbarEleIPShape = 'QCDElectronIsoControlRegionPlotter/Electron-beamspot Plots/electronAbsD0BeamspotM'
 
-normSource = 'EMuPreselection'
-
-dataNormFile = '/data/users/bing/condor/EMuPreselection76X_IsoCorr_April11thPreApr/MuonEG_2015D.root'
-bgNormFile = '/data/users/bing/condor/EMuPreselection76X_IsoCorr_April11thPreApr/Background.root'
-normIPDistribution = 'EMuPreselectionInclusiveTriggerPlotter/Electron-muon-beamspot Plots/electronIpMuonIpMedium'
+normSource = 'Displaced control region'
+dataNormFile = '/data/users/bing/condor/DisplacedControlRegion_Run2PreApproval/MuonEG_2015D.root'
+bgNormFile = '/data/users/bing/condor/DisplacedControlRegion_Run2PreApproval/Background.root'
+normIPDistribution = 'DisplacedControlRegionNoIsoNoOSInclusiveDisplacedTriggerPlotter/Electron-muon-beamspot Plots/electronIpMuonIpMedium'
 
 targetSource = 'Displaced control region'
 
-dataTargetFile = '/data/users/bing/condor/DisplacedControlRegion76X_IsoCorr_April11thPreApr/MuonEG_2015D.root'
-bgTargetFile = '/data/users/bing/condor/DisplacedControlRegion76X_IsoCorr_April11thPreApr/Background.root'
+dataTargetFile = '/data/users/bing/condor/DisplacedControlRegion_Run2PreApproval/MuonEG_2015D.root'
+bgTargetFile = '/data/users/bing/condor/DisplacedControlRegion_Run2PreApproval/Background.root'
 targetNormIPDistribution = 'DisplacedControlRegionNoIsoNoOSInclusiveDisplacedTriggerPlotter/Electron-muon-beamspot Plots/electronIpMuonIpMedium'
 
-normMuLow = 0.01
-normMuHigh = 0.02
-normEleLow = 0.01
-normEleHigh = 0.02
+regionMaps = {
+"regionMap1" : {
+"normMuLow" : 0.01,
+"normMuHigh" : 0.02,
+"normEleLow" : 0.01,
+"normEleHigh" : 0.02,
+"targetMuLow" : 0.02,
+"targetMuHigh" : 10,
+"targetEleLow" : 0.02,
+"targetEleHigh": 10,},
+"regionMap2" : {
+"normMuLow" : 0.01,
+"normMuHigh" : 0.02,
+"normEleLow" : 0.01,
+"normEleHigh" : 0.02,
+"targetMuLow" : 0.05,
+"targetMuHigh" : 10,
+"targetEleLow" : 0.05,
+"targetEleHigh": 10,},
+"regionMap3" : {
+"normMuLow" : 0.01,
+"normMuHigh" : 0.02,
+"normEleLow" : 0.01,
+"normEleHigh" : 0.02,
+"targetMuLow" : 0.1,
+"targetMuHigh" : 10,
+"targetEleLow" : 0.1,
+"targetEleHigh": 10,},
+}
+#regionMaps = {
+#"regionMap1" : {
+#"normMuLow" : 0.01,
+#"normMuHigh" : 0.02,
+#"normEleLow" : 0.01,
+#"normEleHigh" : 0.02,
+#"targetMuLow" : 0.012,
+#"targetMuHigh" : 0.02,
+#"targetEleLow" : 0.012,
+#"targetEleHigh": 0.02,},
+#"regionMap2" : {
+#"normMuLow" : 0.01,
+#"normMuHigh" : 0.02,
+#"normEleLow" : 0.01,
+#"normEleHigh" : 0.02,
+#"targetMuLow" : 0.015,
+#"targetMuHigh" : 0.02,
+#"targetEleLow" : 0.015,
+#"targetEleHigh": 0.02,},
+#"regionMap3" : {
+#"normMuLow" : 0.01,
+#"normMuHigh" : 0.02,
+#"normEleLow" : 0.01,
+#"normEleHigh" : 0.02,
+#"targetMuLow" : 0.018,
+#"targetMuHigh" : 0.02,
+#"targetEleLow" : 0.018,
+#"targetEleHigh": 0.02,},
+#"regionMap4" : {
+#"normMuLow" : 0.01,
+#"normMuHigh" : 0.02,
+#"normEleLow" : 0.01,
+#"normEleHigh" : 0.02,
+#"targetMuLow" : 0.012,
+#"targetMuHigh" : 0.02,
+#"targetEleLow" : 0.015,
+#"targetEleHigh": 0.02,},
+#"regionMap5" : {
+#"normMuLow" : 0.01,
+#"normMuHigh" : 0.02,
+#"normEleLow" : 0.01,
+#"normEleHigh" : 0.02,
+#"targetMuLow" : 0.012,
+#"targetMuHigh" : 0.02,
+#"targetEleLow" : 0.018,
+#"targetEleHigh": 0.02,},
+#"regionMap6" : {
+#"normMuLow" : 0.01,
+#"normMuHigh" : 0.02,
+#"normEleLow" : 0.01,
+#"normEleHigh" : 0.02,
+#"targetMuLow" : 0.015,
+#"targetMuHigh" : 0.02,
+#"targetEleLow" : 0.012,
+#"targetEleHigh": 0.02,},
+#"regionMap7" : {
+#"normMuLow" : 0.01,
+#"normMuHigh" : 0.02,
+#"normEleLow" : 0.01,
+#"normEleHigh" : 0.02,
+#"targetMuLow" : 0.015,
+#"targetMuHigh" : 0.02,
+#"targetEleLow" : 0.018,
+#"targetEleHigh": 0.02,},
+#"regionMap8" : {
+#"normMuLow" : 0.01,
+#"normMuHigh" : 0.02,
+#"normEleLow" : 0.01,
+#"normEleHigh" : 0.02,
+#"targetMuLow" : 0.018,
+#"targetMuHigh" : 0.02,
+#"targetEleLow" : 0.012,
+#"targetEleHigh": 0.02,},
+#"regionMap9" : {
+#"normMuLow" : 0.01,
+#"normMuHigh" : 0.02,
+#"normEleLow" : 0.01,
+#"normEleHigh" : 0.02,
+#"targetMuLow" : 0.018,
+#"targetMuHigh" : 0.02,
+#"targetEleLow" : 0.015,
+#"targetEleHigh": 0.02,},
+#}
 
-targetMuLow = 0.02
-targetMuHigh = 10
-targetEleLow = 0.02
-targetEleHigh = 10
-
-
-dataDrivenQCDFile = './condor/DisplacedControlRegionWithSF_Feb8th/QCDFromData.root'
-dataDrivenQCDIPDistribution = 'EMuPreselectionInclusiveTriggerPlotter/Electron-muon-beamspot Plots/electronIpMuonIpInclusive'
+performClosure = False
