c028f49631617ae7d599bbb7049e1c28897a6ddc
Jamie Antonelli <lantonel@cern.ch>
2016-11-02 10:57:03 -0400

    updated for 2016

--- StandardAnalysis/test/EMuSkimOptions.py
+++ StandardAnalysis/test/EMuSkimOptions.py
@@ -2,84 +2,42 @@
 
 # import the definitions of all the datasets on the T3
 from OSUT3Analysis.Configuration.configurationOptions import *
-from OSUT3Analysis.Configuration.miniAODV2_76X_Samples import *
+from OSUT3Analysis.Configuration.miniAODV2_80X_Samples import *
 
 # specify which config file to pass to cmsRun
 config_file = "EMuSkim_cfg.py"
 
+systematics_file = "DisplacedSUSY.Configuration.systematicsDefinitions"
+external_systematics_directory = "DisplacedSUSY/Configuration/data/"
+
 # choose luminosity used for MC normalization
-intLumi = 2613 # for 13 TeV Silver Json
+intLumi = 12900 # don't know yet...
 
 # create list of datasets to process
 datasets = [
-    #'Diboson',
-    #'WJetsToLNu',
-    #'TTJets_SingleLeptFromT',
-    #'DYJetsToLL_50',
-    #'SingleTop',
-    #'TTJets_Lept',
-    #'TTJets_DiLept',
-    #'QCD_MuEnriched',
-    #'MuonEG_2015D',
-    #'MET_2015D',
-    #'DisplacedSUSYSignal'
-    'stop300_30mm',
-    'stop300_70mm'
-    #'stop600_1mm',
-    #'stop700_1mm',
-    #'WZToLNu2QorQQ2L',
-    #'WG', 
-    #'WWToLNuLNu',
-    #'ZZToLLNuNu',
-    #'WWToLNuQQ',
-    #'ZZToLLQQ',
-    #'QCD_MuEnriched_170to300',
-    #'ZG',
-    #'WZToLLLNu',
-#    'stop200_1mm',  
-#    'stop200_10mm', 
-#    'stop200_100mm', 
-#    'stop200_1000mm',
-#    'stop300_1mm', 
-#    'stop300_10mm',
-#    'stop300_100mm',
-#    'stop300_1000mm',
-#    'stop400_1mm', 
-#    'stop400_10mm', 
-#    'stop400_100mm',
-#    'stop400_1000mm',
-#    'stop500_1mm', 
-#    'stop500_10mm',
-#    'stop500_100mm',
-     #'stop500_1000mm',
-#    'stop600_1mm', 
-#    'stop600_10mm',
-#    'stop600_100mm',
-#    'stop600_1000mm',
-#    'stop700_1mm', 
-#    'stop700_10mm', 
-#    'stop700_100mm',
-#    'stop700_1000mm',
-     #'stop800_1mm', 
-#    'stop800_10mm', 
-#    'stop800_100mm', 
-     #'stop800_1000mm',
-#    'stop900_1mm', 
-#    'stop900_10mm', 
-#    'stop900_100mm', 
-#    'stop900_1000mm',
-#    'stop1000_1mm', 
-#    'stop1000_10mm', 
-#    'stop1000_100mm',
-#    'stop1000_1000mm',
-#    'stop1100_1mm', 
-#    'stop1100_10mm', 
-#    'stop1100_100mm',
-#    'stop1100_1000mm',
-#    'stop1200_1mm',  
-#    'stop1200_10mm',  
-     #'stop1200_100mm', 
-#    'stop1200_1000mm',
+
+    # DY
+#    'DYJetsToLL_50',
+
+    # TTbar
+#    'TTJets_DiLept',
+
+    # tW
+#    'SingleTop_tbarW',
+
+    # Diboson
+#    'WWToLNuLNu',
+#    'ZZToLLNuNu',
+#    'ZG',
+
+    # QCD (mu-enriched is bigger)
+#    'QCD_MuEnriched',
+
+    # Data
+    'MuonEG_2016',
+    
+    
+
 ]
 
-InputCondorArguments = {'request_memory':'2048MB','hold':'true'}
+InputCondorArguments = {}
