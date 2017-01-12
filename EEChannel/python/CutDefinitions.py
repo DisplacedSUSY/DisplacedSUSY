09b746f85acb5b7fbc261fc1348409163d8b5cb0
Bryan Cardwell <cardwell.80@osu.edu>
2017-01-12 16:46:19 -0500

    Added CRII cuts

--- EEChannel/python/CutDefinitions.py
+++ EEChannel/python/CutDefinitions.py
@@ -72,6 +72,23 @@ electron_d0_lt100_cut = cms.PSet(
     alias = cms.string("electron d0 < 100 mum")
     )
 
+# electron d0 > 100 microns
+electron_d0_above100_cut = cms.PSet(
+    inputCollection = cms.vstring("electrons","beamspots"),
+    cutString = cms.string(objectDefs.electronAbsD0 + " > 100"),
+    numberRequired = cms.string(">= 2"),
+    alias = cms.string("electron d0 > 100 mum")
+    )
+
+# electron d0 < 200 microns
+electron_d0_below200_cut = cms.PSet(
+    inputCollection = cms.vstring("electrons","beamspots"),
+    cutString = cms.string(objectDefs.electronAbsD0 + " < 200"),
+    numberRequired = cms.string(">= 2"),
+    alias = cms.string("electron d0 < 200 mum")
+    )
+
+
 ##########################################################################
 
 # BEGIN MUON CUTS
