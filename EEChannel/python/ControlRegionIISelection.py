52baba05100a375e8534302922a403a7aaf214a0
Bryan Cardwell <cardwell.80@osu.edu>
2017-01-12 22:24:46 -0500

    First working setup

--- EEChannel/python/ControlRegionIISelection.py
+++ EEChannel/python/ControlRegionIISelection.py
@@ -1,7 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 import copy
 import string
-from DisplacedSUSY.MuMuChannel.CutDefinitions import *
+from DisplacedSUSY.EEChannel.CutDefinitions import *
 
 ##########################################################################
 ####### Set up control region II for the displaced SUSY analysis #########
@@ -16,15 +16,15 @@ ControlRegionII = cms.PSet(
     cuts = cms.VPSet()
 )
 ### jet selection (just for plotting purposes, doesn't make event cuts)
-PromptControlRegion.cuts.append(jet_eta_cut)
-PromptControlRegion.cuts.append(jet_pt_30_cut)
-PromptControlRegion.cuts.append(jet_id_cut)
+ControlRegionII.cuts.append(jet_eta_cut)
+ControlRegionII.cuts.append(jet_pt_30_cut)
+ControlRegionII.cuts.append(jet_id_cut)
 ### at least two good electrons
-PromptControlRegion.cuts.append(electron_eta_cut)
-PromptControlRegion.cuts.append(electron_gap_veto)
-PromptControlRegion.cuts.append(electron_pt_42_cut)
-PromptControlRegion.cuts.append(electron_id_cut)
-PromptControlRegion.cuts.append(electron_iso_cut)
+ControlRegionII.cuts.append(electron_eta_cut)
+ControlRegionII.cuts.append(electron_gap_veto)
+ControlRegionII.cuts.append(electron_pt_42_cut)
+ControlRegionII.cuts.append(electron_id_cut)
+ControlRegionII.cuts.append(electron_iso_cut)
 ### at least two good muons
 #PromptControlRegion.cuts.append(muon_eta_cut)
 #PromptControlRegion.cuts.append(muon_pt_40_cut)
@@ -35,6 +35,6 @@ PromptControlRegion.cuts.append(electron_iso_cut)
 #PromptControlRegion.cuts.append(electron_d0_lt100_cut)
 #PromptControlRegion.cuts.append(muon_d0_lt100_cut)
 ### require lepton d0 in CRII range
-PromptControlRegion.cuts.append(electron_d0_above100_cut)
-PromptControlRegion.cuts.append(electron_d0_below200_cut)
+ControlRegionII.cuts.append(electron_d0_above100_cut)
+ControlRegionII.cuts.append(electron_d0_below200_cut)
 
