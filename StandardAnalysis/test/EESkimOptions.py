d0672abab670c9f5b9f2f69c10156bbead24d2ef
Bryan Cardwell <cardwell.80@osu.edu>
2017-01-12 22:42:30 -0500

    Fixed lumi and datasets

--- StandardAnalysis/test/EESkimOptions.py
+++ StandardAnalysis/test/EESkimOptions.py
@@ -5,39 +5,44 @@ from OSUT3Analysis.Configuration.configurationOptions import *
 from OSUT3Analysis.Configuration.miniAODV2_80X_Samples import *
 
 # specify which config file to pass to cmsRun
-#config_file = "ZtoEEControlRegion_cfg.py"
 config_file = "EESkim_cfg.py"
+
+# choose luminosity used for MC normalization
+intLumi = 36460
 
 systematics_file = "DisplacedSUSY.Configuration.systematicsDefinitions"
 external_systematics_directory = "DisplacedSUSY/Configuration/data/"
-
-# choose luminosity used for MC normalization
-intLumi = 12900 # don't know yet...
 
 # create list of datasets to process
 datasets = [
 
     # DY
-    'DYJetsToLL_50',
+    'DYJetsToLL',
+    #'DYJetsToLL_50',
+    #'DYJetsToLL_10to50',
 
     # TTbar
     'TTJets_DiLept',
 
     # tW
-#    'SingleTop_tbarW',
+    #'SingleTop_tbarW',
+    'SingleTop',
 
     # Diboson
-#    'WWToLNuLNu',
-#    'ZZToLLNuNu',
-#    'ZG',
+    'Diboson',
+    #'WWToLNuLNu',
+    #'ZZToLLNuNu',
+    #'ZG',
 
     # QCD
-#    'QCD_EMEnriched',
-#    'QCD_bcToE',
-#    'QCD_DoubleEMEnriched',
-
+    'QCD_MuEnriched',
+    
+    # Signal
+    'DisplacedSUSYSignal',
+    
     # Data
-#    'DoubleEG_2016_23Sep',
+    'DoubleEG_2016_23Sep',
+    #'DoubleEG_2016H',
 
 ]
 
