acecb71752ed78f42142feae43791461536c54ee
Bryan Cardwell <cardwell.80@osu.edu>
2017-01-27 10:29:11 -0500

    First commit

---
+++ MuMuChannel/python/ZControlRegionSelection.py
@@ -0,0 +1,29 @@
+import FWCore.ParameterSet.Config as cms
+import copy
+import string
+from DisplacedSUSY.MuMuChannel.CutDefinitions import *
+
+##########################################################################
+### Set up the Z control region for the displaced SUSY analysis #####
+##########################################################################
+
+##########################################################################
+#Selections without triggers
+
+ZControlRegion = cms.PSet(
+    name = cms.string("ZControlRegion"),
+    triggers = cms.vstring(),
+    cuts = cms.VPSet()
+)
+### jet selection (just for plotting purposes, doesn't make event cuts)
+ZControlRegion.cuts.append(jet_eta_cut)
+ZControlRegion.cuts.append(jet_pt_30_cut)
+ZControlRegion.cuts.append(jet_id_cut)
+### at least two good muons
+ZControlRegion.cuts.append(muon_eta_cut)
+ZControlRegion.cuts.append(muon_pt_40_cut)
+ZControlRegion.cuts.append(muon_global_cut)
+ZControlRegion.cuts.append(muon_id_cut)
+ZControlRegion.cuts.append(muon_iso_cut)
+### invMass in Z range
+ZControlRegion.cuts.append(diMuon_Z_mass_cut)
