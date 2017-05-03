bb867df6ab75d080f4930f42f0e1c7326b34aabf
Jamie Antonelli <lantonel@cern.ch>
2017-05-03 16:25:11 -0400

    added deltaEta back into electron ID

--- Configuration/python/objectDefinitions.py
+++ Configuration/python/objectDefinitions.py
@@ -105,6 +105,7 @@ jet_id_alias = cms.string("jet ID against leptons")
 
 electron_id_cutstring = cms.string("(isEB & \
                             full5x5_sigmaIetaIeta < 0.00998 & \
+                            abs(deltaEtaSuperClusterTrackAtVtx) < 0.00308 &\
                             abs(deltaPhiSuperClusterTrackAtVtx) < 0.0816 & \
                             hadronicOverEm < 0.0597 & \
                             abs(1/ecalEnergy - eSuperClusterOverP/ecalEnergy) < 0.0129 & \
@@ -112,6 +113,7 @@ electron_id_cutstring = cms.string("(isEB & \
                             passConversionVeto) | \
                             (isEE & \
                             full5x5_sigmaIetaIeta < 0.0292 & \
+                            abs(deltaEtaSuperClusterTrackAtVtx) < 0.00605 &\
                             abs(deltaPhiSuperClusterTrackAtVtx) < 0.0394 & \
                             hadronicOverEm < 0.0641 & \
                             abs(1/ecalEnergy - eSuperClusterOverP/ecalEnergy) < 0.0129 & \
