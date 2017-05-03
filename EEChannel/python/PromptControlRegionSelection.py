ebc56dea9841727d8c324d87e8287c5dba69656f
Bryan Cardwell <cardwell.80@osu.edu>
2017-05-03 15:43:38 -0400

    Cosmetic changes

--- EEChannel/python/PromptControlRegionSelection.py
+++ EEChannel/python/PromptControlRegionSelection.py
@@ -7,8 +7,6 @@ from DisplacedSUSY.EEChannel.CutDefinitions import *
 ### Set up the prompt control region for the displaced SUSY analysis #####
 ##########################################################################
 
-##########################################################################
-
 PromptControlRegion = cms.PSet(
     name = cms.string("PromptControlRegion"),
     triggers = cms.vstring("HLT_Photon42_R9Id85_OR_CaloId24b40e_Iso50T80L_Photon25_AND_HE10_R9Id65_Eta2_Mass15_v"),
@@ -24,12 +22,6 @@ PromptControlRegion.cuts.append(electron_gap_veto)
 PromptControlRegion.cuts.append(electron_pt_42_cut)
 PromptControlRegion.cuts.append(electron_id_cut)
 PromptControlRegion.cuts.append(electron_iso_cut)
-### at least two good muons
-#PromptControlRegion.cuts.append(muon_eta_cut)
-#PromptControlRegion.cuts.append(muon_pt_40_cut)
-#PromptControlRegion.cuts.append(muon_global_cut)
-#PromptControlRegion.cuts.append(muon_id_cut)
-#PromptControlRegion.cuts.append(muon_iso_cut)
 ### require prompt leptons
 PromptControlRegion.cuts.append(electron_d0_lt100_cut)
 #PromptControlRegion.cuts.append(muon_d0_lt100_cut)
