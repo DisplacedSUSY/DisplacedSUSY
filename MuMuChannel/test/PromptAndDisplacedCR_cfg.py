5c0b1acc1125d0ea954148ec9647bea4d2207caa
Bryan Cardwell <cardwell.80@osu.edu>
2017-05-08 09:43:14 -0400

    First commit

---
+++ MuMuChannel/test/PromptAndDisplacedCR_cfg.py
@@ -0,0 +1,137 @@
+import FWCore.ParameterSet.Config as cms
+from OSUT3Analysis.Configuration.processingUtilities import *
+import OSUT3Analysis.DBTools.osusub_cfg as osusub
+from OSUT3Analysis.Configuration.configurationOptions import *
+from OSUT3Analysis.Configuration.LifetimeWeightProducer_cff import *
+
+import math
+import os
+
+################################################################################
+##### Set up the 'process' object ##############################################
+################################################################################
+
+process = cms.Process ('OSUAnalysis')
+
+# how often to print a log message
+process.load ('FWCore.MessageService.MessageLogger_cfi')
+process.MessageLogger.cerr.FwkReport.reportEvery = 100
+process.source = cms.Source ('PoolSource',
+  fileNames = cms.untracked.vstring (
+     'file:/store/user/bcardwell/MuMuSkim_17_02_03/DoubleMu_2016B/MuMuSkim/skim_0.root',
+  )
+)
+
+# output histogram file name when running interactively
+process.TFileService = cms.Service ('TFileService',
+    fileName = cms.string ('hist.root')
+)
+
+# suppress gen-matching erros
+process.load ('FWCore.MessageService.MessageLogger_cfi')
+process.MessageLogger.categories.append ("osu_GenMatchable")
+process.MessageLogger.cerr.osu_GenMatchable = cms.untracked.PSet(
+    limit = cms.untracked.int32 (0)
+)
+
+# number of events to process when running interactively
+process.maxEvents = cms.untracked.PSet (
+    input = cms.untracked.int32 (1000)
+)
+
+data_global_tag = '80X_dataRun2_2016SeptRepro_v3'
+mc_global_tag = '80X_mcRun2_asymptotic_2016_miniAODv2_v1'
+
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+from Configuration.AlCa.GlobalTag import GlobalTag
+process.GlobalTag = GlobalTag(process.GlobalTag, mc_global_tag, '')
+if osusub.batchMode and (osusub.datasetLabel in types) and (types[osusub.datasetLabel] == "data"):
+    print "using global tag " + data_global_tag + "..."
+    process.GlobalTag = GlobalTag(process.GlobalTag, data_global_tag, '')
+else:
+    print "using global tag " + mc_global_tag + "..."
+
+
+################################################################################
+##### Set up the 'collections' map #############################################
+################################################################################
+
+# this PSet specifies which collections to get from the input files
+miniAOD_collections = cms.PSet (
+  electrons       =  cms.InputTag  ('slimmedElectrons',''),
+  genjets         =  cms.InputTag  ('slimmedGenJets',                 ''),
+  jets            =  cms.InputTag  ('slimmedJets',                    ''),
+  bjets           =  cms.InputTag  ('slimmedJets',                    ''),
+  generatorweights=  cms.InputTag  ('generator', ''), 
+  mcparticles     =  cms.InputTag  ('packedGenParticles',             ''),
+  hardInteractionMcparticles  =  cms.InputTag  ('prunedGenParticles',             ''),
+  mets            =  cms.InputTag  ('slimmedMETs',                    ''),
+  muons           =  cms.InputTag  ('slimmedMuons',                   ''),
+  photons         =  cms.InputTag  ('slimmedPhotons',                 ''),
+  primaryvertexs  =  cms.InputTag  ('offlineSlimmedPrimaryVertices',  ''),
+  pileupinfos     =  cms.InputTag  ('slimmedAddPileupInfo',  ''),
+  beamspots       =  cms.InputTag  ('offlineBeamSpot',                ''),
+  superclusters   =  cms.InputTag  ('reducedEgamma',                  'reducedSuperClusters'),
+  taus            =  cms.InputTag  ('slimmedTaus',                    ''),
+  triggers        =  cms.InputTag  ('TriggerResults',                 '',  'HLT'),
+  trigobjs        =  cms.InputTag  ('selectedPatTrigger',             ''),
+)
+
+collections = miniAOD_collections
+
+################################################################################
+##### Set up any user-defined variable producers ###############################
+################################################################################
+
+variableProducers = []
+variableProducers.append('DisplacedSUSYEventVariableProducer')
+variableProducers.append('LifetimeWeightProducer')
+
+weights = cms.VPSet(
+    cms.PSet (
+        inputCollections = cms.vstring("eventvariables"),
+        inputVariable = cms.string("lifetimeWeight")
+    ),
+)
+
+scalingfactorproducers = []
+
+################################################################################
+##### Import the channels to be run ############################################
+################################################################################
+
+from DisplacedSUSY.MuMuChannel.PromptControlRegionSelection import *
+from DisplacedSUSY.MuMuChannel.DisplacedControlRegionSelection import *
+
+eventSelections = [PromptControlRegion, DisplacedControlRegion]
+
+################################################################################
+##### Import the histograms to be plotted ######################################
+################################################################################
+
+from OSUT3Analysis.Configuration.histogramDefinitions import MuonHistograms, DiMuonHistograms 
+from DisplacedSUSY.Configuration.histogramDefinitions import MuonD0Histograms, BeamspotHistograms
+from OSUT3Analysis.Configuration.histogramDefinitions import JetHistograms, MuonJetHistograms
+from OSUT3Analysis.Configuration.histogramDefinitions import MetHistograms, MuonMetHistograms
+from DisplacedSUSY.Configuration.histogramDefinitions import eventHistograms
+
+histograms = cms.VPSet()
+histograms.append(MuonHistograms)
+histograms.append(DiMuonHistograms)
+histograms.append(MuonD0Histograms)
+histograms.append(BeamspotHistograms)
+histograms.append(JetHistograms)
+histograms.append(MuonJetHistograms)
+histograms.append(MetHistograms)
+histograms.append(MuonMetHistograms)
+histograms.append(eventHistograms)
+
+################################################################################
+##### Attach the channels and histograms to the process ########################
+################################################################################
+
+add_channels (process, eventSelections, histograms, weights, scalingfactorproducers, collections, variableProducers, False)
+
+process.DisplacedSUSYEventVariableProducer.type = cms.string("data")
+#process.DisplacedSUSYEventVariableProducer.triggerPath = cms.string("")
+#process.DisplacedSUSYEventVariableProducer.triggerScalingFactor = cms.double(1.0)
