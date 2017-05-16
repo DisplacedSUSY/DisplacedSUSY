c4285a81102699a5e3c7645594a90273439ea58a
Bryan Cardwell <cardwell.80@osu.edu>
2017-05-16 15:39:49 -0400

    First commit

---
+++ EMuChannel/test/signalRegionYieldsTableConfig.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python
+
+from array import array
+
+systematic_error = 0.0 # testing placeholder - should be irrelavent for now
+
+channel = "PreselectionPlotter"
+d0histogramName = "Electron-muon-beamspot Plots/electronAbsD0_vs_muonAbsD0_500um"
+d0cuts_array = array('d', [0.1])
+
+datasets = ['Diboson']
+
+
+
+
+
