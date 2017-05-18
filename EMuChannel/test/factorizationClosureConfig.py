f9dba969be41b36928de397f7ff3c1d8ad76e18f
Bryan Cardwell <cardwell.80@osu.edu>
2017-05-17 20:03:19 -0400

    Changed data format

--- EMuChannel/test/factorizationClosureConfig.py
+++ EMuChannel/test/factorizationClosureConfig.py
@@ -1,20 +1,14 @@
 #! /usr/bin/env python
 
-input_sources = [
-    {
-      'dataset'  : 'DYJetsToLL_50',
-      'histPath' : 'PreselectionPlotter/Electron-muon-beamspot Plots/electronAbsD0_vs_muonAbsD0_500um',
-    },
-    {
-      'dataset'  : 'Diboson',
-      'histPath' : 'PreselectionPlotter/Electron-muon-beamspot Plots/electronAbsD0_vs_muonAbsD0_500um',
-    },
-    {
-      'dataset'  : 'SingleTop',
-      'histPath' : 'PreselectionPlotter/Electron-muon-beamspot Plots/electronAbsD0_vs_muonAbsD0_500um',
-    },
-    {
-      'dataset'  : 'TTJets_DiLept',
-      'histPath' : 'PreselectionPlotter/Electron-muon-beamspot Plots/electronAbsD0_vs_muonAbsD0_500um',
-    },
-]
+histPath = 'PreselectionPlotter/Electron-muon-beamspot Plots/electronAbsD0_vs_muonAbsD0_500um'
+
+datasets = [
+            'WWToLNuLNu',
+            'WWToLNuQQ',
+            'WZToLLLNu',
+            'WZToLNuNuNu',
+            'ZZToLLLL',
+            'ZZToLLNuNu',
+            'ZZToLLQQ',
+            'ZZToNuNuQQ',
+           ]
