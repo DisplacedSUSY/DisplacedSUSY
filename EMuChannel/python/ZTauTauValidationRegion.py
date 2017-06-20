7b5f9eef9626ace9931be201b941370cc91da3cf
Jamie Antonelli <lantonel@cern.ch>
2017-06-20 15:39:18 -0400

    tau->Tau

--- EMuChannel/python/ZTauTauValidationRegion.py
+++ EMuChannel/python/ZTauTauValidationRegion.py
@@ -4,7 +4,7 @@ import string
 from DisplacedSUSY.EMuChannel.CutDefinitions import *
 
 ##########################################################################
-### Set up the Ztautau validation region
+### Set up the ZTauTau validation region
 ##########################################################################
 
 # Prompt Control Region with lepton-met mass < 50 and dilepton mass < 100
@@ -12,33 +12,33 @@ from DisplacedSUSY.EMuChannel.CutDefinitions import *
 ##########################################################################
 #Selections without triggers
 
-ZtautauValidationRegion = cms.PSet(
-    name = cms.string("ZtautauValidationRegion"),
-    triggers = cms.vstring(),
+ZTauTauValidationRegion = cms.PSet(
+    name = cms.string("ZTauTauValidationRegion"),
+    triggers = cms.vstring("HLT_Mu38NoFiltersNoVtx_Photon38_CaloIdL_v"),
     cuts = cms.VPSet()
 )
 ### jet selection (just for plotting purposes, doesn't make event cuts)
-ZtautauValidationRegion.cuts.append(jet_eta_cut)
-ZtautauValidationRegion.cuts.append(jet_pt_30_cut)
-ZtautauValidationRegion.cuts.append(jet_id_cut)
+ZTauTauValidationRegion.cuts.append(jet_eta_cut)
+ZTauTauValidationRegion.cuts.append(jet_pt_30_cut)
+ZTauTauValidationRegion.cuts.append(jet_id_cut)
 ### at least one good electron
-ZtautauValidationRegion.cuts.append(electron_eta_cut)
-ZtautauValidationRegion.cuts.append(electron_gap_veto)
-ZtautauValidationRegion.cuts.append(electron_pt_42_cut)
-ZtautauValidationRegion.cuts.append(electron_id_cut)
-ZtautauValidationRegion.cuts.append(electron_iso_cut)
+ZTauTauValidationRegion.cuts.append(electron_eta_cut)
+ZTauTauValidationRegion.cuts.append(electron_gap_veto)
+ZTauTauValidationRegion.cuts.append(electron_pt_42_cut)
+ZTauTauValidationRegion.cuts.append(electron_id_cut)
+ZTauTauValidationRegion.cuts.append(electron_iso_cut)
 ### at least one good muon
-ZtautauValidationRegion.cuts.append(muon_eta_cut)
-ZtautauValidationRegion.cuts.append(muon_pt_40_cut)
-ZtautauValidationRegion.cuts.append(muon_global_cut)
-ZtautauValidationRegion.cuts.append(muon_id_cut)
-ZtautauValidationRegion.cuts.append(muon_iso_cut)
+ZTauTauValidationRegion.cuts.append(muon_eta_cut)
+ZTauTauValidationRegion.cuts.append(muon_pt_40_cut)
+ZTauTauValidationRegion.cuts.append(muon_global_cut)
+ZTauTauValidationRegion.cuts.append(muon_id_cut)
+ZTauTauValidationRegion.cuts.append(muon_iso_cut)
 ### require prompt leptons
-ZtautauValidationRegion.cuts.append(electron_d0_lt100_cut)
-ZtautauValidationRegion.cuts.append(muon_d0_lt100_cut)
+ZTauTauValidationRegion.cuts.append(electron_d0_lt100_cut)
+ZTauTauValidationRegion.cuts.append(muon_d0_lt100_cut)
 ### ztautau-enriching cuts
-ZtautauValidationRegion.cuts.append(electron_mt_cut)
-ZtautauValidationRegion.cuts.append(muon_mt_cut)
-ZtautauValidationRegion.cuts.append(emu_mass_lt100_cut)
-ZtautauValidationRegion.cuts.append(jet_btag_lwp_veto)
+ZTauTauValidationRegion.cuts.append(electron_mt_cut)
+ZTauTauValidationRegion.cuts.append(muon_mt_cut)
+ZTauTauValidationRegion.cuts.append(emu_mass_lt100_cut)
+ZTauTauValidationRegion.cuts.append(jet_btag_lwp_veto)
 
