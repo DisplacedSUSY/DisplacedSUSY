958ddd4d9d29cee38786ca9e7713c731750308c1
Bryan Cardwell <cardwell.80@osu.edu>
2017-06-20 14:25:37 -0400

    Added cuts for ttbar cross-section paper selection

--- EMuChannel/python/CutDefinitions.py
+++ EMuChannel/python/CutDefinitions.py
@@ -20,6 +20,20 @@ jet_pt_30_cut = cms.PSet(
     numberRequired = cms.string(">= 0")
     )
 
+jet_eta_real_cut = cms.PSet(
+    inputCollection = cms.vstring("jets"),
+    cutString = cms.string("abs(eta) < 2.4"),
+    numberRequired = cms.string(">= 2"),
+    alias = cms.string('>= 2 jets w/ eta < 2.4')
+    )
+
+jet_pt_30_real_cut = cms.PSet(
+    inputCollection = cms.vstring("jets"),
+    cutString = cms.string("pt > 30"),
+    numberRequired = cms.string(">= 2"),
+    alias = cms.string('>= 2 jets w/ pT > 30GeV')
+    )
+
 jet_id_cut = cms.PSet(
     inputCollection = cms.vstring("jets"),
     cutString = objectDefs.jet_id_cutstring,
@@ -27,11 +41,39 @@ jet_id_cut = cms.PSet(
     alias = objectDefs.jet_id_alias
     )
 
+jet_loose_id_cut = cms.PSet(
+    inputCollection = cms.vstring("jets"),
+    cutString = objectDefs.jet_loose_id_cutstring,
+    numberRequired = cms.string(">= 2"),
+    alias = objectDefs.jet_loose_id_alias
+    )
+
+jet_btag_twp_cut = cms.PSet (
+        inputCollection = cms.vstring("jets"),
+        cutString = cms.string("pfCombinedInclusiveSecondaryVertexV2BJetTags > 0.935"),
+        numberRequired = cms.string(">= 1"),
+        alias = cms.string('>= 1 tight b tags')
+    )
+
 jet_btag_mwp_cut = cms.PSet (
         inputCollection = cms.vstring("jets"),
         cutString = cms.string("pfCombinedInclusiveSecondaryVertexV2BJetTags > 0.800"),
         numberRequired = cms.string(">= 1"),
         alias = cms.string('>= 1 medium b tags')
+    )
+
+jet_btag_2_mwp_cut = cms.PSet (
+        inputCollection = cms.vstring("jets"),
+        cutString = cms.string("pfCombinedInclusiveSecondaryVertexV2BJetTags > 0.800"),
+        numberRequired = cms.string(">= 2"),
+        alias = cms.string('>= 2 medium b tags')
+    )
+
+jet_btag_lwp_cut = cms.PSet (
+        inputCollection = cms.vstring("jets"),
+        cutString = cms.string("pfCombinedInclusiveSecondaryVertexV2BJetTags > 0.5426"),
+        numberRequired = cms.string(">= 1"),
+        alias = cms.string('>= 1 loose b tags')
     )
 
 jet_btag_2_lwp_cut = cms.PSet (
@@ -54,6 +96,20 @@ jet_2jet_veto = cms.PSet (
         cutString = cms.string("pt > -1"),
         numberRequired = cms.string("< 2"),
         alias = cms.string('< 2 jets')
+    )
+
+jet_muon_deltaR_cut = cms.PSet (
+        inputCollection = cms.vstring("jets", "muons"),
+        cutString = cms.string("deltaR(jet, muon) > 0.4"),
+        numberRequired = cms.string(">= 2"),
+        alias = cms.string('separated jet-mu pair')
+    )
+
+jet_electron_deltaR_cut = cms.PSet (
+        inputCollection = cms.vstring("jets", "electrons"),
+        cutString = cms.string("deltaR(jet, electron) > 0.4"),
+        numberRequired = cms.string(">= 2"),
+        alias = cms.string('separated jet-e pair')
     )
 
 ##########################################################################
@@ -90,6 +146,13 @@ electron_id_cut = cms.PSet(
     cutString = objectDefs.electron_id_cutstring,
     numberRequired = cms.string(">= 1"),
     alias = objectDefs.electron_id_alias
+    )
+
+electron_ttbar_paper_id_cut = cms.PSet(
+    inputCollection = cms.vstring("electrons"),
+    cutString = objectDefs.electron_id_cutstring,
+    numberRequired = cms.string(">= 1"),
+    alias = objectDefs.electron_ttbar_paper_id_alias
     )
 
 electron_iso_cut = cms.PSet(
@@ -144,6 +207,13 @@ electron_mt_cut = cms.PSet (
     alias = cms.string("electron m_{T} < 50 GeV")
     )
 
+electron_num_exactly_1_cut = cms.PSet(
+    inputCollection = cms.vstring("electrons"),
+    cutString = cms.string("pt > 0"),
+    numberRequired = cms.string(">= 1"),
+    alias = cms.string("exactly 1 electron")
+    )
+
 ##########################################################################
 
 # BEGIN MUON CUTS
@@ -178,6 +248,20 @@ muon_id_cut = cms.PSet(
     cutString = objectDefs.muon_id_cutstring,
     numberRequired = cms.string(">= 1"),
     alias = objectDefs.muon_id_alias
+    )
+
+muon_d0_below2000_cut = cms.PSet(
+    inputCollection = cms.vstring("muons","beamspots"),
+    cutString = cms.string(objectDefs.muonAbsD0_cm + " < 0.2"),
+    numberRequired = cms.string(">= 1"),
+    alias = cms.string("muon d0 < 0.2 cm")
+    )
+
+muon_dZ_below5000_cut = cms.PSet(
+    inputCollection = cms.vstring("muons","beamspots"),
+    cutString = cms.string(objectDefs.muonAbsDz + " < 5000"),
+    numberRequired = cms.string(">= 1"),
+    alias = cms.string("muon dZ < 0.5 cm")
     )
 
 muon_iso_cut = cms.PSet(
@@ -232,10 +316,31 @@ muon_mt_cut = cms.PSet (
     alias = cms.string("muon m_{T} < 50 GeV")
     )
 
+muon_num_exactly_1_cut = cms.PSet(
+    inputCollection = cms.vstring("muons"),
+    cutString = cms.string("pt > 0"),
+    numberRequired = cms.string(">= 1"),
+    alias = cms.string("exactly 1 muon")
+    )
+
 
 ##########################################################################
 
 # BEGIN ELECTRON-MUON CUTS
+
+emu_mass_20_cut = cms.PSet (
+    inputCollection = cms.vstring("electrons", "muons"),
+    cutString = cms.string("invMass(electron,muon) > 20"),
+    numberRequired = cms.string(">= 1"),
+    alias = cms.string("invariant mass < 20 GeV")
+    )
+
+emu_opposite_charge_cut = cms.PSet (
+    inputCollection = cms.vstring("electrons", "muons"),
+    cutString = cms.string("electron.charge * muon.charge < 0"),
+    numberRequired = cms.string(">= 1"),
+    alias = cms.string("oppositely-charged e-mu pair")
+    )
 
 emu_mass_lt100_cut = cms.PSet (
     inputCollection = cms.vstring("electrons", "muons"),
@@ -255,6 +360,12 @@ emu_pt_gt50_cut = cms.PSet (
 ##########################################################################
 
 # BEGIN MET CUTS
+
+met_pt_40_cut = cms.PSet(
+    inputCollection = cms.vstring("mets"),
+    cutString = cms.string("pt > 40"),
+    numberRequired = cms.string(">= 1")
+    )
 
 met_gt60_cut =  cms.PSet (
     inputCollection = cms.vstring("mets"),
