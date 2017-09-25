14e4d4cb6e993cb081f415a4b44af0eacfaa93eb
Bryan Cardwell <cardwell.80@osu.edu>
2017-09-25 17:09:48 -0400

    Initial commit

---
+++ EMuChannel/python/TTbarForTrigEff.py
@@ -0,0 +1,50 @@
+import FWCore.ParameterSet.Config as cms
+import copy
+import string
+from DisplacedSUSY.EMuChannel.CutDefinitions import *
+
+##########################################################################
+### Set up the TTbar regions for trigger efficiency plots
+##########################################################################
+
+# Opposite sign e-mu pair with >=2 jets and >= 1 b jet
+# Designed to mimic the selection Bing used for trigger efficiency
+
+##########################################################################
+
+TTbarForTrigEffNoTrig = cms.PSet(
+    name = cms.string("TTbarForTrigEffNoTrig"),
+    triggers = cms.vstring(),
+    cuts = cms.VPSet()
+)
+### at least one good electron
+TTbarForTrigEffNoTrig.cuts.append(electron_eta_cut)
+TTbarForTrigEffNoTrig.cuts.append(electron_gap_veto)
+TTbarForTrigEffNoTrig.cuts.append(electron_pt_25_cut)
+TTbarForTrigEffNoTrig.cuts.append(electron_id_cut)
+TTbarForTrigEffNoTrig.cuts.append(electron_iso_cut)
+### at least one good muon
+TTbarForTrigEffNoTrig.cuts.append(muon_eta_cut)
+TTbarForTrigEffNoTrig.cuts.append(muon_pt_25_cut)
+TTbarForTrigEffNoTrig.cuts.append(muon_global_cut)
+TTbarForTrigEffNoTrig.cuts.append(muon_id_cut)
+TTbarForTrigEffNoTrig.cuts.append(muon_iso_cut)
+### good electron, muon pair
+TTbarForTrigEffNoTrig.cuts.append(emu_opposite_charge_cut)
+TTbarForTrigEffNoTrig.cuts.append(emu_deltaR_cut)
+### two good jets, one medium b jet
+TTbarForTrigEffNoTrig.cuts.append(jet_eta_real_cut)
+TTbarForTrigEffNoTrig.cuts.append(jet_pt_30_real_cut)
+TTbarForTrigEffNoTrig.cuts.append(jet_id_real_cut)
+TTbarForTrigEffNoTrig.cuts.append(jet_lepton_cleaning_cut)
+TTbarForTrigEffNoTrig.cuts.append(jet_btag_mwp_cut)
+### extra lepton vetos
+TTbarForTrigEffNoTrig.cuts.append(electron_num_exactly_1_cut)
+TTbarForTrigEffNoTrig.cuts.append(muon_num_exactly_1_cut)
+
+
+TTbarForTrigEff = cms.PSet(
+    name = cms.string("TTbarForTrigEff"),
+    triggers = cms.vstring("HLT_Mu38NoFiltersNoVtx_Photon38_CaloIdL_v"),
+    cuts = cms.VPSet(copy.deepcopy(TTbarForTrigEffNoTrig.cuts))
+)
