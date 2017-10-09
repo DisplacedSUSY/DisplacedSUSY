8f97b63b9b662614d8af18d29c42f83fe8147e7a
Bryan Cardwell <cardwell.80@osu.edu>
2017-10-09 18:32:13 -0400

    Add tag muon and trigger veto channels

--- MuMuChannel/python/TTbarForTrigEff.py
+++ MuMuChannel/python/TTbarForTrigEff.py
@@ -36,6 +36,7 @@ TTbarForTrigEffNoTrig.cuts.append(jet_btag_mwp_cut)
 TTbarForTrigEffNoTrig.cuts.append(muon_2muon_cut)
 
 
+### Add triggers ###
 TTbarForTrigEff43 = cms.PSet(
     name = cms.string("TTbarForTrigEff43"),
     triggers = cms.vstring("HLT_DoubleMu43NoFiltersNoVtx_v"),
@@ -47,3 +48,42 @@ TTbarForTrigEff48 = cms.PSet(
     triggers = cms.vstring("HLT_DoubleMu48NoFiltersNoVtx_v"),
     cuts = cms.VPSet(copy.deepcopy(TTbarForTrigEffNoTrig.cuts))
 )
+
+
+### Veto triggers ###
+TTbarForTrigEffVeto43 = cms.PSet(
+    name = cms.string("TTbarForTrigEffVeto43"),
+    triggers = cms.vstring(),
+    triggersToVeto = cms.vstring("HLT_DoubleMu43NoFiltersNoVtx_v"),
+    cuts = cms.VPSet(copy.deepcopy(TTbarForTrigEffNoTrig.cuts))
+)
+TTbarForTrigEffVeto43.cuts.append(muon_pt_70_cut)
+
+
+### Use tag muon eventvariable
+TTbarForTrigEffTagMuonNoTrig = cms.PSet(
+    name = cms.string("TTbarForTrigEffTagMuonNoTrig"),
+    triggers = cms.vstring(),
+    cuts = cms.VPSet(copy.deepcopy(TTbarForTrigEffNoTrig.cuts))
+)
+for cut in TTbarForTrigEffTagMuonNoTrig.cuts:
+    if "muon.charge * muon.charge < 0" in str(cut.cutString):
+        TTbarForTrigEffTagMuonNoTrig.cuts.remove(cut)
+    if "deltaR(muon, muon) > 0.5" in str(cut.cutString):
+        TTbarForTrigEffTagMuonNoTrig.cuts.remove(cut)
+TTbarForTrigEffTagMuonNoTrig.cuts.append(tagMuonExists_cut)
+TTbarForTrigEffTagMuonNoTrig.cuts.append(muon_opposite_charge_from_tag_cut)
+TTbarForTrigEffTagMuonNoTrig.cuts.append(muon_deltaR_from_tag_cut)
+
+TTbarForTrigEffTagMuon43 = cms.PSet(
+    name = cms.string("TTbarForTrigEffTagMuon43"),
+    triggers = cms.vstring("HLT_DoubleMu43NoFiltersNoVtx_v"),
+    cuts = cms.VPSet(copy.deepcopy(TTbarForTrigEffTagMuonNoTrig.cuts))
+)
+
+TTbarForTrigEffTagMuon48 = cms.PSet(
+    name = cms.string("TTbarForTrigEffTagMuon48"),
+    triggers = cms.vstring("HLT_DoubleMu48NoFiltersNoVtx_v"),
+    cuts = cms.VPSet(copy.deepcopy(TTbarForTrigEffTagMuonNoTrig.cuts))
+)
+
