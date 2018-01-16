8b64620200f0ea670910fc2f995453ed58d8b02d
Bryan Cardwell <cardwell.80@osu.edu>
2018-01-16 11:54:46 -0500

    Use leading lepton d0 distributions

--- BackgroundStudies/test/estimateBackground_cfg.py
+++ BackgroundStudies/test/estimateBackground_cfg.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python
 
 # output info
-channel = "SidebandFit"
+channel = "BackgroundEstimate"
 emu_dir = "Electron-muon Plots"
 fit_dir = "Sideband Fits"
 th2_name = "electronAbsD0_vs_muonAbsD0_10cm"
 regions = {
     # "name"  : (low, high, num_bins) note: bin edges must line up with edges in input hist
-    "prompt"  : (0.00,  0.02, 1),
-    "signal1" : (0.02,  0.05, 1),
-    "signal2" : (0.05,  0.10, 1),
-    "signal3" : (0.10, 10.00, 1),
+    "prompt"  : (0.00,   0.02,  1),
+    "signal1" : (0.02,   0.05,  1),
+    "signal2" : (0.05,   0.10,  1),
+    "signal3" : (0.10,  10.00,  1),
 }
 
 # input info
@@ -23,13 +23,13 @@ sidebands = [
             "name" : "Prompt Electron Background",
         },
         "displaced" : {
-            "file" : "/data/users/bcardwell/condor/QCDElectronCR_17_11_02/SingleEle_2016_postHIP.root",
+            "file" : "/data/users/bcardwell/condor/QCDElectronCR_El27_17_12_12/SingleEle_2016_postHIP.root",
             "hist" : "QCDElectronControlRegionPlotter/Electron Plots/electronAbsD0_10cm",
             "name" : "Displaced Electron Background",
         },
         "sideband" : {
-            "file" : "/data/users/bcardwell/condor/EMu_Sidebands_17_11_03/MuonEG_2016_postHIP.root",
-            "hist" : "PreselectionPromptMuonPlotter/Electron Plots/electronAbsD0_10cm",
+            "file" : "/data/users/bcardwell/condor/EMu_Sidebands_18_01_15/MuonEG_2016_postHIP.root",
+            "hist" : "PreselectionPromptMuonPlotter/Electron Plots/electronLeadingAbsD0_10cm",
             "name" : "Electron Sideband",
         },
     },
@@ -46,8 +46,8 @@ sidebands = [
             "name" : "Displaced Muon Background",
         },
         "sideband" : {
-            "file" : "/data/users/bcardwell/condor/EMu_Sidebands_17_11_03/MuonEG_2016_postHIP.root",
-            "hist" : "PreselectionPromptElectronPlotter/Muon Plots/muonAbsD0_10cm",
+            "file" : "/data/users/bcardwell/condor/EMu_Sidebands_18_01_15/MuonEG_2016_postHIP.root",
+            "hist" : "PreselectionPromptElectronPlotter/Muon Plots/muonLeadingAbsD0_10cm",
             "name" : "Muon Sideband",
         },
     },
