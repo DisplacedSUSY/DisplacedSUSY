57b3c8f3709e9d8b7a46d6ca4dda820f83429206
Bryan Cardwell <cardwell.80@osu.edu>
2018-01-29 14:00:52 -0500

    Print error and add option for rebin

--- TriggerEfficiency/script/makeSimpleEfficiencyPlots.py
+++ TriggerEfficiency/script/makeSimpleEfficiencyPlots.py
@@ -2,12 +2,15 @@
 import sys
 import os
 import re
+import math
 from array import array
 from optparse import OptionParser
 from ROOT import TFile, TCanvas, gPad, gROOT, TH1, TGraphAsymmErrors, TMultiGraph, TLegend, Double
 
 parser = OptionParser()
 parser.add_option("-l", "--localConfig", dest="localConfig",
+                  help="local configuration file")
+parser.add_option("-r", "--rebin", action="store_true", dest="rebin", default=False,
                   help="local configuration file")
 
 (arguments, args) = parser.parse_args()
@@ -18,45 +21,70 @@ if arguments.localConfig:
 
 gROOT.SetBatch()
 
+# copypasta from fitSidebands
+def propagateError(func, a, a_err, b, b_err):
+    a = float(a)
+    b = float(b)
+    if func is "sum":
+        return (a + b, math.sqrt(a_err**2 + b_err**2))
+    elif func is "product":
+        return (a*b, a*b * math.sqrt((a_err/a)**2 + (b_err/b)**2))
+    elif func is "quotient":
+        return (a/b, a/b * math.sqrt((a_err/a)**2 + (b_err/b)**2))
+    else:
+        print "Unrecognized function"
+        return -1
+
+out_file = TFile(output_file_name, "recreate")
+
 for plot in plots:
     combined_plot = TMultiGraph();
     combined_plot.SetTitle(";"+plot["x_axis_title"]+";Efficiency")
     legend = TLegend(0.35,0.35,0.85,0.5)
-    efficiencies = []
+    eff_and_err = {}
 
     for pair in plot["hist_pairs"]:
-        input_file = TFile(pair["input_file"])
-        pass_hist = input_file.Get(pair["pass_hist"]).Clone()
-        total_hist = input_file.Get(pair["total_hist"]).Clone()
+        in_file = TFile(pair["input_file"])
+        pass_hist = in_file.Get(pair["pass_hist"]).Clone()
+        total_hist = in_file.Get(pair["total_hist"]).Clone()
 
         bins_array = array('d',plot["bins"])
         rebinned_pass  = pass_hist.Rebin(len(bins_array)-1, "Rebinned pass hist", bins_array)
         rebinned_total = total_hist.Rebin(len(bins_array)-1, "Rebinned total hist", bins_array)
 
-        eff_plot = TGraphAsymmErrors(rebinned_pass, rebinned_total)
+        if arguments.rebin:
+            eff_plot = TGraphAsymmErrors(rebinned_pass, rebinned_total)
+        else:
+            eff_plot = TGraphAsymmErrors(pass_hist, total_hist)
         eff_plot.SetLineColor(pair["color"])
         eff_plot.SetLineWidth(1)
         eff_plot.SetMarkerStyle(21)
         eff_plot.SetMarkerColor(pair["color"])
         eff_plot.SetMarkerSize(1)
 
-        # Record efficiency of last bin
+        # Record efficiency and error of last bin
         x, y = Double(0.0), Double(0.0)
         eff_plot.GetPoint(eff_plot.GetN()-1, x, y)
-        efficiencies.append(y)
+        y_err = eff_plot.GetErrorY(eff_plot.GetN()-1)
+        eff_and_err[pair["type"]] = {"eff" : y, "err" : y_err}
 
         combined_plot.Add(eff_plot)
         legend.AddEntry(eff_plot, pair["label"])
 
-    print plot["channel"]
-    print efficiencies[0]/efficiencies[1]
+    (sf, sf_err) = propagateError("quotient", eff_and_err['data']["eff"], eff_and_err['data']["err"],
+                                              eff_and_err['mc']["eff"], eff_and_err['mc']["err"])
+    print plot["channel"], sf, sf_err
+
     canvas = TCanvas(plot["channel"], plot["channel"], 700, 700)
     combined_plot.Draw("ALP")
     combined_plot.SetMinimum(0.)
     combined_plot.SetMaximum(1.)
     legend.Draw()
     canvas.Update()
-    canvas.SaveAs(plot["channel"]+".png")
+    #canvas.SaveAs(plot["channel"]+".png")
+    out_file.cd()
+    canvas.Write()
 
-input_file.Close()
+in_file.Close()
+out_file.Close()
 
