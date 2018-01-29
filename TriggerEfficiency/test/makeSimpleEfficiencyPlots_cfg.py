6dd8f586c6e49966596b4cc389f66cc5e2d2f8b5
Bryan Cardwell <cardwell.80@osu.edu>
2018-01-29 14:06:51 -0500

    Update inputs to use MET trigger

--- TriggerEfficiency/test/makeSimpleEfficiencyPlots_cfg.py
+++ TriggerEfficiency/test/makeSimpleEfficiencyPlots_cfg.py
@@ -1,95 +1,98 @@
 import numpy
 output_file_name = 'trigEff_pT.root'
-#bins_array = numpy.geomspace(20,300,20)
 
 plots = [
     {
     "channel"      : "emu_trig_eff_vs_e_pt",
     "x_axis_title" : 'Electron p_{T} [GeV]',
-    "bins"         : [20,30,35,37,42,50,60,100,180,300],
+    "bins"         : [20,25,30,35,40,80,120,300],
     "hist_pairs"   :
         [
             {
-            "input_file"     : '/data/users/bcardwell/condor/EMu_TrigEfficiency2016_17_10_13/JetHT_2016_postHIP.root',
+            "input_file"     : '/data/users/bcardwell/condor/EMu_TrigEfficiency2016_MET_18_01_25/MET_2016_postHIP.root',
             "pass_hist"      : 'TTbarForTrigEff38HighPtMuPlotter/Electron Plots/electronPt',
-            "total_hist"     : 'TTbarForTrigEffNoTrigHighPtMuPlotter/Electron Plots/electronPt',
+            "total_hist"     : 'TTbarForTrigEffHighPtMuPlotter/Electron Plots/electronPt',
             "color"          : 1,
-            "label"          : 'JetHT_2016 G and H',
+            "label"          : 'MET_2016 G and H',
+            "type"           : 'data',
             },
             {
-            "input_file"     : '/data/users/bcardwell/condor/EMu_TrigEfficiency2016_17_10_13/TTJets_Lept.root',
+            "input_file"     : '/data/users/bcardwell/condor/EMu_TrigEfficiency2016_MET_18_01_25/TTJets_DiLept.root',
             "pass_hist"      : 'TTbarForTrigEff38HighPtMuPlotter/Electron Plots/electronPt',
-            "total_hist"     : 'TTbarForTrigEffNoTrigHighPtMuPlotter/Electron Plots/electronPt',
+            "total_hist"     : 'TTbarForTrigEffHighPtMuPlotter/Electron Plots/electronPt',
             "color"          : 2,
-            "label"          : 'TTJets_Lept',
+            "label"          : 'TTJets_DiLept',
+            "type"           : 'mc',
             },
         ]
     },
     {
     "channel"      : "emu_trig_eff_vs_mu_pt",
     "x_axis_title" : 'Muon p_{T} [GeV]',
-    "bins"         : [20,30,35,37,40,50,60,100,180,300],
+    "bins"         : [20,25,30,35,40,80,120,300],
     "hist_pairs"   :
         [
             {
-            "input_file"     : '/data/users/bcardwell/condor/EMu_TrigEfficiency2016_17_10_13/JetHT_2016_postHIP.root',
+            "input_file"     : '/data/users/bcardwell/condor/EMu_TrigEfficiency2016_MET_18_01_25/MET_2016_postHIP.root',
             "pass_hist"      : 'TTbarForTrigEff38HighPtEPlotter/Muon Plots/muonPt',
-            "total_hist"     : 'TTbarForTrigEffNoTrigHighPtEPlotter/Muon Plots/muonPt',
+            "total_hist"     : 'TTbarForTrigEffHighPtEPlotter/Muon Plots/muonPt',
             "color"          : 1,
-            "label"          : 'JetHT_2016 G and H',
+            "label"          : 'MET_2016 G and H',
+            "type"           : 'data',
             },
             {
-            "input_file"     : '/data/users/bcardwell/condor/EMu_TrigEfficiency2016_17_10_13/TTJets_Lept.root',
+            "input_file"     : '/data/users/bcardwell/condor/EMu_TrigEfficiency2016_MET_18_01_25/TTJets_DiLept.root',
             "pass_hist"      : 'TTbarForTrigEff38HighPtEPlotter/Muon Plots/muonPt',
-            "total_hist"     : 'TTbarForTrigEffNoTrigHighPtEPlotter/Muon Plots/muonPt',
+            "total_hist"     : 'TTbarForTrigEffHighPtEPlotter/Muon Plots/muonPt',
             "color"          : 2,
             "label"          : 'TTJets_Lept',
+            "type"           : 'mc',
             },
         ]
     },
-    {
-    "channel"      : "emu_trig_eff_vs_e_pt_combineBins",
-    "x_axis_title" : 'Electron p_{T} [GeV]',
-    "bins"         : [20,35,42,300],
-    "hist_pairs"   :
-        [
-            {
-            "input_file"     : '/data/users/bcardwell/condor/EMu_TrigEfficiency2016_17_10_13/JetHT_2016_postHIP.root',
-            "pass_hist"      : 'TTbarForTrigEff38HighPtMuPlotter/Electron Plots/electronPt',
-            "total_hist"     : 'TTbarForTrigEffNoTrigHighPtMuPlotter/Electron Plots/electronPt',
-            "color"          : 1,
-            "label"          : 'JetHT_2016 G and H',
-            },
-            {
-            "input_file"     : '/data/users/bcardwell/condor/EMu_TrigEfficiency2016_17_10_13/TTJets_Lept.root',
-            "pass_hist"      : 'TTbarForTrigEff38HighPtMuPlotter/Electron Plots/electronPt',
-            "total_hist"     : 'TTbarForTrigEffNoTrigHighPtMuPlotter/Electron Plots/electronPt',
-            "color"          : 2,
-            "label"          : 'TTJets_Lept',
-            },
-        ]
-    },
-    {
-    "channel"      : "emu_trig_eff_vs_mu_pt_combineBins",
-    "x_axis_title" : 'Muon p_{T} [GeV]',
-    "bins"         : [20,35,40,300],
-    "hist_pairs"   :
-        [
-            {
-            "input_file"     : '/data/users/bcardwell/condor/EMu_TrigEfficiency2016_17_10_13/JetHT_2016_postHIP.root',
-            "pass_hist"      : 'TTbarForTrigEff38HighPtEPlotter/Muon Plots/muonPt',
-            "total_hist"     : 'TTbarForTrigEffNoTrigHighPtEPlotter/Muon Plots/muonPt',
-            "color"          : 1,
-            "label"          : 'JetHT_2016 G and H',
-            },
-            {
-            "input_file"     : '/data/users/bcardwell/condor/EMu_TrigEfficiency2016_17_10_13/TTJets_Lept.root',
-            "pass_hist"      : 'TTbarForTrigEff38HighPtEPlotter/Muon Plots/muonPt',
-            "total_hist"     : 'TTbarForTrigEffNoTrigHighPtEPlotter/Muon Plots/muonPt',
-            "color"          : 2,
-            "label"          : 'TTJets_Lept',
-            },
-        ]
-    },
+    #{
+    #"channel"      : "emu_trig_eff_vs_e_pt_combineBins",
+    #"x_axis_title" : 'Electron p_{T} [GeV]',
+    #"bins"         : [20,30,33,300],
+    #"hist_pairs"   :
+    #    [
+    #        {
+    #        "input_file"     : '/data/users/bcardwell/condor/EMu_TrigEfficiency2016_17_10_30/JetHT_2016_postHIP.root',
+    #        "pass_hist"      : 'TTbarForTrigEffStandardTrigHighPtMuPlotter/Electron Plots/electronPt',
+    #        "total_hist"     : 'TTbarForTrigEffNoTrigHighPtMuPlotter/Electron Plots/electronPt',
+    #        "color"          : 1,
+    #        "label"          : 'JetHT_2016 G and H',
+    #        },
+    #        {
+    #        "input_file"     : '/data/users/bcardwell/condor/EMu_TrigEfficiency2016_17_10_30/TTJets_Lept.root',
+    #        "pass_hist"      : 'TTbarForTrigEffStandardTrigHighPtMuPlotter/Electron Plots/electronPt',
+    #        "total_hist"     : 'TTbarForTrigEffNoTrigHighPtMuPlotter/Electron Plots/electronPt',
+    #        "color"          : 2,
+    #        "label"          : 'TTJets_Lept',
+    #        },
+    #    ]
+    #},
+    #{
+    #"channel"      : "emu_trig_eff_vs_mu_pt_combineBins",
+    #"x_axis_title" : 'Muon p_{T} [GeV]',
+    #"bins"         : [20,30,33,300],
+    #"hist_pairs"   :
+    #    [
+    #        {
+    #        "input_file"     : '/data/users/bcardwell/condor/EMu_TrigEfficiency2016_17_10_30/JetHT_2016_postHIP.root',
+    #        "pass_hist"      : 'TTbarForTrigEffStandardTrigHighPtEPlotter/Muon Plots/muonPt',
+    #        "total_hist"     : 'TTbarForTrigEffNoTrigHighPtEPlotter/Muon Plots/muonPt',
+    #        "color"          : 1,
+    #        "label"          : 'JetHT_2016 G and H',
+    #        },
+    #        {
+    #        "input_file"     : '/data/users/bcardwell/condor/EMu_TrigEfficiency2016_17_10_30/TTJets_Lept.root',
+    #        "pass_hist"      : 'TTbarForTrigEffStandardTrigHighPtEPlotter/Muon Plots/muonPt',
+    #        "total_hist"     : 'TTbarForTrigEffNoTrigHighPtEPlotter/Muon Plots/muonPt',
+    #        "color"          : 2,
+    #        "label"          : 'TTJets_Lept',
+    #        },
+    #    ]
+    #},
 ]
 
