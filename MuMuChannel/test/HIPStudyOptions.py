ab66117e073d3f94f4cf7a623b95d047616b9f46
Juliette Alimena <juliette.alimena@cern.ch>
2018-03-27 11:55:05 -0400

    modernize two more Options configs

--- MuMuChannel/test/HIPStudyOptions.py
+++ MuMuChannel/test/HIPStudyOptions.py
@@ -1,17 +1,12 @@
 #!/usr/bin/env python
-
-# import the definitions of all the datasets on the T3
-from OSUT3Analysis.Configuration.configurationOptions import *
-from DisplacedSUSY.Configuration.miniAODV2_80X_Samples import *
+from DisplacedSUSY.StandardAnalysis.Options import *
 
 # specify which config file to pass to cmsRun
 config_file = "HIPStudyRegions_cfg.py"
 
 # choose luminosity used for MC normalization
-intLumi = 35863.308
+intLumi = 35863.308 #redefine intLumi here for HIP study
 
-systematics_file = "DisplacedSUSY.Configuration.systematicsDefinitions"
-external_systematics_directory = "DisplacedSUSY/Configuration/data/"
 
 # create list of datasets to process
 datasets = [
