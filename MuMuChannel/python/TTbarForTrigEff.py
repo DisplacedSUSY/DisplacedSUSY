b5bad9ecc91644a10e9f53c1415a52e8a8b628fa
Estefany Nunez <nunezornelas.1@osu.edu>
2018-05-24 11:40:38 -0400

    MuMu channel trigger efficiency

--- MuMuChannel/python/TTbarForTrigEff.py
+++ MuMuChannel/python/TTbarForTrigEff.py
@@ -37,28 +37,91 @@ TTbarForTrigEffNoTrig.cuts.append(muon_2muon_cut)
 
 
 ### Add triggers ###
-TTbarForTrigEff43 = cms.PSet(
-    name = cms.string("TTbarForTrigEff43"),
-    triggers = cms.vstring("HLT_DoubleMu43NoFiltersNoVtx_v"),
-    cuts = cms.VPSet(copy.deepcopy(TTbarForTrigEffNoTrig.cuts))
+
+#######################################
+### Triggers used by Jamie and Bryan###
+#######################################
+# TTbarForTrigEff43 = cms.PSet(
+#     name = cms.string("TTbarForTrigEff43"),
+#     triggers = cms.vstring("HLT_DoubleMu43NoFiltersNoVtx_v"),
+#     cuts = cms.VPSet(copy.deepcopy(TTbarForTrigEffNoTrig.cuts))
+# )
+# 
+# TTbarForTrigEff48 = cms.PSet(
+#     name = cms.string("TTbarForTrigEff48"),
+#     triggers = cms.vstring("HLT_DoubleMu48NoFiltersNoVtx_v"),
+#     cuts = cms.VPSet(copy.deepcopy(TTbarForTrigEffNoTrig.cuts))
+# )
+# 
+# ### Veto triggers ###
+# TTbarForTrigEffVeto43 = cms.PSet(
+#     name = cms.string("TTbarForTrigEffVeto43"),
+#     triggers = cms.vstring(),
+#     triggersToVeto = cms.vstring("HLT_DoubleMu43NoFiltersNoVtx_v"),
+#     cuts = cms.VPSet(copy.deepcopy(TTbarForTrigEffNoTrig.cuts))
+# )
+# TTbarForTrigEffVeto43.cuts.append(muon_pt_70_cut)
+
+###############################
+### Triggers used for 2016 ####
+###############################
+TriggerDoubleMu33 = cms.PSet(
+    name = cms.string("TriggerDoubleMu33"),
+    triggers = cms.vstring("HLT_DoubleMu33NoFiltersNoVtx"),
+    cuts = cms.VPSet()
 )
+TriggerDoubleMu33.cuts.append(pass_trigger)
 
-TTbarForTrigEff48 = cms.PSet(
-    name = cms.string("TTbarForTrigEff48"),
-    triggers = cms.vstring("HLT_DoubleMu48NoFiltersNoVtx_v"),
-    cuts = cms.VPSet(copy.deepcopy(TTbarForTrigEffNoTrig.cuts))
+TriggerDoubleMu23Displaced = cms.PSet(
+    name = cms.string("TriggerDoubleMu23Displaced"),
+    triggers = cms.vstring("HLT_DoubleMu23NoFiltersNoVtxDisplaced"),
+    cuts = cms.VPSet()
 )
+TriggerDoubleMu23Displaced.cuts.append(pass_trigger)
 
+# Trigger to be used for 2016 MuMu channel
+TriggerDoubleMu33ORDoubleMu23Displaced = cms.PSet(
+    name = cms.string("TriggerDoubleMu33ORDoubleMu23Displaced"),
+    triggers = cms.vstring("HLT_DoubleMu33NoFiltersNoVtx","HLT_DoubleMu23NoFiltersNoVtxDisplaced"),
+    cuts = cms.VPSet()
+)
+TriggerDoubleMu33ORDoubleMu23Displaced.cuts.append(pass_trigger)
 
-### Veto triggers ###
-TTbarForTrigEffVeto43 = cms.PSet(
-    name = cms.string("TTbarForTrigEffVeto43"),
+TrigMET = cms.PSet(
+    name = cms.string("TrigMET"),
     triggers = cms.vstring(),
-    triggersToVeto = cms.vstring("HLT_DoubleMu43NoFiltersNoVtx_v"),
+    cuts = cms.VPSet()
+)
+TrigMET.cuts.append(pass_trigger)
+
+### TTbar for triggers without tag muon eventvariable ###
+MuMuTrigTTbarMET = cms.PSet(
+    name = cms.string("MuMuTrigTTbarMET"),
+    triggers = cms.vstring("HLT_DoubleMu33NoFiltersNoVtx","HLT_DoubleMu23NoFiltersNoVtxDisplaced"),
     cuts = cms.VPSet(copy.deepcopy(TTbarForTrigEffNoTrig.cuts))
 )
-TTbarForTrigEffVeto43.cuts.append(muon_pt_70_cut)
+MuMuTrigTTbarMET.cuts.append(pass_trigger)
 
+TrigTTbarMET = cms.PSet(
+    name = cms.string("TrigTTbarMET"),
+    triggers = cms.vstring(),
+    cuts = cms.VPSet(copy.deepcopy(TTbarForTrigEffNoTrig.cuts))
+)
+TrigTTbarMET.cuts.append(pass_trigger)
+
+DoubleMu33TTbar = cms.PSet(
+    name = cms.string("DoubleMu33TTbar"),
+    triggers = cms.vstring(),
+    cuts = cms.VPSet(copy.deepcopy(TTbarForTrigEffNoTrig.cuts))
+)
+DoubleMu33TTbar.cuts.append(pass_trigger)
+
+DoubleMu23DisplacedTTbar = cms.PSet(
+    name = cms.string("DoubleMu23DisplacedTTbar"),
+    triggers = cms.vstring(),
+    cuts = cms.VPSet(copy.deepcopy(TTbarForTrigEffNoTrig.cuts))
+)
+DoubleMu23DisplacedTTbar.cuts.append(pass_trigger)
 
 ### Use tag muon eventvariable
 TTbarForTrigEffTagMuonNoTrig = cms.PSet(
@@ -75,15 +138,46 @@ TTbarForTrigEffTagMuonNoTrig.cuts.append(tagMuonExists_cut)
 TTbarForTrigEffTagMuonNoTrig.cuts.append(muon_opposite_charge_from_tag_cut)
 TTbarForTrigEffTagMuonNoTrig.cuts.append(muon_deltaR_from_tag_cut)
 
-TTbarForTrigEffTagMuon43 = cms.PSet(
-    name = cms.string("TTbarForTrigEffTagMuon43"),
-    triggers = cms.vstring("HLT_DoubleMu43NoFiltersNoVtx_v"),
-    cuts = cms.VPSet(copy.deepcopy(TTbarForTrigEffTagMuonNoTrig.cuts))
-)
+# TTbarForTrigEffTagMuon43 = cms.PSet(
+#     name = cms.string("TTbarForTrigEffTagMuon43"),
+#     triggers = cms.vstring("HLT_DoubleMu43NoFiltersNoVtx_v"),
+#     cuts = cms.VPSet(copy.deepcopy(TTbarForTrigEffTagMuonNoTrig.cuts))
+# )
+# 
+# TTbarForTrigEffTagMuon48 = cms.PSet(
+#     name = cms.string("TTbarForTrigEffTagMuon48"),
+#     triggers = cms.vstring("HLT_DoubleMu48NoFiltersNoVtx_v"),
+#     cuts = cms.VPSet(copy.deepcopy(TTbarForTrigEffTagMuonNoTrig.cuts))
+# )
 
-TTbarForTrigEffTagMuon48 = cms.PSet(
-    name = cms.string("TTbarForTrigEffTagMuon48"),
-    triggers = cms.vstring("HLT_DoubleMu48NoFiltersNoVtx_v"),
+
+TriggerDoubleMu33ORDoubleMu23DisplacedTagMuon = cms.PSet(
+    name = cms.string("TriggerDoubleMu33ORDoubleMu23DisplacedTagMuon"),
+    triggers = cms.vstring("HLT_DoubleMu33NoFiltersNoVtx","HLT_DoubleMu23NoFiltersNoVtxDisplaced"),
     cuts = cms.VPSet(copy.deepcopy(TTbarForTrigEffTagMuonNoTrig.cuts))
 )
+TriggerDoubleMu33ORDoubleMu23DisplacedTagMuon.cuts.append(pass_trigger)
+
+
+TTbarTagMuonTrigMET = cms.PSet(
+    name = cms.string("TTbarTagMuonTrigMET"),
+    triggers = cms.vstring(),
+    cuts = cms.VPSet(copy.deepcopy(TTbarForTrigEffTagMuonNoTrig.cuts))
+)
+TTbarTagMuonTrigMET.cuts.append(pass_trigger)
+
+DoubleMu33TTbarTagMuon = cms.PSet(
+    name = cms.string("DoubleMu33TTbarTagMuon"),
+    triggers = cms.vstring(),
+    cuts = cms.VPSet(copy.deepcopy(TTbarForTrigEffTagMuonNoTrig.cuts))
+)
+DoubleMu33TTbarTagMuon.cuts.append(pass_trigger)
+
+DoubleMu23DisplacedTTbarTagMuon = cms.PSet(
+    name = cms.string("DoubleMu23DisplacedTTbarTagMuon"),
+    triggers = cms.vstring(),
+    cuts = cms.VPSet(copy.deepcopy(TTbarForTrigEffTagMuonNoTrig.cuts))
+)
+DoubleMu23DisplacedTTbarTagMuon.cuts.append(pass_trigger)
+
 
---
+++ MuMuChannel/test/TTbarForTrigEffOptions.py
@@ -0,0 +1,33 @@
+import os
+from DisplacedSUSY.StandardAnalysis.Options import *
+
+# specify which config file to pass to cmsRun
+config_file = "TTbarForTrigEfficiency_cfg.py"
+
+# create list of datasets to process
+datasets = [
+
+
+    #signal
+    # 'stop200_1mm',
+    # 'stop200_10mm',
+    # 'stop200_100mm',
+    # 'stop200_1000mm',
+    # 'stop800_1mm',
+    # 'stop800_10mm',
+    # 'stop800_100mm',
+    # 'stop800_1000mm',
+    # 'stop1200_1mm',
+    # 'stop1200_10mm',
+    # 'stop1200_100mm',
+    # 'stop1200_1000mm',
+
+    #data
+    'MET_2016_postHIP',
+    # 'MET_2016G',
+    # 'MET_2016H',
+
+    #ttbar
+    'TTJets_Lept',
+]
+
