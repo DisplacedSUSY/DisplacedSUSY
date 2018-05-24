6e75a1f2e0d074a920ff415604914e445c75da83
Juliette Alimena <juliette.alimena@cern.ch>
2018-05-24 07:44:42 -0500

    add crab configs for 2018A PromptReco v1 and v2

---
+++ TriggerEfficiencyFromDQM/crab_HltExoDqm_NoBPTX_Run2018A_v1.py
@@ -0,0 +1,20 @@
+from CRABClient.UserUtilities import config, getUsernameFromSiteDB
+config = config()
+
+config.General.workArea = 'crab'
+config.General.transferOutputs = True
+config.General.transferLogs = True
+config.General.requestName = 'HltExoDqm_NoBPTX_Run2018A_PromptReco_v1_22May2018_v1'
+
+config.JobType.pluginName = 'Analysis'
+config.JobType.psetName = 'hltExodqm2018.py'
+
+config.Data.splitting = 'FileBased'
+config.Data.unitsPerJob = 1
+config.Data.lumiMask = '/uscms_data/d3/alimena/2018TriggerDisplacedSUSY_take2/CMSSW_10_1_5/src/json_DCSONLY_22May2018.txt'
+config.Data.outLFNDirBase = '/store/user/%s/' % (getUsernameFromSiteDB())
+config.Data.publication = True
+config.Data.inputDataset = '/NoBPTX/Run2018A-PromptReco-v1/AOD'
+config.Data.outputDatasetTag = 'HltExoDqm_NoBPTX_Run2018A_PromptReco_v1_22May2018_v1'
+
+config.Site.storageSite = 'T3_US_FNALLPC'
