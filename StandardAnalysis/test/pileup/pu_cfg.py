3d4526cc62eab5c24a6e6cdc8ffe09a8293272b5
btcardwell <cardwell.80@osu.edu>
2018-06-19 13:23:25 -0400

    Run different MC for 2016 or 2017 depending on CMSSW release

--- StandardAnalysis/test/pileup/pu_cfg.py
+++ StandardAnalysis/test/pileup/pu_cfg.py
@@ -1,3 +1,4 @@
+import os
 import FWCore.ParameterSet.Config as cms
 
 
@@ -8,9 +9,22 @@ import FWCore.ParameterSet.Config as cms
 process = cms.Process ('SignalMC')
 process.load ('FWCore.MessageService.MessageLogger_cfi')
 process.MessageLogger.cerr.FwkReport.reportEvery = 100
-process.source = cms.Source ('PoolSource',
-    fileNames = cms.untracked.vstring ("root://xrootd-cms.infn.it//store/mc/RunIISpring15MiniAODv2/TTJets_SingleLeptFromTbar_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/MINIAODSIM/74X_mcRun2_asymptotic_v2_ext1-v1/10000/06C6F736-BB71-E511-AAB4-6C3BE5B59060.root"),
-)
+
+if os.environ["CMSSW_VERSION"].startswith("CMSSW_8_0_"):
+    process.source = cms.Source ('PoolSource',
+        fileNames = cms.untracked.vstring (
+            'root://xrootd-cms.infn.it//store/mc/RunIISpring15MiniAODv2/TTJets_SingleLeptFromTbar_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/MINIAODSIM/74X_mcRun2_asymptotic_v2_ext1-v1/10000/06C6F736-BB71-E511-AAB4-6C3BE5B59060.root'
+        )
+    )
+elif os.environ["CMSSW_VERSION"].startswith("CMSSW_9_4_"):
+    process.source = cms.Source ('PoolSource',
+        fileNames = cms.untracked.vstring (
+            '/store/mc/RunIIFall17MiniAOD/ST_tW_top_5f_NoFullyHadronicDecays_TuneCP5_13TeV-powheg-pythia8/MINIAODSIM/94X_mc2017_realistic_v10-v1/10000/9A55E3AC-E8F8-E711-9214-0CC47AB58BE4.root'
+        )
+    )
+else:
+    print "What CMSSW release are you in? We expect to be in 80X or 94X"
+
 process.TFileService = cms.Service ("TFileService",
     fileName = cms.string ('hist.root')
 )
