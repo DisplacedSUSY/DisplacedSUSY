3dcbb5d6bd557f006da08e7845cd1e3c421548e3
Bryan <cardwell.80@osu.edu>
2018-08-22 09:38:54 -0500

    Use composite background in fitGaussian_cfg

--- EEChannel/test/fitGaussian_cfg.py
+++ EEChannel/test/fitGaussian_cfg.py
@@ -2,13 +2,13 @@
 
 distributions = [
     {
-        'name' : "dy_jets",
-        'file' : "~/nobackup/condor/EEPromptControlRegion_2017Analysis_02August2018/DYJetsToLL.root",
+        'name' : "Background",
+        'file' : "~/nobackup/condor/EEPromptControlRegion_2017Analysis_02August2018/Background.root",
         'hist' : "PromptControlRegionPlotter/Electron-beamspot Plots/electronD0_50um",
         'fit_range' : (-20, 20), # optional parameter. comment out to fit full range
     },
     {
-        'name' : "data",
+        'name' : "DoubleEG_2017",
         'file' : "~/nobackup/condor/EEPromptControlRegion_2017Analysis_02August2018/DoubleEG_2017.root",
         'hist' : "PromptControlRegionPlotter/Electron-beamspot Plots/electronD0_50um",
         'fit_range' : (-20, 20), # optional parameter. comment out to fit full range
