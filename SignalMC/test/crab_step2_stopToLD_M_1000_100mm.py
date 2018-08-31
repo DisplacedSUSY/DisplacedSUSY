4a65f3f30fcd8d6c405ba89fb91d086b6e40fcce
Juliette Alimena <juliette.alimena@cern.ch>
2018-08-31 04:28:17 -0500

    add more crab config files

---
+++ SignalMC/test/crab_step2_stopToLD_M_1000_100mm.py
@@ -0,0 +1,25 @@
+from CRABClient.UserUtilities import config, getUsernameFromSiteDB
+config = config()
+
+
+config.General.requestName = 'stopToLD_M_1000_100mm_13TeV_2017MC_DigiRawHlt'
+config.General.workArea = 'crab'
+config.General.transferOutputs = True
+config.General.transferLogs = True
+
+config.JobType.pluginName = 'Analysis'
+config.JobType.psetName = 'step2_DIGIPREMIX_S2_DATAMIX_L1_DIGI2RAW_HLT.py'
+config.JobType.maxMemoryMB = 8000
+config.JobType.numCores = 8
+
+config.Data.inputDataset = '/StopToLD_M_1000_100mm_13TeV_2017MC/jalimena-GenSim-00645c2195a8b371eb1fc238b74644dd/USER'
+config.Data.outputDatasetTag = 'DigiRawHlt'
+config.Data.inputDBS = 'phys03'
+config.Data.splitting = 'FileBased'
+config.Data.unitsPerJob = 1
+config.Data.outLFNDirBase = '/store/group/lpclonglived/DisplacedLeptons'
+config.Data.publication = True
+config.Data.ignoreLocality = True
+
+config.Site.whitelist = ["T1_US_FNAL"]
+config.Site.storageSite = 'T3_US_FNALLPC'
---
+++ SignalMC/test/crab_step4_stopToLD_M_1000_100mm.py
@@ -0,0 +1,25 @@
+from CRABClient.UserUtilities import config, getUsernameFromSiteDB
+config = config()
+
+
+config.General.requestName = 'stopToLD_M_1000_100mm_13TeV_2017MC_MiniAod'
+config.General.workArea = 'crab'
+config.General.transferOutputs = True
+config.General.transferLogs = True
+
+config.JobType.pluginName = 'Analysis'
+config.JobType.psetName = 'step4_PAT.py'
+config.JobType.maxMemoryMB = 8000
+config.JobType.numCores = 4
+
+config.Data.inputDataset = ''
+config.Data.outputDatasetTag = 'MiniAod'
+config.Data.inputDBS = 'phys03'
+config.Data.splitting = 'FileBased'
+config.Data.unitsPerJob = 1
+config.Data.outLFNDirBase = '/store/group/lpclonglived/DisplacedLeptons'
+config.Data.publication = True
+config.Data.ignoreLocality = True
+
+config.Site.whitelist = ["T1_US_FNAL"]
+config.Site.storageSite = 'T3_US_FNALLPC'
