20a902dac13f744bee4c84659d894b68323fffba
Bryan <cardwell.80@osu.edu>
2018-11-20 16:16:19 -0600

    Read from config file; use condor directory

--- Configuration/scripts/EstimateNonQcdBackground.py
+++ Configuration/scripts/EstimateNonQcdBackground.py
@@ -1,22 +1,37 @@
 #!/usr/bin/env python
+import sys
+import os
+import re
+from optparse import OptionParser
+from ROOT import TFile, TCanvas, TGraphAsymmErrors, TMultiGraph, TLegend, Double, gROOT
 
-import time
-from ROOT import TFile, TCanvas, TGraphAsymmErrors, TMultiGraph, TLegend, Double, gPad
+parser = OptionParser()
+parser.add_option("-l", "--localConfig", dest="localConfig",
+                  help="local configuration file")
+parser.add_option("-w", "--workDirectory", dest="condorDir",
+                  help="condor working directory")
 
-#condor_dir = "EEPreselection_2017Analysis_26July2018"
-condor_dir = "EMuPreselection_2017Analysis_28July2018"
+(arguments, args) = parser.parse_args()
+if arguments.localConfig:
+    sys.path.append(os.getcwd())
+    exec("from " + re.sub (r".py$", r"", arguments.localConfig) + " import *")
+else:
+    print "you forgot to specify a config file with -l"
+    sys.exit(1)
 
-#plot = "PreselectionPlotter/Electron-beamspot Plots/electronAbsD0[0]_vs_electronAbsD0[1]_500um"
-plot = "PreselectionPlotter/Electron-muon-beamspot Plots/electronAbsD0_vs_muonAbsD0_500um"
+if arguments.condorDir:
+    output_path = "condor/" + arguments.condorDir + "/"
+    if not os.path.exists(output_path):
+        os.makedirs(output_path)
+else:
+    print "you forgot to specify a condor directory with -w"
+    sys.exit(1)
 
-samples = [
-    'DYJetsToLL',
-    'TTJets_inclusive',
-    'SingleTop',
-    'Diboson',
-]
+gROOT.SetBatch()
 
-
+# pick good TColors
+colors = [1,2,3,4,6,7,8,9,11,29,33,36,38,40,45,48]
+color_ix = 0
 
 def makeEfficiencyHist(in_hist):
     last_bin = in_hist.GetXaxis().GetLast()
@@ -48,35 +63,32 @@ def makeEfficiencyHist(in_hist):
 
 ###############################################################################
 
-colors = [1,2,3,4,6,7,8,9,11,29,33,36,38,40,45,48]
-color_ix = 0
-
 x_multigraph = TMultiGraph()
 y_multigraph = TMultiGraph()
 x_legend = TLegend()
 y_legend = TLegend()
 
 for sample in samples:
-    in_file = TFile("condor/"+condor_dir+"/"+sample+".root")
+    in_file = TFile(output_path+sample+".root")
     in_hist = in_file.Get(plot)
 
     x_eff_hist = makeEfficiencyHist(in_hist.ProjectionX().Clone())
     y_eff_hist = makeEfficiencyHist(in_hist.ProjectionY().Clone())
-
     x_eff_hist.SetLineColor(colors[color_ix])
     y_eff_hist.SetLineColor(colors[color_ix])
     color_ix += 1
 
     x_multigraph.Add(x_eff_hist)
     y_multigraph.Add(y_eff_hist)
+    x_multigraph.SetTitle(";"+in_hist.GetXaxis().GetTitle()+";Efficiency")
+    y_multigraph.SetTitle(";"+in_hist.GetYaxis().GetTitle()+";Efficiency")
 
     x_legend.AddEntry(x_eff_hist, sample, "elp")
     y_legend.AddEntry(y_eff_hist, sample, "elp")
 
-    x_multigraph.SetTitle(";"+in_hist.GetXaxis().GetTitle()+";Efficiency")
-    y_multigraph.SetTitle(";"+in_hist.GetYaxis().GetTitle()+";Efficiency")
 
-out_file = TFile("nonQcdBgEstimate.root", "recreate")
+# plot everything and make output file
+out_file = TFile(output_path+"nonQcdBgEstimate.root", "recreate")
 x_canvas = TCanvas("x", "x", 100, 100, 700, 700)
 x_canvas.SetLogy()
 x_multigraph.Draw("ap")
