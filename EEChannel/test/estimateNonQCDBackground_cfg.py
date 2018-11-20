4295d25f18fad6dcea4601a44696663fc9010095
Bryan <cardwell.80@osu.edu>
2018-11-20 16:24:14 -0600

    First commit of sample config for estimateNonQcdBackground.py

---
+++ EEChannel/test/estimateNonQCDBackground_cfg.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+plot = "PreselectionPlotter/Electron-beamspot Plots/electronAbsD0[0]_vs_electronAbsD0[1]_500um"
+
+samples = [
+    'DYJetsToLL',
+    'TTJets_inclusive',
+    'SingleTop',
+    'Diboson',
+]
