b620cf23b77adbabda45af39f059287f5c4aad24
estefanynunez <nunezornelas.1@osu.edu>
2018-11-25 14:50:30 -0600

    work-in-progress for non qcd background estimation

--- Configuration/scripts/estimateNonQcdBackground.py
+++ Configuration/scripts/estimateNonQcdBackground.py
@@ -2,6 +2,7 @@
 import sys
 import os
 import re
+import numpy
 from optparse import OptionParser
 from ROOT import TFile, TCanvas, TGraphAsymmErrors, TMultiGraph, TLegend, Double, gROOT
 
@@ -61,6 +62,13 @@ def makeEfficiencyHist(in_hist):
 
     return TGraphAsymmErrors(pass_hist, total_hist)
 
+def get_yields(x_eff, y_eff):
+    last_bin = in_hist.GetXaxis().GetLast()
+    total_error = Double(0.0)
+    n_total = in_hist.IntegralAndError(0, last_bin, total_error)
+    yield_estimation = x_eff * y_eff * total
+    return yield_estimation
+
 ###############################################################################
 
 x_multigraph = TMultiGraph()
@@ -68,24 +76,39 @@ y_multigraph = TMultiGraph()
 x_legend = TLegend(0.6, 0.6, 0.8, 0.8)
 y_legend = TLegend(0.6, 0.6, 0.8, 0.8)
 
-for sample in samples:
+for x_lo, x_hi in zip(bins_x[:-1], bins_x[1:]):
     in_file = TFile(output_path+sample+".root")
     in_hist = in_file.Get(plot)
 
-    x_eff_hist = makeEfficiencyHist(in_hist.ProjectionX().Clone())
-    y_eff_hist = makeEfficiencyHist(in_hist.ProjectionY().Clone())
-    x_eff_hist.SetLineColor(colors[color_ix])
-    y_eff_hist.SetLineColor(colors[color_ix])
-    color_ix += 1
+    x_bin_lo = in_hist.GetXaxis().FindBin(x_lo)
+    x_bin_hi = in_hist.GetXaxis().FindBin(x_hi)-1
 
-    x_multigraph.Add(x_eff_hist)
-    y_multigraph.Add(y_eff_hist)
-    x_multigraph.SetTitle(";"+in_hist.GetXaxis().GetTitle()+";Efficiency")
-    y_multigraph.SetTitle(";"+in_hist.GetYaxis().GetTitle()+";Efficiency")
+    for y_lo, y_hi in zip(bins_y[:-1], bins_y[1:]):
+        y_bin_lo = in_hist.GetYaxis().FindBin(y_lo)
+        y_bin_hi = in_hist.GetYaxis().FindBin(y_hi)-1
 
-    x_legend.AddEntry(x_eff_hist, sample, "elp")
-    y_legend.AddEntry(y_eff_hist, sample, "elp")
+        for sample in samples:
+            #in_file = TFile(output_path+sample+".root")
+            #in_hist = in_file.Get(plot)
 
+            x_eff_hist = makeEfficiencyHist(in_hist.ProjectionX().Clone())
+            y_eff_hist = makeEfficiencyHist(in_hist.ProjectionY().Clone())
+            x_eff_hist.SetLineColor(colors[color_ix])
+            y_eff_hist.SetLineColor(colors[color_ix])
+            color_ix += 1
+
+            total_error = Double(0.0)
+            x_eff = x_eff_hist.IntegralAndError(x_lo, x_hi, error)
+            y_eff = y_eff_hist.IntegralAndError(y_lo, y_hi, error)
+
+            x_multigraph.Add(x_eff_hist)
+            y_multigraph.Add(y_eff_hist)
+            x_multigraph.SetTitle(";"+in_hist.GetXaxis().GetTitle()+";Efficiency")
+            y_multigraph.SetTitle(";"+in_hist.GetYaxis().GetTitle()+";Efficiency")
+
+            x_legend.AddEntry(x_eff_hist, sample, "elp")
+
+    estimation = get_yields(x_eff, y_eff)
 
 # plot everything and make output file
 out_file = TFile(output_path+"nonQcdBgEstimate.root", "recreate")
