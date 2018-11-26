9c7c010f0013d56b0dc3a28641261a8a68187255
Bryan <cardwell.80@osu.edu>
2018-11-25 23:58:50 -0600

    Do closure test in non-qcd background estimate script

--- Configuration/scripts/estimateNonQcdBackground.py
+++ Configuration/scripts/estimateNonQcdBackground.py
@@ -2,7 +2,6 @@
 import sys
 import os
 import re
-import numpy
 from optparse import OptionParser
 from ROOT import TFile, TCanvas, TGraphAsymmErrors, TMultiGraph, TLegend, Double, gROOT
 
@@ -62,13 +61,6 @@ def makeEfficiencyHist(in_hist):
 
     return TGraphAsymmErrors(pass_hist, total_hist)
 
-def get_yields(x_eff, y_eff):
-    last_bin = in_hist.GetXaxis().GetLast()
-    total_error = Double(0.0)
-    n_total = in_hist.IntegralAndError(0, last_bin, total_error)
-    yield_estimation = x_eff * y_eff * total
-    return yield_estimation
-
 ###############################################################################
 
 x_multigraph = TMultiGraph()
@@ -76,42 +68,57 @@ y_multigraph = TMultiGraph()
 x_legend = TLegend(0.6, 0.6, 0.8, 0.8)
 y_legend = TLegend(0.6, 0.6, 0.8, 0.8)
 
-for x_lo, x_hi in zip(bins_x[:-1], bins_x[1:]):
+for sample in samples:
     in_file = TFile(output_path+sample+".root")
-    in_hist = in_file.Get(plot)
+    in_hist = in_file.Get(plot).Clone()
 
-    x_bin_lo = in_hist.GetXaxis().FindBin(x_lo)
-    x_bin_hi = in_hist.GetXaxis().FindBin(x_hi)-1
+    # make efficiency plots
+    x_eff_hist = makeEfficiencyHist(in_hist.ProjectionX().Clone())
+    y_eff_hist = makeEfficiencyHist(in_hist.ProjectionY().Clone())
+    x_eff_hist.SetLineColor(colors[color_ix])
+    y_eff_hist.SetLineColor(colors[color_ix])
+    color_ix += 1
 
-    for y_lo, y_hi in zip(bins_y[:-1], bins_y[1:]):
-        y_bin_lo = in_hist.GetYaxis().FindBin(y_lo)
-        y_bin_hi = in_hist.GetYaxis().FindBin(y_hi)-1
+    # add efficiency plots to summary plot for all samples
+    x_multigraph.Add(x_eff_hist)
+    y_multigraph.Add(y_eff_hist)
+    x_multigraph.SetTitle(";"+in_hist.GetXaxis().GetTitle()+";Efficiency")
+    y_multigraph.SetTitle(";"+in_hist.GetYaxis().GetTitle()+";Efficiency")
+    x_legend.AddEntry(x_eff_hist, sample, "elp")
+    y_legend.AddEntry(y_eff_hist, sample, "elp")
 
-        for sample in samples:
-            #in_file = TFile(output_path+sample+".root")
-            #in_hist = in_file.Get(plot)
+    # use efficiencies to estimate background
+    last_x_bin = in_hist.GetXaxis().GetLast()
+    last_y_bin = in_hist.GetYaxis().GetLast()
+    total_error = Double(0.0)
+    total_yield = in_hist.IntegralAndError(0, last_x_bin, 0, last_y_bin, total_error)
+    bin_width = in_hist.GetXaxis().GetBinWidth(0)
 
-            x_eff_hist = makeEfficiencyHist(in_hist.ProjectionX().Clone())
-            y_eff_hist = makeEfficiencyHist(in_hist.ProjectionY().Clone())
-            x_eff_hist.SetLineColor(colors[color_ix])
-            y_eff_hist.SetLineColor(colors[color_ix])
-            color_ix += 1
+    print sample
 
-            total_error = Double(0.0)
-            x_eff = x_eff_hist.IntegralAndError(x_lo, x_hi, error)
-            y_eff = y_eff_hist.IntegralAndError(y_lo, y_hi, error)
+    x_eff_lo = Double(0.0)
+    x_eff_hi = Double(0.0)
+    y_eff_lo = Double(0.0)
+    y_eff_hi = Double(0.0)
+    _ = Double(0.0) # dummy variable for GetPoint method
+    for lo, hi in closure_test_bins:
+        lo_bin = int(lo / bin_width)
+        hi_bin = int(hi / bin_width)
+        x_eff_hist.GetPoint(lo_bin, _, x_eff_lo)
+        x_eff_hist.GetPoint(hi_bin, _, x_eff_hi)
+        y_eff_hist.GetPoint(lo_bin, _, y_eff_lo)
+        y_eff_hist.GetPoint(hi_bin, _, y_eff_hi)
 
-            x_multigraph.Add(x_eff_hist)
-            y_multigraph.Add(y_eff_hist)
-            x_multigraph.SetTitle(";"+in_hist.GetXaxis().GetTitle()+";Efficiency")
-            y_multigraph.SetTitle(";"+in_hist.GetYaxis().GetTitle()+";Efficiency")
+        overall_eff = (x_eff_lo - x_eff_hi) * (y_eff_lo - y_eff_hi)
 
-            x_legend.AddEntry(x_eff_hist, sample, "elp")
-
-    estimation = get_yields(x_eff, y_eff)
+        # print bg estimate from parameterization method and from direct counting
+        print lo, hi
+        print "Estimate:", overall_eff * total_yield, overall_eff * total_error # haven't accounted for error on efficiency values yet
+        err = Double(0.0)
+        print "Actual:", in_hist.IntegralAndError(lo_bin, hi_bin, lo_bin, hi_bin, err), err
 
 # plot everything and make output file
-out_file = TFile(output_path+"nonQcdBgEstimate.root", "recreate")
+out_file = TFile("nonQcdBgEstimate.root", "recreate")
 x_canvas = TCanvas("x", "x", 100, 100, 700, 700)
 x_canvas.SetLogy()
 x_multigraph.Draw("ap")
