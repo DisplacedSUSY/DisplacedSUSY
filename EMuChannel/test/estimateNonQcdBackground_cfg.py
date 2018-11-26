22c6cb4e4cfb95478f2569c04f687d90847512c7
Bryan <cardwell.80@osu.edu>
2018-11-25 23:11:40 -0600

    First commit of e-mu estimateNonQcdBackground config file

---
+++ EMuChannel/test/estimateNonQcdBackground_cfg.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+plot = "PreselectionPlotter/Electron-muon-beamspot Plots/electronAbsD0_vs_muonAbsD0_500um"
+
+samples = [
+    'DYJetsToLL',
+    'TTJets_inclusive',
+    'SingleTop',
+    'Diboson',
+]
+
+closure_test_bins = [
+    (60,200),
+    (80,200),
+    (100,200),
+    (120,200),
+    (140,200),
+    (160,200),
+]
