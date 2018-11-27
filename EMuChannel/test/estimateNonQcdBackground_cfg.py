9fac9c11d8215fb45c09e43c470ebb229ab80509
Bryan <cardwell.80@osu.edu>
2018-11-27 13:04:00 -0600

    Add 2016 samples to e-mu non-qcd bg estimation config

--- EMuChannel/test/estimateNonQcdBackground_cfg.py
+++ EMuChannel/test/estimateNonQcdBackground_cfg.py
@@ -1,13 +1,24 @@
 #!/usr/bin/env python
+import os
 
 plot = "PreselectionPlotter/Electron-muon-beamspot Plots/electronAbsD0_vs_muonAbsD0_500um"
 
-samples = [
-    'DYJetsToLL',
-    'TTJets_inclusive',
-    'SingleTop',
-    'Diboson',
-]
+if os.environ["CMSSW_VERSION"].startswith ("CMSSW_8_0_"):
+    samples = [
+        'DYJetsToLL',
+        'TTJets_Lept',
+        'SingleTop',
+        'Diboson',
+    ]
+
+elif os.environ["CMSSW_VERSION"].startswith ("CMSSW_9_4_"):
+    samples = [
+        'DYJetsToLL',
+        'TTJets_inclusive',
+        'SingleTop',
+        'Diboson',
+    ]
+
 
 closure_test_bins = [
     (60,200),
