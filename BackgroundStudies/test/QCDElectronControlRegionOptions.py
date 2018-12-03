df990f4f9620478f36855fe806f726c78ef50879
Juliette Alimena <juliette.alimena@cern.ch>
2018-12-03 11:00:30 -0600

    fix typo

--- BackgroundStudies/test/QCDElectronControlRegionOptions.py
+++ BackgroundStudies/test/QCDElectronControlRegionOptions.py
@@ -3,7 +3,7 @@ import os
 from DisplacedSUSY.BackgroundStudies.localOptionsElectron import *
 
 # specify which config file to pass to cmsRun
-config_file = "QCDElectronControlRegions_cfg.py"
+config_file = "QCDElectronControlRegion_cfg.py"
 
 # create list of datasets to process
 datasets = default_datasets
