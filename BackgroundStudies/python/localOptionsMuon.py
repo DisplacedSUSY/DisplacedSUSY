1604594420859e135d7a26f3441042b705cc22bd
Juliette Alimena <juliette.alimena@cern.ch>
2019-03-14 04:37:18 -0500

    update 2018 lumi with new rereco json

--- BackgroundStudies/python/localOptionsMuon.py
+++ BackgroundStudies/python/localOptionsMuon.py
@@ -37,7 +37,7 @@ elif os.environ["CMSSW_VERSION"].startswith ("CMSSW_9_4_"):
 
 elif os.environ["CMSSW_VERSION"].startswith ("CMSSW_10_2_"):
 
-    intLumi = 59710 #prompt reco 2018 golden json, with normtag
+    intLumi = 60020 #17Sept early rereco 2018 golden json, with normtag
 
     default_datasets = [
     'WJetsToLNu',
