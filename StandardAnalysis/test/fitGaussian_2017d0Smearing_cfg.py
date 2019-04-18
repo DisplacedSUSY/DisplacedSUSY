8d040b82d652486b86e290cfbbf64d1d04623f35
Juliette Alimena <juliette.alimena@cern.ch>
2019-04-18 09:00:19 -0500

    make two d0 smearing fitGaussian configs, for 2017 and 2018

--- StandardAnalysis/test/fitGaussian_2017d0Smearing_cfg.py
+++ StandardAnalysis/test/fitGaussian_2017d0Smearing_cfg.py
@@ -4,51 +4,51 @@ distributions = [
     # e-e channel
     {
         'name' : "EE_eD0_BackgroundMC",
-        'file' : "~/nobackup/condor/EEPromptControlRegion_testD0Smearing_2017Analysis_13September2018/Background.root",
+        'file' : "/uscms_data/d3/alimena/condor/EEPromptControlRegion_2017Analysis_10April2019/Background.root",
         'hist' : "PromptControlRegionPlotter/Electron-beamspot Plots/electronUnsmearedD0_50um",
         'fit_range' : (-20, 20), # optional parameter; comment out to fit full range
     },
     {
         'name' : "EE_eD0_Data",
-        'file' : "~/nobackup/condor/EEPromptControlRegion_testD0Smearing_2017Analysis_13September2018/DoubleEG_2017.root",
+        'file' : "/uscms_data/d3/alimena/condor/EEPromptControlRegion_2017Analysis_10April2019/DoubleEG_2017.root",
         'hist' : "PromptControlRegionPlotter/Electron-beamspot Plots/electronUnsmearedD0_50um",
         'fit_range' : (-20, 20), # optional parameter; comment out to fit full range
     },
     # e-mu channel
     {
         'name' : "EMu_eD0_BackgroundMC",
-        'file' : "~/nobackup/condor/EMuPromptControlRegion_testD0Smearing_2017Analysis_13September2018/Background.root",
+        'file' : "/uscms_data/d3/alimena/condor/EMuPromptControlRegion_2017Analysis_10April2019/Background.root",
         'hist' : "PromptControlRegionPlotter/Electron-beamspot Plots/electronUnsmearedD0_50um",
         'fit_range' : (-20, 20), # optional parameter; comment out to fit full range
     },
     {
         'name' : "EMu_eD0_Data",
-        'file' : "~/nobackup/condor/EMuPromptControlRegion_testD0Smearing_2017Analysis_13September2018/MuonEG_2017_withoutB.root",
+        'file' : "/uscms_data/d3/alimena/condor/EMuPromptControlRegion_2017Analysis_10April2019/MuonEG_2017_withoutB.root",
         'hist' : "PromptControlRegionPlotter/Electron-beamspot Plots/electronUnsmearedD0_50um",
         'fit_range' : (-20, 20), # optional parameter; comment out to fit full range
     },
     {
         'name' : "EMu_muD0_BackgroundMC",
-        'file' : "~/nobackup/condor/EMuPromptControlRegion_testD0Smearing_2017Analysis_13September2018/Background.root",
+        'file' : "/uscms_data/d3/alimena/condor/EMuPromptControlRegion_2017Analysis_10April2019/Background.root",
         'hist' : "PromptControlRegionPlotter/Muon-beamspot Plots/muonUnsmearedD0_50um",
         'fit_range' : (-20, 20), # optional parameter; comment out to fit full range
     },
     {
         'name' : "EMu_muD0_Data",
-        'file' : "~/nobackup/condor/EMuPromptControlRegion_testD0Smearing_2017Analysis_13September2018/MuonEG_2017_withoutB.root",
+        'file' : "/uscms_data/d3/alimena/condor/EMuPromptControlRegion_2017Analysis_10April2019/MuonEG_2017_withoutB.root",
         'hist' : "PromptControlRegionPlotter/Muon-beamspot Plots/muonUnsmearedD0_50um",
         'fit_range' : (-20, 20), # optional parameter; comment out to fit full range
     },
     # mu-mu channel
     {
         'name' : "MuMu_muD0_BackgroundMC",
-        'file' : "~/nobackup/condor/MuMuPromptControlRegion_testD0Smearing_2017Analysis_13September2018/Background.root",
+        'file' : "/uscms_data/d3/alimena/condor/MuMuPromptControlRegion_2017Analysis_10April2019/Background.root",
         'hist' : "PromptControlRegionPlotter/Muon-beamspot Plots/muonUnsmearedD0_50um",
         'fit_range' : (-20, 20), # optional parameter; comment out to fit full range
     },
     {
         'name' : "MuMu_muD0_Data",
-        'file' : "~/nobackup/condor/MuMuPromptControlRegion_testD0Smearing_2017Analysis_13September2018/DoubleMu_2017_withoutB.root",
+        'file' : "/uscms_data/d3/alimena/condor/MuMuPromptControlRegion_2017Analysis_10April2019/DoubleMu_2017_withoutB.root",
         'hist' : "PromptControlRegionPlotter/Muon-beamspot Plots/muonUnsmearedD0_50um",
         'fit_range' : (-20, 20), # optional parameter; comment out to fit full range
     },
