6d43aa3006d2728feb1074b2bb57e7ae18da7415
Juliette Alimena <juliette.alimena@cern.ch>
2019-04-19 06:47:45 -0500

    add test input files

--- SignalMC/test/step4_PAT_2016MC_miniAODv3.py
+++ SignalMC/test/step4_PAT_2016MC_miniAODv3.py
@@ -1,7 +1,7 @@
 # Auto generated configuration file
-# using: 
-# Revision: 1.19 
-# Source: /local/reps/CMSSW/CMSSW/Configuration/Applications/python/ConfigBuilder.py,v 
+# using:
+# Revision: 1.19
+# Source: /local/reps/CMSSW/CMSSW/Configuration/Applications/python/ConfigBuilder.py,v
 # with command line options: step1 --filein dbs:/DisplacedSUSY_StopToBL_M-100_CTau-100_TuneCUETP8M1_13TeV_pythia8/RunIISummer16DR80Premix-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/AODSIM --fileout file:EXO-RunIISummer16MiniAODv3-09483.root --mc --eventcontent MINIAODSIM --runUnscheduled --datatier MINIAODSIM --conditions 94X_mcRun2_asymptotic_v3 --step PAT --nThreads 8 --era Run2_2016,run2_miniAOD_80XLegacy --no_exec
 import FWCore.ParameterSet.Config as cms
 
@@ -23,34 +23,39 @@ process.load('Configuration.StandardSequences.EndOfProcess_cff')
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
 
 process.maxEvents = cms.untracked.PSet(
-    input = cms.untracked.int32(1)
+    input = cms.untracked.int32(10)
 )
 
 # Input source
 process.source = cms.Source("PoolSource",
-    fileNames = cms.untracked.vstring('/store/mc/RunIISummer16DR80Premix/DisplacedSUSY_StopToBL_M-100_CTau-100_TuneCUETP8M1_13TeV_pythia8/AODSIM/PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/70000/4C14C9B3-2320-E711-A8D3-0242AC130004.root', 
-        '/store/mc/RunIISummer16DR80Premix/DisplacedSUSY_StopToBL_M-100_CTau-100_TuneCUETP8M1_13TeV_pythia8/AODSIM/PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/70000/2EAD60C6-2620-E711-B4EC-0CC47A537688.root', 
-        '/store/mc/RunIISummer16DR80Premix/DisplacedSUSY_StopToBL_M-100_CTau-100_TuneCUETP8M1_13TeV_pythia8/AODSIM/PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/70000/625D2B64-B620-E711-97AF-0242AC130002.root', 
-        '/store/mc/RunIISummer16DR80Premix/DisplacedSUSY_StopToBL_M-100_CTau-100_TuneCUETP8M1_13TeV_pythia8/AODSIM/PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/70000/C4804772-B620-E711-B838-6C3BE5B5B340.root', 
-        '/store/mc/RunIISummer16DR80Premix/DisplacedSUSY_StopToBL_M-100_CTau-100_TuneCUETP8M1_13TeV_pythia8/AODSIM/PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/70000/74052E42-1220-E711-9A68-D067E5F90F2A.root', 
-        '/store/mc/RunIISummer16DR80Premix/DisplacedSUSY_StopToBL_M-100_CTau-100_TuneCUETP8M1_13TeV_pythia8/AODSIM/PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/70000/B656A45D-1F20-E711-A6AA-0242AC130004.root', 
-        '/store/mc/RunIISummer16DR80Premix/DisplacedSUSY_StopToBL_M-100_CTau-100_TuneCUETP8M1_13TeV_pythia8/AODSIM/PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/70000/6ECEEF27-1B20-E711-8329-001E674DA347.root', 
-        '/store/mc/RunIISummer16DR80Premix/DisplacedSUSY_StopToBL_M-100_CTau-100_TuneCUETP8M1_13TeV_pythia8/AODSIM/PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/70000/A88CB7A7-2620-E711-9D01-0242AC130005.root', 
-        '/store/mc/RunIISummer16DR80Premix/DisplacedSUSY_StopToBL_M-100_CTau-100_TuneCUETP8M1_13TeV_pythia8/AODSIM/PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/70000/56320908-B220-E711-87DB-A0369F3102B6.root', 
-        '/store/mc/RunIISummer16DR80Premix/DisplacedSUSY_StopToBL_M-100_CTau-100_TuneCUETP8M1_13TeV_pythia8/AODSIM/PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/70000/603A8D4F-B620-E711-888A-0CC47A706DC0.root', 
-        '/store/mc/RunIISummer16DR80Premix/DisplacedSUSY_StopToBL_M-100_CTau-100_TuneCUETP8M1_13TeV_pythia8/AODSIM/PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/70000/8A8D6B4E-B620-E711-BF27-008CFA197A5C.root', 
-        '/store/mc/RunIISummer16DR80Premix/DisplacedSUSY_StopToBL_M-100_CTau-100_TuneCUETP8M1_13TeV_pythia8/AODSIM/PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/70000/3050AE52-B620-E711-A7D9-008CFA197CCC.root', 
-        '/store/mc/RunIISummer16DR80Premix/DisplacedSUSY_StopToBL_M-100_CTau-100_TuneCUETP8M1_13TeV_pythia8/AODSIM/PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/70000/7666662A-B620-E711-B78E-0025905C43EA.root', 
-        '/store/mc/RunIISummer16DR80Premix/DisplacedSUSY_StopToBL_M-100_CTau-100_TuneCUETP8M1_13TeV_pythia8/AODSIM/PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/70000/D2DE4283-B620-E711-9882-1866DA89095D.root', 
-        '/store/mc/RunIISummer16DR80Premix/DisplacedSUSY_StopToBL_M-100_CTau-100_TuneCUETP8M1_13TeV_pythia8/AODSIM/PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/70000/A2888D55-B620-E711-BC58-20CF305B050E.root', 
-        '/store/mc/RunIISummer16DR80Premix/DisplacedSUSY_StopToBL_M-100_CTau-100_TuneCUETP8M1_13TeV_pythia8/AODSIM/PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/70000/2EB3447E-B620-E711-861F-00259029E720.root', 
-        '/store/mc/RunIISummer16DR80Premix/DisplacedSUSY_StopToBL_M-100_CTau-100_TuneCUETP8M1_13TeV_pythia8/AODSIM/PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/70000/74286949-B620-E711-85A6-0CC47A5FBE35.root', 
-        '/store/mc/RunIISummer16DR80Premix/DisplacedSUSY_StopToBL_M-100_CTau-100_TuneCUETP8M1_13TeV_pythia8/AODSIM/PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/70000/E208EB6E-B620-E711-8B68-20CF307C98DC.root', 
-        '/store/mc/RunIISummer16DR80Premix/DisplacedSUSY_StopToBL_M-100_CTau-100_TuneCUETP8M1_13TeV_pythia8/AODSIM/PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/70000/6E07842D-B620-E711-8FEC-C4346BBCD528.root', 
-        '/store/mc/RunIISummer16DR80Premix/DisplacedSUSY_StopToBL_M-100_CTau-100_TuneCUETP8M1_13TeV_pythia8/AODSIM/PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/70000/38B39B45-B620-E711-894A-141877410ACD.root', 
-        '/store/mc/RunIISummer16DR80Premix/DisplacedSUSY_StopToBL_M-100_CTau-100_TuneCUETP8M1_13TeV_pythia8/AODSIM/PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/70000/8A357150-B620-E711-A816-FA163EFF2427.root', 
-        '/store/mc/RunIISummer16DR80Premix/DisplacedSUSY_StopToBL_M-100_CTau-100_TuneCUETP8M1_13TeV_pythia8/AODSIM/PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/70000/F2F15529-B620-E711-8530-FA163E6559A6.root'),
-    secondaryFileNames = cms.untracked.vstring()
+    fileNames = cms.untracked.vstring(
+#'file:/eos/uscms/store/user/alimena/StopToLB_M_1300_1mm_13TeV_2016MC/Reco/190416_083856/0000/EXO-RunIISummer16DR80Premix-08936_1.root'
+'file:/eos/uscms/store/user/alimena/StopToLB_M_1700_1000mm_13TeV_2016MC/Reco/190416_084027/0000/EXO-RunIISummer16DR80Premix-08936_1.root'
+
+#'/store/mc/RunIISummer16DR80Premix/DisplacedSUSY_StopToBL_M-100_CTau-100_TuneCUETP8M1_13TeV_pythia8/AODSIM/PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/70000/4C14C9B3-2320-E711-A8D3-0242AC130004.root',
+ #       '/store/mc/RunIISummer16DR80Premix/DisplacedSUSY_StopToBL_M-100_CTau-100_TuneCUETP8M1_13TeV_pythia8/AODSIM/PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/70000/2EAD60C6-2620-E711-B4EC-0CC47A537688.root',
+  #      '/store/mc/RunIISummer16DR80Premix/DisplacedSUSY_StopToBL_M-100_CTau-100_TuneCUETP8M1_13TeV_pythia8/AODSIM/PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/70000/625D2B64-B620-E711-97AF-0242AC130002.root',
+   #     '/store/mc/RunIISummer16DR80Premix/DisplacedSUSY_StopToBL_M-100_CTau-100_TuneCUETP8M1_13TeV_pythia8/AODSIM/PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/70000/C4804772-B620-E711-B838-6C3BE5B5B340.root',
+    #    '/store/mc/RunIISummer16DR80Premix/DisplacedSUSY_StopToBL_M-100_CTau-100_TuneCUETP8M1_13TeV_pythia8/AODSIM/PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/70000/74052E42-1220-E711-9A68-D067E5F90F2A.root',
+        #'/store/mc/RunIISummer16DR80Premix/DisplacedSUSY_StopToBL_M-100_CTau-100_TuneCUETP8M1_13TeV_pythia8/AODSIM/PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/70000/B656A45D-1F20-E711-A6AA-0242AC130004.root',
+        #'/store/mc/RunIISummer16DR80Premix/DisplacedSUSY_StopToBL_M-100_CTau-100_TuneCUETP8M1_13TeV_pythia8/AODSIM/PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/70000/6ECEEF27-1B20-E711-8329-001E674DA347.root',
+        #'/store/mc/RunIISummer16DR80Premix/DisplacedSUSY_StopToBL_M-100_CTau-100_TuneCUETP8M1_13TeV_pythia8/AODSIM/PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/70000/A88CB7A7-2620-E711-9D01-0242AC130005.root',
+        #'/store/mc/RunIISummer16DR80Premix/DisplacedSUSY_StopToBL_M-100_CTau-100_TuneCUETP8M1_13TeV_pythia8/AODSIM/PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/70000/56320908-B220-E711-87DB-A0369F3102B6.root',
+        #'/store/mc/RunIISummer16DR80Premix/DisplacedSUSY_StopToBL_M-100_CTau-100_TuneCUETP8M1_13TeV_pythia8/AODSIM/PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/70000/603A8D4F-B620-E711-888A-0CC47A706DC0.root',
+        #'/store/mc/RunIISummer16DR80Premix/DisplacedSUSY_StopToBL_M-100_CTau-100_TuneCUETP8M1_13TeV_pythia8/AODSIM/PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/70000/8A8D6B4E-B620-E711-BF27-008CFA197A5C.root',
+        #'/store/mc/RunIISummer16DR80Premix/DisplacedSUSY_StopToBL_M-100_CTau-100_TuneCUETP8M1_13TeV_pythia8/AODSIM/PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/70000/3050AE52-B620-E711-A7D9-008CFA197CCC.root',
+        #'/store/mc/RunIISummer16DR80Premix/DisplacedSUSY_StopToBL_M-100_CTau-100_TuneCUETP8M1_13TeV_pythia8/AODSIM/PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/70000/7666662A-B620-E711-B78E-0025905C43EA.root',
+        #'/store/mc/RunIISummer16DR80Premix/DisplacedSUSY_StopToBL_M-100_CTau-100_TuneCUETP8M1_13TeV_pythia8/AODSIM/PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/70000/D2DE4283-B620-E711-9882-1866DA89095D.root',
+        #'/store/mc/RunIISummer16DR80Premix/DisplacedSUSY_StopToBL_M-100_CTau-100_TuneCUETP8M1_13TeV_pythia8/AODSIM/PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/70000/A2888D55-B620-E711-BC58-20CF305B050E.root',
+        #'/store/mc/RunIISummer16DR80Premix/DisplacedSUSY_StopToBL_M-100_CTau-100_TuneCUETP8M1_13TeV_pythia8/AODSIM/PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/70000/2EB3447E-B620-E711-861F-00259029E720.root',
+        #'/store/mc/RunIISummer16DR80Premix/DisplacedSUSY_StopToBL_M-100_CTau-100_TuneCUETP8M1_13TeV_pythia8/AODSIM/PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/70000/74286949-B620-E711-85A6-0CC47A5FBE35.root',
+        #'/store/mc/RunIISummer16DR80Premix/DisplacedSUSY_StopToBL_M-100_CTau-100_TuneCUETP8M1_13TeV_pythia8/AODSIM/PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/70000/E208EB6E-B620-E711-8B68-20CF307C98DC.root',
+        #'/store/mc/RunIISummer16DR80Premix/DisplacedSUSY_StopToBL_M-100_CTau-100_TuneCUETP8M1_13TeV_pythia8/AODSIM/PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/70000/6E07842D-B620-E711-8FEC-C4346BBCD528.root',
+        #'/store/mc/RunIISummer16DR80Premix/DisplacedSUSY_StopToBL_M-100_CTau-100_TuneCUETP8M1_13TeV_pythia8/AODSIM/PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/70000/38B39B45-B620-E711-894A-141877410ACD.root',
+        #'/store/mc/RunIISummer16DR80Premix/DisplacedSUSY_StopToBL_M-100_CTau-100_TuneCUETP8M1_13TeV_pythia8/AODSIM/PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/70000/8A357150-B620-E711-A816-FA163EFF2427.root',
+        #'/store/mc/RunIISummer16DR80Premix/DisplacedSUSY_StopToBL_M-100_CTau-100_TuneCUETP8M1_13TeV_pythia8/AODSIM/PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/70000/F2F15529-B620-E711-8530-FA163E6559A6.root'),
+    #secondaryFileNames = cms.untracked.vstring()
+)
 )
 
 process.options = cms.untracked.PSet(
@@ -59,7 +64,7 @@ process.options = cms.untracked.PSet(
 
 # Production Info
 process.configurationMetadata = cms.untracked.PSet(
-    annotation = cms.untracked.string('step1 nevts:1'),
+    annotation = cms.untracked.string('step1 nevts:10'),
     name = cms.untracked.string('Applications'),
     version = cms.untracked.string('$Revision: 1.19 $')
 )
@@ -81,47 +86,47 @@ process.MINIAODSIMoutput = cms.OutputModule("PoolOutputModule",
     overrideBranchesSplitLevel = cms.untracked.VPSet(cms.untracked.PSet(
         branch = cms.untracked.string('patPackedCandidates_packedPFCandidates__*'),
         splitLevel = cms.untracked.int32(99)
-    ), 
+    ),
         cms.untracked.PSet(
             branch = cms.untracked.string('recoGenParticles_prunedGenParticles__*'),
             splitLevel = cms.untracked.int32(99)
-        ), 
+        ),
         cms.untracked.PSet(
             branch = cms.untracked.string('patTriggerObjectStandAlones_slimmedPatTrigger__*'),
             splitLevel = cms.untracked.int32(99)
-        ), 
+        ),
         cms.untracked.PSet(
             branch = cms.untracked.string('patPackedGenParticles_packedGenParticles__*'),
             splitLevel = cms.untracked.int32(99)
-        ), 
+        ),
         cms.untracked.PSet(
             branch = cms.untracked.string('patJets_slimmedJets__*'),
             splitLevel = cms.untracked.int32(99)
-        ), 
+        ),
         cms.untracked.PSet(
             branch = cms.untracked.string('recoVertexs_offlineSlimmedPrimaryVertices__*'),
             splitLevel = cms.untracked.int32(99)
-        ), 
+        ),
         cms.untracked.PSet(
             branch = cms.untracked.string('recoCaloClusters_reducedEgamma_reducedESClusters_*'),
             splitLevel = cms.untracked.int32(99)
-        ), 
+        ),
         cms.untracked.PSet(
             branch = cms.untracked.string('EcalRecHitsSorted_reducedEgamma_reducedEBRecHits_*'),
             splitLevel = cms.untracked.int32(99)
-        ), 
+        ),
         cms.untracked.PSet(
             branch = cms.untracked.string('EcalRecHitsSorted_reducedEgamma_reducedEERecHits_*'),
             splitLevel = cms.untracked.int32(99)
-        ), 
+        ),
         cms.untracked.PSet(
             branch = cms.untracked.string('recoGenJets_slimmedGenJets__*'),
             splitLevel = cms.untracked.int32(99)
-        ), 
+        ),
         cms.untracked.PSet(
             branch = cms.untracked.string('patJets_slimmedJetsPuppi__*'),
             splitLevel = cms.untracked.int32(99)
-        ), 
+        ),
         cms.untracked.PSet(
             branch = cms.untracked.string('EcalRecHitsSorted_reducedEgamma_reducedESRecHits_*'),
             splitLevel = cms.untracked.int32(99)
@@ -184,7 +189,7 @@ process=convertToUnscheduled(process)
 # customisation of the process.
 
 # Automatic addition of the customisation function from PhysicsTools.PatAlgos.slimming.miniAOD_tools
-from PhysicsTools.PatAlgos.slimming.miniAOD_tools import miniAOD_customizeAllMC 
+from PhysicsTools.PatAlgos.slimming.miniAOD_tools import miniAOD_customizeAllMC
 
 #call to customisation function miniAOD_customizeAllMC imported from PhysicsTools.PatAlgos.slimming.miniAOD_tools
 process = miniAOD_customizeAllMC(process)
