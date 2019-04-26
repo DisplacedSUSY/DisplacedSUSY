429d36efeb5e3ca3dd310713f7f0de6bebb7ccaf
Juliette Alimena <juliette.alimena@cern.ch>
2019-04-26 15:14:31 -0500

    replace multicrab with individual crab configs for miniaod 2017 MC

---
+++ SignalMC/test/crab_step4_stopToLB_M_400_1mm_2017MC.py
@@ -0,0 +1,23 @@
+from CRABClient.UserUtilities import config, getUsernameFromSiteDB
+config = config()
+
+
+config.General.requestName = 'stopToLB_M_400_1mm_13TeV_2017MC_MiniAod'
+config.General.workArea = 'crab'
+config.General.transferOutputs = True
+config.General.transferLogs = True
+
+config.JobType.pluginName = 'Analysis'
+config.JobType.psetName = 'step4_PAT_2017MC.py'
+config.JobType.maxMemoryMB = 8000
+config.JobType.numCores = 4
+
+config.Data.inputDataset = '/StopToLB_M_400_1mm_13TeV_2017MC/jalimena-Reco-ce5bdde49b39cbb6b29d59a381558487/USER'
+config.Data.outputDatasetTag = 'MiniAod'
+config.Data.inputDBS = 'phys03'
+config.Data.splitting = 'FileBased'
+config.Data.unitsPerJob = 1
+config.Data.outLFNDirBase = '/store/user/%s/' % (getUsernameFromSiteDB())
+config.Data.publication = True
+
+config.Site.storageSite = 'T3_US_FNALLPC'
