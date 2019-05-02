dc7fe58df97e6978ea19aeac6ba7d39d4523de53
Bryan <cardwell.80@osu.edu>
2019-05-01 23:25:53 -0500

    Update improved abcd script to make fit summary plots

--- Configuration/scripts/improved_abcd.py
+++ Configuration/scripts/improved_abcd.py
@@ -6,7 +6,7 @@ import re
 from array import array
 from optparse import OptionParser
 from DisplacedSUSY.Configuration.helperFunctions import propagateError
-from ROOT import TFile, TF1, TCanvas, Double, gStyle, gROOT, TLine, TGraphAsymmErrors, TMultiGraph
+from ROOT import TFile, TF1, TCanvas, Double, gStyle, gPad, gROOT, TLine, TGraphAsymmErrors, TMultiGraph, TLegend
 
 parser = OptionParser()
 parser.add_option("-l", "--localConfig", dest="localConfig",
@@ -36,7 +36,7 @@ else:
 
 gROOT.SetBatch(True)
 gStyle.SetOptStat(1)
-gStyle.SetOptFit(1)
+gStyle.SetOptFit(0)
 gStyle.SetCanvasBorderMode(0)
 gStyle.SetPadBorderMode(0)
 gStyle.SetPadColor(0)
@@ -46,77 +46,132 @@ gStyle.SetPaintTextFormat('6.4f')
 gStyle.SetStatFormat('6.4f')
 gStyle.SetFitFormat('6.4f')
 gROOT.ForceStyle()
+# select high contrast TColors for summary plots
+colors = [x for x in range(1, 10)] + [11, 12, 28, 29, 33, 38, 41, 42, 43, 45, 46]
 
 out_file = TFile(output_path + "improved_abcd_results.root", "recreate")
 fit_results = {}
 c_yields = {}
 
 for sample in samples:
-    print "\n" + sample
-    in_hists = {}
-    in_file = TFile(output_path + sample + ".root")
+    color_ix = 0
+    fit_summary_plot = TMultiGraph()
+    fit_legend = TLegend(0.4, 0.5, 0.89, 0.89)
+    for fit_range in fit_ranges:
+        print "\n" + sample
+        in_hists = {}
+        in_file = TFile(output_path + sample + ".root")
 
-    # get histograms and statistical uncertainties for all regions
-    for region, channel in channels.iteritems():
-        in_hists[region] = in_file.Get(channel + "Plotter/" + input_hist)
-        in_hists[region] = in_hists[region].Rebin(len(bin_edges)-1, in_hists[region].GetName(), array('d', bin_edges))
-        if not in_hists[region]:
-            print "Warning: could not load histogram"
+        # get histograms and statistical uncertainties for all regions
+        for region, channel in channels.iteritems():
+            in_hists[region] = in_file.Get(channel + "Plotter/" + input_hist)
+            in_hists[region] = in_hists[region].Rebin(len(bin_edges)-1, in_hists[region].GetName(), array('d', bin_edges))
+            if not in_hists[region]:
+                print "Warning: could not load histogram"
 
-    # plot A/B and C/D
-    b_over_a_hist = TGraphAsymmErrors(in_hists['b'], in_hists['a'], "pois")
-    d_over_c_hist = TGraphAsymmErrors(in_hists['d'], in_hists['c'], "pois")
+        # plot A/B and C/D
+        b_over_a_plot = TGraphAsymmErrors(in_hists['b'], in_hists['a'], "pois")
+        d_over_c_plot = TGraphAsymmErrors(in_hists['d'], in_hists['c'], "pois")
 
-    # fit A/B
-    composite = sample in composite_samples
-    model = composite_model if composite else component_model
-    fit_func = TF1(sample + "_fit", model, 0, in_hists['a'].GetXaxis().GetXmax())
-    if composite:
-        components = composite_samples[sample]
-        print "Fitting composite dataset:"
-        print "model:", model
-        print "[1] and [2] correspond to " + components[0]
-        print "[3] and [4] correspond to " + components[1]
-        #fit_func.SetParLimits(0, 0, 1) # let weights float
-        fit_func.FixParameter(0, c_yields[components[0]] / (c_yields[components[0]]+c_yields[components[1]])) # fix weights to ratio of events in region c
-        fit_func.FixParameter(1, fit_results[components[0]].GetParameter(0))
-        fit_func.FixParameter(2, fit_results[components[0]].GetParameter(1))
-        fit_func.FixParameter(3, fit_results[components[1]].GetParameter(0))
-        fit_func.FixParameter(4, fit_results[components[1]].GetParameter(1))
-    else:
-        print "Fitting component dataset:"
-        print "model:", model
-        fit_func.SetParLimits(0, 0, 100)
-        fit_func.SetParLimits(1, 0, 100)
+        # fit A/B
+        composite = sample in composite_samples
+        model = composite_model if composite else component_model
+        fit_func = TF1(sample + "_fit", model, 0, in_hists['a'].GetXaxis().GetXmax())
+        if composite:
+            components = composite_samples[sample]
+            print "Fitting composite dataset:"
+            print "model:", model
+            print "[1] and [2] correspond to " + components[0]
+            print "[3] and [4] correspond to " + components[1]
+            #fit_func.SetParLimits(0, 0, 1) # let weights float
+            fit_func.FixParameter(0, c_yields[components[0]] / (c_yields[components[0]]+c_yields[components[1]])) # fix weights to ratio of events in region c
+            fit_func.FixParameter(1, fit_results[components[0]].GetParameter(0))
+            fit_func.FixParameter(2, fit_results[components[0]].GetParameter(1))
+            fit_func.FixParameter(3, fit_results[components[1]].GetParameter(0))
+            fit_func.FixParameter(4, fit_results[components[1]].GetParameter(1))
+        else:
+            print "Fitting component dataset:"
+            print "model:", model
+            fit_func.SetParLimits(0, 0, 100)
+            fit_func.SetParLimits(1, 0, 100)
 
-    b_over_a_hist.Fit(fit_func, "", "", fit_range[0], fit_range[1])
-    fit = b_over_a_hist.GetFunction(sample+"_fit")
-    fit_results[sample] = fit.Clone()
-    c_yields[sample] = in_hists["c"].Integral()
+        b_over_a_plot.Fit(fit_func, "", "", fit_range[0], fit_range[1])
+        fit = b_over_a_plot.GetFunction(sample+"_fit")
+        fit_results[sample] = fit.Clone()
+        c_yields[sample] = in_hists["c"].Integral()
 
-    # calculate d(pT) = c(pT) * model(pT)
-    transfer_factor_hist = in_hists['c'].Clone()
-    for b in range(1, in_hists["c"].GetNbinsX()+1):
-        transfer_factor_hist.SetBinContent(b, fit.Eval(transfer_factor_hist.GetBinCenter(b)))
-        transfer_factor_hist.SetBinError(b, 0) # fixme: account for fit uncertainty
-    d_estimate_hist = in_hists['c'].Clone()
-    d_estimate_hist.Multiply(transfer_factor_hist)
+        # calculate d(pT) = c(pT) * model(pT)
+        transfer_factor_hist = in_hists['c'].Clone()
+        for b in range(1, in_hists["c"].GetNbinsX()+1):
+            transfer_factor_hist.SetBinContent(b, fit.Eval(transfer_factor_hist.GetBinCenter(b)))
+            transfer_factor_hist.SetBinError(b, 0) # fixme: account for fit uncertainty
+        d_estimate_hist = in_hists['c'].Clone()
+        d_estimate_hist.Multiply(transfer_factor_hist)
 
-    # print closure test results
-    # fixme: need to account for overflow bin
-    error = Double()
-    print "estimate:", round(d_estimate_hist.IntegralAndError(0, d_estimate_hist.GetNbinsX(), error),2), "+-", round(error,2)
-    if arguments.unblind:
-        print "actual:", round(in_hists["d"].IntegralAndError(0, in_hists["d"].GetNbinsX(), error),2), "+-", round(error,2)
-    else:
-        print "actual: BLINDED"
+        # print closure test results
+        # fixme: need to account for overflow bin
+        error = Double()
+        estimate = d_estimate_hist.IntegralAndError(0, d_estimate_hist.GetNbinsX(), error)
+        print "estimate:", round(estimate, 2), "+-", round(error, 2)
+        if arguments.unblind:
+            actual_yield = in_hists["d"].IntegralAndError(0, in_hists["d"].GetNbinsX(), error)
+            actual_error = error
+            print "actual:", round(actual_yield, 2), "+-", round(error, 2)
+        else:
+            print "actual: BLINDED"
 
-    # Save plots
-    out_file.cd()
-    # transfer factor plots
-    transfer_factor_hist.SetTitle(sample + "_tf")
-    transfer_factor_hist.SetName(sample + "_tf")
-    transfer_factor_hist.Write()
+        # Save once-per-fit plots
+        out_file.cd()
+        sample_and_range = sample + "_" + str(fit_range[0])
+        # estimated and actual pT distributions in region D
+        d_canvas = TCanvas(sample_and_range+"_d", sample_and_range+"_d", 100, 100, 700, 600)
+        d_estimate_hist.SetTitle(sample_and_range + " d estimate and actual")
+        d_estimate_hist.SetName(sample_and_range + " d estimate and actual")
+        d_estimate_hist.SetLineColor(2)
+        d_estimate_hist.Draw()
+        if arguments.unblind:
+            in_hists['d'].Draw("sames")
+        d_canvas.Write()
+        # fits and combined B/A and D/C plots
+        fit_canvas = TCanvas(sample_and_range+"_fit", sample_and_range+"_fit", 100, 100, 700, 600)
+        b_over_a_plot.PaintStats(fit)
+        fit_plot = TMultiGraph()
+        fit_plot.Add(b_over_a_plot, "P")
+        if arguments.unblind:
+            fit_plot.Add(d_over_c_plot, "P")
+        fit_plot.Draw("A")
+        fit_plot.SetTitle(sample_and_range + " fit")
+        fit_plot.SetName(sample_and_range + " fit")
+        fit_plot.GetYaxis().SetTitle("B/A or D/C")
+        fit_plot.GetYaxis().SetTitleOffset(1.4)
+        fit_plot.GetYaxis().SetLabelSize(0.025)
+        fit.SetRange(0, 500)
+        line = TLine()
+        line.SetLineWidth(2)
+        line.SetLineColor(4)
+        line.SetLineStyle(9)
+        gPad.Update()
+        line.DrawLine(fit_range[0], 0, fit_range[0], gPad.GetUymax())
+        line.DrawLine(fit_range[1], 0, fit_range[1], gPad.GetUymax())
+        fit_canvas.Write()
+        # add fit plots to summary plot
+        fit.SetLineColor(colors[color_ix])
+        b_over_a_clone = b_over_a_plot.Clone() # clone plots to please pyroot
+        b_over_a_clone.SetMarkerStyle(6)
+        b_over_a_clone.SetMarkerColor(colors[color_ix])
+        b_over_a_clone.SetLineColor(colors[color_ix])
+        fit_summary_plot.Add(b_over_a_clone, "PX")
+        fit_legend.AddEntry(b_over_a_clone, str(fit_range[0]) + " GeV --> " +
+                                           str(round(estimate, 2)) + " events")
+        if arguments.unblind:
+            d_over_c_clone = d_over_c_plot.Clone()
+            d_over_c_clone.SetMarkerStyle(6)
+            d_over_c_clone.SetMarkerColor(colors[color_ix])
+            d_over_c_clone.SetLineColor(colors[color_ix])
+            fit_summary_plot.Add(d_over_c_clone, "PX")
+        color_ix = (color_ix + 1) % len(colors)
+
+    # save once-per-sample plots
     # input hists
     a_canvas = TCanvas(sample+"_a", sample+"_a", 100, 100, 700, 600)
     in_hists['a'].Draw()
@@ -127,41 +182,19 @@ for sample in samples:
     c_canvas = TCanvas(sample+"_c", sample+"_c", 100, 100, 700, 600)
     in_hists['c'].Draw()
     c_canvas.Write()
-    # estimated and actual pT distributions in region D
-    d_canvas = TCanvas(sample+"_d", sample+"_d", 100, 100, 700, 600)
-    d_estimate_hist.SetTitle(sample + " d estimate and actual")
-    d_estimate_hist.SetName(sample + " d estimate and actual")
-    d_estimate_hist.SetLineColor(2)
-    d_estimate_hist.Draw()
-    if arguments.unblind:
-        in_hists['d'].Draw("sames")
-    d_canvas.Write()
-    # fits and combined B/A and D/C plots
-    fit_canvas = TCanvas(sample + " fit", sample + " fit", 100, 100, 700, 600)
-    b_over_a_hist.PaintStats(fit)
-    badc_mg = TMultiGraph()
-    badc_mg.Add(b_over_a_hist)
-    if arguments.unblind:
-        badc_mg.Add(d_over_c_hist)
-    badc_mg.Draw("AP")
-    badc_mg.SetTitle(sample + " fit")
-    badc_mg.SetName(sample + " fit")
-    if "QCD" in sample:
-        y_range = 0.5
-    else:
-        y_range = 0.005
-    badc_mg.GetYaxis().SetRangeUser(0, y_range)
-    badc_mg.GetYaxis().SetTitle("B/A or D/C")
-    badc_mg.GetYaxis().SetTitleOffset(1.4)
-    badc_mg.GetYaxis().SetLabelSize(0.025)
-    fit.SetRange(0, 500)
-    line = TLine()
-    line.SetLineWidth(2)
-    line.SetLineColor(4)
-    line.SetLineStyle(9)
-    line.DrawLine(fit_range[0], 0, fit_range[0], y_range)
-    line.DrawLine(fit_range[1], 0, fit_range[1], y_range)
-    fit_canvas.Write()
-    #fit_canvas.SaveAs(sample+"_fit.pdf")
+    # fit summary plot
+    fit_summary_canvas = TCanvas(sample+"_fit_summary", sample+"_fit_summary", 100, 100, 700, 600)
+    fit_summary_plot.Draw("A")
+    fit_summary_plot.SetTitle(sample + " fit summary")
+    fit_summary_plot.SetName(sample + " fit summary")
+    fit_summary_plot.GetYaxis().SetTitle("B/A or D/C")
+    fit_summary_plot.GetYaxis().SetTitleOffset(1.4)
+    fit_summary_plot.GetYaxis().SetLabelSize(0.025)
+    fit_legend.SetHeader("Fit Range Lower Bound --> Corresponding BG Estimate (Actual yield is "
+                     + str(round(actual_yield, 2)) + " +- " + str(round(actual_error, 2)) + ")")
+    fit_legend.SetTextSize(0.015)
+    fit_legend.Draw()
+    line.DrawLine(fit_range[1], 0, fit_range[1], gPad.GetUymax())
+    fit_summary_canvas.Write()
 
 out_file.Close()
--- MuMuChannel/test/improved_abcd_cfg.py
+++ MuMuChannel/test/improved_abcd_cfg.py
@@ -2,18 +2,18 @@
 
 # composite samples must be listed after component samples
 samples = [
-    'DYJetsToLL',
-    'TTJets_Lept',
-    'SingleTop',
-    'Diboson',
-    'QCD_MuEnriched',
+    #'DYJetsToLL',
+    #'TTJets_Lept',
+    #'SingleTop',
+    #'Diboson',
+    #'QCD_MuEnriched',
     'NonQcdBackground',
-    'Background',
+    #'Background',
 ]
 
 # fit assumes composite samples have two components
 composite_samples = {
-    'Background' : ['NonQcdBackground', 'QCD_MuEnriched']
+    #'Background' : ['NonQcdBackground', 'QCD_MuEnriched']
 }
 
 # specifify which channel will be used for each region b=a*f(x), d=c*f(x)
@@ -25,7 +25,7 @@ channels = {
 }
 
 input_hist = "Muon Plots/muonLeadingPt"
-bin_edges = [0,40,50,60,70,80,90,100,125,150,200,250,500]
+bin_edges = [0] + [x for x in range(40, 100, 2)] + [100,125,150,200,250,500]
+fit_ranges = [(x, 100) for x in range(40, 71, 2)]
 component_model = "[0] + [1]/x" # |d0| resolution as a function of pT
 composite_model = "[0] * ([1] + [2]/x) + (1 - [0]) * ([3] + [4]/x)"
-fit_range = (40, 100)
