2c1104edfe6036c0c57e5c0e5c192f83cbe303b4
Bryan <cardwell.80@osu.edu>
2019-07-23 08:10:42 -0500

    Remove repeated value in improved abcd 3D config

--- MuMuChannel/test/improved_abcd_3Dhist_cfg.py
+++ MuMuChannel/test/improved_abcd_3Dhist_cfg.py
@@ -33,7 +33,7 @@ elif (os.environ["CMSSW_VERSION"].startswith ("CMSSW_9_4_") or os.environ["CMSSW
     fitMin = 50 #muon pt cut at 50 GeV in 2017 and 2018 mumu
 
 input_hist = "Muon-beamspot Plots/muonAbsD0[0]_vs_muonAbsD0[1]_2000um_vs_muonPt[0]"
-fit_ranges = [(x, 100) for x in range(fitMin, 71, 2)]
+fit_ranges = [(x, pt_cut) for x in range(fitMin, 71, 2)]
 error_tolerance = 0.1 # maximum error/bin content ratio for b/a plot
 
 
@@ -51,5 +51,5 @@ error_tolerance = 0.1 # maximum error/bin content ratio for b/a plot
 #    fitMin = 50 #muon pt cut at 50 GeV in 2017 and 2018 mumu
 #
 #input_hist = "Muon-beamspot Plots/muonAbsD0[0]_vs_muonAbsD0[1]_2000um_vs_muonPt[0]"
-#fit_ranges = [(x, 120) for x in range(fitMin, 91, 2)]
+#fit_ranges = [(x, pt_cut) for x in range(fitMin, 91, 2)]
 #error_tolerance = 0.1 # maximum error/bin content ratio for b/a plot
