06139c22ccdccf1b717bcd9acd902bc23c9b814b
Bryan <cardwell.80@osu.edu>
2019-07-23 04:35:53 -0500

    Fix typo

--- Configuration/scripts/improved_abcd.py
+++ Configuration/scripts/improved_abcd.py
@@ -343,9 +343,9 @@ for sample in samples:
         fit_plot.Add(b_over_a_plot, "P")
         if arguments.unblind:
             d_over_c = RatioPlot(in_hists['d'], in_hists['c'])
+            upper_edge = pt_max if arguments.threeD else in_hists['c'].GetXaxis().GetXmax()
             # use 2*error_tolerance due to lower stats in c and d regions
-            max_bin = max_bin_z if arguments.threeD else in_hists['c'].GetXaxis().GetXmax()
-            d_over_c.improve_binning(error_tolerance*2, max_bin)
+            d_over_c.improve_binning(error_tolerance*2, upper_edge)
             d_over_c_plot = d_over_c.get_plot()
             fit_plot.Add(d_over_c_plot, "P")
         fit_plot.Draw("A")
