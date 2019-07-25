c7945f82412b2304dcd8e936f982a23c740a52d6
Juliette Alimena <juliette.alimena@cern.ch>
2019-07-25 05:55:27 -0500

    add sideband tests tried for 2017F ee data

--- EEChannel/test/improved_abcd_3Dhist_cfg.py
+++ EEChannel/test/improved_abcd_3Dhist_cfg.py
@@ -19,13 +19,21 @@ composite_samples = {
 }
 
 # the following parameters only take effect when using the -t option
-d0_0_cut = 15.
-d0_1_cut = 15.
-#pt_cut = 100.
+# 1st sideband tried:
+#d0_0_cut = 15.
+#d0_1_cut = 15.
+#pt_cut = 120.
+#d0_0_max = 30 # set to 0 to remove upper limit
+#d0_1_max = 30 # set to 0 to remove upper limit
+#pt_max = 0 # set to 0 to remove upper limit
+
+# 2nd sideband tried:
+d0_0_cut = 30.
+d0_1_cut = 30.
 pt_cut = 120.
-d0_0_max = 30 # set to 0 to remove upper limit
-d0_1_max = 30 # set to 0 to remove upper limit
-pt_max = 0 # set to 0 to remove upper limit
+d0_0_max = 100 # set to 0 to remove upper limit
+d0_1_max = 100 # set to 0 to remove upper limit
+pt_max = 300 # set to 0 to remove upper limit
 
 if os.environ["CMSSW_VERSION"].startswith ("CMSSW_8_0_"):
     fitMin = 65 #electron pt cut at 65 GeV in 2016 ee
@@ -33,5 +41,5 @@ elif (os.environ["CMSSW_VERSION"].startswith ("CMSSW_9_4_") or os.environ["CMSSW
     fitMin = 75 #electron pt cut at 75 GeV in 2017 and 2018 ee
 
 input_hist = "PreselectionPlotter/Electron-beamspot Plots/electronAbsD0[0]_vs_electronAbsD0[1]_2000um_vs_electronPt[0]"
-fit_ranges = [(x, pt_cut) for x in range(fitMin, 81, 2)]
+fit_ranges = [(x, pt_cut) for x in range(fitMin, 91, 2)]
 error_tolerance = 0.1 # maximum error/bin content ratio for b/a plot
