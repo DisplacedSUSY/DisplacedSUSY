c0f7063f7332921de1d3aefb14ac959198d4174c
estefanynunez <nunezornelas.1@osu.edu>
2019-10-14 10:08:56 -0500

    generating stop signal samples: step 2 of 4 config files

---
+++ SignalMC/test/crab_step2_stopToLBottom_M_150_1mm.py
@@ -0,0 +1,25 @@
+from CRABClient.UserUtilities import config, getUsernameFromSiteDB
+config = config()
+
+
+config.General.requestName = 'stopToLBottom_M_150_1mm_13TeV_2018MC_DigiRawHlt'
+config.General.workArea = 'crab'
+config.General.transferOutputs = True
+config.General.transferLogs = True
+
+config.JobType.pluginName = 'Analysis'
+config.JobType.psetName = 'step2_DIGI_DATAMIX_L1_DIGI2RAW_HLT_2018MC.py'
+config.JobType.maxMemoryMB = 8000
+config.JobType.numCores = 8
+
+config.Data.inputDataset = '/StopToLBottom_M_150_1mm_13TeV_2018MC/manunezo-GenSim-0257f17fa318bfeb3d6e674515a9c0ca/USER'
+config.Data.outputDatasetTag = 'DigiRawHlt'
+config.Data.inputDBS = 'phys03'
+config.Data.splitting = 'FileBased'
+config.Data.unitsPerJob = 1
+config.Data.outLFNDirBase = '/store/user/manunezo/'
+config.Data.publication = True
+config.Data.ignoreLocality = True
+
+config.Site.whitelist = ["T1_US_FNAL"]
+config.Site.storageSite = 'T3_US_FNALLPC'
---
+++ SignalMC/test/crab_step2_stopToLBottom_M_175_1000mm.py
@@ -0,0 +1,25 @@
+from CRABClient.UserUtilities import config, getUsernameFromSiteDB
+config = config()
+
+
+config.General.requestName = 'stopToLBottom_M_175_1000mm_13TeV_2018MC_DigiRawHlt'
+config.General.workArea = 'crab'
+config.General.transferOutputs = True
+config.General.transferLogs = True
+
+config.JobType.pluginName = 'Analysis'
+config.JobType.psetName = 'step2_DIGI_DATAMIX_L1_DIGI2RAW_HLT_2018MC.py'
+config.JobType.maxMemoryMB = 8000
+config.JobType.numCores = 8
+
+config.Data.inputDataset = '/StopToLBottom_M_175_1000mm_13TeV_2018MC/manunezo-GenSim-0ed02f452d5a4e21ddc53f1a8c85c095/USER'
+config.Data.outputDatasetTag = 'DigiRawHlt'
+config.Data.inputDBS = 'phys03'
+config.Data.splitting = 'FileBased'
+config.Data.unitsPerJob = 1
+config.Data.outLFNDirBase = '/store/user/manunezo/'
+config.Data.publication = True
+config.Data.ignoreLocality = True
+
+config.Site.whitelist = ["T1_US_FNAL"]
+config.Site.storageSite = 'T3_US_FNALLPC'
