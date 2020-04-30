c25c10b2cab3f2fdc067796fe80fcdee8f0dfb07
Juliette Alimena <juliette.alimena@cern.ch>
2020-04-30 11:08:54 -0500

    add a comparison plot config

---
+++ EMuChannel/test/comparisonPlots_DY.py
@@ -0,0 +1,16 @@
+input_sources = [
+    { 'condor_dir' : 'EMuPreselection_newIso_TauTau_2018Analysis_26Apr2020',
+      'dataset' : 'DYJetsToLL',
+      'channel' : 'Preselection2TausFromZ',
+      'legend_entry' : 'inclusive DY sample, select gen tautau',
+      'marker' : 'circle',
+      'color' : 'black',
+  },
+    { 'condor_dir' : 'EMuPreselection_newIso_TauTau_2018Analysis_26Apr2020',
+      'dataset' : 'DYJetsToTauTauLeptonic',
+      'channel' : 'Preselection2TausFromZ',
+      'legend_entry' : 'DY to tautau leptonic sample, select gen tautau',
+      'marker' : 'square',
+      'color' : 'red',
+  }
+]
