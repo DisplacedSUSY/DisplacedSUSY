c63ac6960d5075dc9e308745fcaca60779434281
Juliette Alimena <juliette.alimena@cern.ch>
2020-09-04 08:08:35 -0500

    limits made for 100 to 1800 GeV

--- Configuration/python/limitOptions.py
+++ Configuration/python/limitOptions.py
@@ -39,7 +39,7 @@ datacardCombinations = {
 }
 
 processes = ['stopToLB']
-masses = [m for m in range(200, 1801, 100)]
+masses = [m for m in range(100, 1801, 100)]
 #lifetimes = [10**e for e in range(-1, 4)]
 lifetimes = [b*10**e for e in range(-1, 4) for b in range(1, 10)] + [10000]
 signal_points = ["{}{}_{}mm".format(p, m, l) for p in processes for m in masses for l in lifetimes]
