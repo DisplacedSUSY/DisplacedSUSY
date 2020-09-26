0aec1bb54f9bc68169dbe68ce13c90630a34431b
Juliette Alimena <juliette.alimena@cern.ch>
2020-09-26 05:00:56 -0500

    add crab configs for additional lifetimes for 2018 digi-miniaod steps

---
+++ SignalMC/test/crab_step2_stopToLBottom_M_1000_0p1mm.py
@@ -0,0 +1,25 @@
+from CRABClient.UserUtilities import config
+config = config()
+
+
+config.General.requestName = 'stopToLBottom_M_1000_0p1mm_13TeV_2018MC_DigiRawHlt'
+config.General.workArea = 'crab'
+config.General.transferOutputs = True
+config.General.transferLogs = True
+
+config.JobType.pluginName = 'Analysis'
+config.JobType.psetName = 'step2_DIGI_DATAMIX_L1_DIGI2RAW_HLT_2018MC.py'
+config.JobType.maxMemoryMB = 8000
+config.JobType.numCores = 8
+config.JobType.allowUndistributedCMSSW = True
+
+config.Data.inputDataset = '/StopToLBottom_M_1000_0p1mm_13TeV_2018MC/jalimena-GenSim-51d24ccab0940230915301625a822c78/USER'
+config.Data.outputDatasetTag = 'DigiRawHlt'
+config.Data.inputDBS = 'phys03'
+config.Data.splitting = 'FileBased'
+config.Data.unitsPerJob = 1
+config.Data.publication = True
+config.Data.ignoreLocality = True
+
+config.Site.whitelist = ["T1_US_FNAL"]
+config.Site.storageSite = 'T3_US_FNALLPC'
