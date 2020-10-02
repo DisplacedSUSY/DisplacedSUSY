bba825609f1fc0cf6ac48c5479e0f6883bca531f
Juliette Alimena <juliette.alimena@cern.ch>
2020-10-02 04:25:41 -0500

    add step2 2018 crab configs

---
+++ SignalMC/test/crab_step2_stopToLD_M_600_0p1mm_2018MC.py
@@ -0,0 +1,25 @@
+from CRABClient.UserUtilities import config
+config = config()
+
+
+config.General.requestName = 'stopToLD_M_600_0p1mm_13TeV_2018MC_DigiRawHlt'
+config.General.workArea = 'crab'
+config.General.transferOutputs = True
+config.General.transferLogs = True
+
+config.JobType.pluginName = 'Analysis'
+config.JobType.psetName = 'step2_DIGI_DATAMIX_L1_DIGI2RAW_HLT_2018MC.py'
+config.JobType.maxMemoryMB = 8000
+config.JobType.numCores = 8
+config.JobType.allowUndistributedCMSSW = True
+
+config.Data.inputDataset = '/StopToLD_M_600_0p1mm_13TeV_2018MC/jalimena-GenSim-bef293f25766866109b7742dbbc4a12c/USER'
+config.Data.outputDatasetTag = 'DigiRawHlt'
+config.Data.inputDBS = 'phys03'
+config.Data.splitting = 'FileBased'
+config.Data.unitsPerJob = 1
+config.Data.publication = True
+config.Data.ignoreLocality = True
+
+config.Site.whitelist = ["T1_US_FNAL"]
+config.Site.storageSite = 'T3_US_FNALLPC'
---
+++ SignalMC/test/crab_step2_stopToLD_M_800_0p1mm_2018MC.py
@@ -0,0 +1,25 @@
+from CRABClient.UserUtilities import config
+config = config()
+
+
+config.General.requestName = 'stopToLD_M_800_0p1mm_13TeV_2018MC_DigiRawHlt'
+config.General.workArea = 'crab'
+config.General.transferOutputs = True
+config.General.transferLogs = True
+
+config.JobType.pluginName = 'Analysis'
+config.JobType.psetName = 'step2_DIGI_DATAMIX_L1_DIGI2RAW_HLT_2018MC.py'
+config.JobType.maxMemoryMB = 8000
+config.JobType.numCores = 8
+config.JobType.allowUndistributedCMSSW = True
+
+config.Data.inputDataset = '/StopToLD_M_800_0p1mm_13TeV_2018MC/jalimena-GenSim-27c83ea7ac4a0d23d75e963ae2caf6ca/USER'
+config.Data.outputDatasetTag = 'DigiRawHlt'
+config.Data.inputDBS = 'phys03'
+config.Data.splitting = 'FileBased'
+config.Data.unitsPerJob = 1
+config.Data.publication = True
+config.Data.ignoreLocality = True
+
+config.Site.whitelist = ["T1_US_FNAL"]
+config.Site.storageSite = 'T3_US_FNALLPC'
