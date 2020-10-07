f9760aed13e61df8b3a7bb436b2fda0542b4e6a5
Bryan Cardwell <bryan.cardwell@cern.ch>
2020-10-07 05:40:12 -0500

    Add 2017 stopToLD step2 crab configs

---
+++ SignalMC/test/crab_step2_stopToLD_M_100_0p1mm_2017MC.py
@@ -0,0 +1,25 @@
+from CRABClient.UserUtilities import config
+config = config()
+
+
+config.General.requestName = 'stopToLD_M_100_0p1mm_13TeV_2017MC_DigiRawHlt'
+config.General.workArea = 'crab'
+config.General.transferOutputs = True
+config.General.transferLogs = True
+
+config.JobType.pluginName = 'Analysis'
+config.JobType.psetName = 'step2_DIGIPREMIX_S2_DATAMIX_L1_DIGI2RAW_HLT_2017MC.py'
+config.JobType.maxMemoryMB = 8000
+config.JobType.numCores = 8
+config.JobType.allowUndistributedCMSSW = True
+
+config.Data.inputDataset = '/StopToLD_M_100_0p1mm_13TeV_2017MC/bcardwel-GenSim-97b013eba809730fbb7b9527fef1a508/USER'
+config.Data.outputDatasetTag = 'DigiRawHlt'
+config.Data.inputDBS = 'phys03'
+config.Data.splitting = 'FileBased'
+config.Data.unitsPerJob = 1
+config.Data.publication = True
+config.Data.ignoreLocality = True
+
+config.Site.whitelist = ["T1_US_FNAL"]
+config.Site.storageSite = 'T3_US_FNALLPC'
