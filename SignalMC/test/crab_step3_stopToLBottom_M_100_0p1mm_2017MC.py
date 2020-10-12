6671f9345ae3b636431138c2c5c8f27a2ae3a3e9
Bryan Cardwell <bryan.cardwell@cern.ch>
2020-10-12 04:43:06 -0500

    Add 2017 StopToLB step3 configs

---
+++ SignalMC/test/crab_step3_stopToLBottom_M_100_0p1mm_2017MC.py
@@ -0,0 +1,25 @@
+from CRABClient.UserUtilities import config
+config = config()
+
+
+config.General.requestName = 'stopToLBottom_M_100_0p1mm_13TeV_2017MC_Reco'
+config.General.workArea = 'crab'
+config.General.transferOutputs = True
+config.General.transferLogs = True
+
+config.JobType.pluginName = 'Analysis'
+config.JobType.psetName = ''
+config.JobType.maxMemoryMB = 8000
+config.JobType.numCores = 8
+config.JobType.allowUndistributedCMSSW = True
+
+config.Data.inputDataset = ''
+config.Data.outputDatasetTag = ''
+config.Data.inputDBS = 'phys03'
+config.Data.splitting = 'FileBased'
+config.Data.unitsPerJob = 1
+config.Data.publication = True
+config.Data.ignoreLocality = True
+
+config.Site.whitelist = ["T1_US_FNAL"]
+config.Site.storageSite = 'T3_US_FNALLPC'
