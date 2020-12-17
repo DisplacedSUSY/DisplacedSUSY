68032db226a8223f8310012dd6cc45d15026b1cf
Juliette Alimena <juliette.alimena@cern.ch>
2020-12-17 04:59:09 -0600

    clean up SkimOptions.py

--- MuMuChannel/test/SkimOptions.py
+++ MuMuChannel/test/SkimOptions.py
@@ -5,16 +5,6 @@ from DisplacedSUSY.MuMuChannel.localOptions import *
 config_file = "Skim_cfg.py"
 
 # create list of datasets to process
-if os.environ["CMSSW_VERSION"].startswith ("CMSSW_8_0_"):
-    datasets = default_datasets
-    #datasets.append()
-    datasets.remove('DisplacedSUSYSignal') #all samples ready
-
-elif os.environ["CMSSW_VERSION"].startswith ("CMSSW_9_4_"):
-    datasets = default_datasets
-    #datasets.append()
-    datasets.remove('DisplacedSUSYSignal') #all samples ready
-
-elif os.environ["CMSSW_VERSION"].startswith ("CMSSW_10_2_"):
-    datasets = default_datasets
-    datasets.remove('DisplacedSUSYSignal') #NOT ready for stop-->l+b, NOT ready for stop-->l+d
+datasets = default_datasets
+#datasets.append()
+datasets.remove('DisplacedSUSYSignal')
