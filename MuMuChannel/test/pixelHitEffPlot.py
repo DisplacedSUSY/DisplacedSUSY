b4e519494af6eaef2ba6db430231dc4a8d65de6c
Juliette Alimena <juliette.alimena@cern.ch>
2021-02-11 10:48:50 -0600

    update pixelHitEffPlot.py

--- MuMuChannel/test/pixelHitEffPlot.py
+++ MuMuChannel/test/pixelHitEffPlot.py
@@ -1,24 +1,17 @@
 #!/usr/bin/env python
-from DisplacedSUSY.EMuChannel.localOptions import *
+from DisplacedSUSY.MuMuChannel.localOptions import *
 
 cutName = '>0 pixel hits'
 input_sources = [
     {
-        #'condor_dir' : 'MuMuNum1PixelHitSelWithCosmics_2016Analysis_NoBPTXData_8Feb2021',
-        #'condor_dir_den' : 'MuMuDen1PixelHitSelWithCosmics_2016Analysis_NoBPTXData_8Feb2021',
-        #'condor_dir' : 'MuMuNum1PixelHitSelWithCosmicsEta1p0_2016Analysis_NoBPTXData_10Feb2021',
-        #'condor_dir_den' : 'MuMuDen1PixelHitSelWithCosmicsEta1p0_2016Analysis_NoBPTXData_10Feb2021',
-        #'condor_dir' : 'MuMuNum1PixelHitSelWithCosmicsEta1p0Global_2016Analysis_NoBPTXData_10Feb2021',
-        #'condor_dir_den' : 'MuMuDen1PixelHitSelWithCosmicsEta1p0Global_2016Analysis_NoBPTXData_10Feb2021',
-
-        'condor_dir' : 'MuMuNum1PixelHitSelWithCosmicsPt25Eta1p0PFGlobal_2016Analysis_NoBPTXData_10Feb2021',
-        'condor_dir_den' : 'MuMuDen1PixelHitSelWithCosmicsPt25Eta1p0PFGlobal_2016Analysis_NoBPTXData_10Feb2021',
+        'condor_dir' : 'MuMuNum1PixelHitSelWithCosmicsWithDz15cmCut_2016Analysis_NoBPTXData_10Feb2021',
+        'condor_dir_den' : 'MuMuDen1PixelHitSelWithCosmicsWithDz15cmCut_2016Analysis_NoBPTXData_10Feb2021',
         'dataset' :   'NoBPTX_2016_postHIP',
-        #'condor_dir' : 'MuMuNum1PixelHitSelWithCosmics_2017Analysis_NoBPTXData_10Feb2021',
-        #'condor_dir_den' : 'MuMuDen1PixelHitSelWithCosmics_2017Analysis_NoBPTXData_10Feb2021',
+        #'condor_dir' : 'MuMuNum1PixelHitSelWithCosmics_2017Analysis_NoBPTXData_11Feb2021',
+        #'condor_dir_den' : 'MuMuDen1PixelHitSelWithCosmics_2017Analysis_NoBPTXData_11Feb2021',
         #'dataset' :   'NoBPTX_2017',
-        #'condor_dir' : 'MuMuNum1PixelHitSelWithCosmics_2018Analysis_NoBPTXData_10Feb2021',
-        #'condor_dir_den' : 'MuMuDen1PixelHitSelWithCosmics_2018Analysis_NoBPTXData_10Feb2021',
+        #'condor_dir' : 'MuMuNum1PixelHitSelWithCosmics_2018Analysis_NoBPTXData_11Feb2021',
+        #'condor_dir_den' : 'MuMuDen1PixelHitSelWithCosmics_2018Analysis_NoBPTXData_11Feb2021',
         #'dataset' :   'NoBPTX_2018',
         'den_channel' : 'Den1PixelHitSelWithCosmics',
         'num_channel' : 'Num1PixelHitSelWithCosmics',
@@ -28,12 +21,12 @@ input_sources = [
         'marker' : 'circle',
     },
     {
-        'condor_dir' : 'MuMuNum1PixelHitSelWithCosmics_2016Analysis_CosmicMC_10Feb2021',
-        'condor_dir_den' : 'MuMuDen1PixelHitSelWithCosmics_2016Analysis_CosmicMC_10Feb2021',
-        #'condor_dir' : 'MuMuNum1PixelHitSelWithCosmics_2017Analysis_CosmicMC_10Feb2021',
-        #'condor_dir_den' : 'MuMuDen1PixelHitSelWithCosmics_2017Analysis_CosmicMC_10Feb2021',
-        #'condor_dir' : 'MuMuNum1PixelHitSelWithCosmics_2018Analysis_CosmicMC_10Feb2021',
-        #'condor_dir_den' : 'MuMuDen1PixelHitSelWithCosmics_2018Analysis_CosmicMC_10Feb2021',
+        'condor_dir' : 'MuMuNum1PixelHitSelWithCosmics_2016Analysis_CosmicMC_11Feb2021',
+        'condor_dir_den' : 'MuMuDen1PixelHitSelWithCosmics_2016Analysis_CosmicMC_11Feb2021',
+        #'condor_dir' : 'MuMuNum1PixelHitSelWithCosmics_2017Analysis_CosmicMC_11Feb2021',
+        #'condor_dir_den' : 'MuMuDen1PixelHitSelWithCosmics_2017Analysis_CosmicMC_11Feb2021',
+        #'condor_dir' : 'MuMuNum1PixelHitSelWithCosmics_2018Analysis_CosmicMC_11Feb2021',
+        #'condor_dir_den' : 'MuMuDen1PixelHitSelWithCosmics_2018Analysis_CosmicMC_11Feb2021',
         'dataset' :   'Cosmics',
         'den_channel' : 'Den1PixelHitSelWithCosmics',
         'num_channel' : 'Num1PixelHitSelWithCosmics',
