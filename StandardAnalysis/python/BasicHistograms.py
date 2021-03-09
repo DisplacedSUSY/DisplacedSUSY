8c8c7f222bca254269e9269889b7371d87aef258
Juliette Alimena <juliette.alimena@cern.ch>
2021-03-09 04:05:03 -0600

    add possibility to look at bjet histograms

--- StandardAnalysis/python/BasicHistograms.py
+++ StandardAnalysis/python/BasicHistograms.py
@@ -4,11 +4,13 @@ import FWCore.ParameterSet.Config as cms
 ##### Basic histograms to be plotted for almost every selection ################
 ################################################################################
 
-from DisplacedSUSY.Configuration.histogramDefinitions import JetHistograms, MetHistograms
+from DisplacedSUSY.Configuration.histogramDefinitions import JetHistograms, MetHistograms, BjetHistograms, JetBjetHistograms
 from DisplacedSUSY.Configuration.histogramDefinitions import BeamspotHistograms, eventHistograms, GenParticleHistograms, GenParticleD0Histograms
 
 histograms = cms.VPSet()
 histograms.append(JetHistograms)
+#histograms.append(BjetHistograms)
+#histograms.append(JetBjetHistograms)
 histograms.append(MetHistograms)
 histograms.append(BeamspotHistograms)
 histograms.append(eventHistograms)
