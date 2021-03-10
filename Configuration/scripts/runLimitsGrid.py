34626084dce806304057c1a9f93ab7d775a9ab3f
Bryan Cardwell <bryan.cardwell@cern.ch>
2021-03-10 04:43:25 -0600

    In runLimitsGrid, handle case where makeLimitPlots has renamed input file

--- Configuration/scripts/runLimitsGrid.py
+++ Configuration/scripts/runLimitsGrid.py
@@ -75,6 +75,10 @@ def first_half(l):
 def get_asymptotic_limits(in_dir, signal_point):
     file_name_template = "{0}/{1}_expected/higgsCombine{1}.AsymptoticLimits.mH120.root"
     file_name = file_name_template.format(in_dir, signal_point)
+    # if file doesn't exist, check for input file created by makeLimitPlots
+    if not os.path.isfile(file_name):
+        file_name = "{0}/{1}_expected/limits_{1}.root".format(in_dir, signal_point)
+        print file_name
     try:
         f = TFile(file_name)
         limit_tree = f.Get('limit').Clone()
