ec6f1b76d45adc57e5d28e6527a9ce302d1e8927
Juliette Alimena <juliette.alimena@cern.ch>
2021-03-29 11:50:06 -0500

    remove old gen-sim samples with cloud model but with r-hadron sim tracks as HSCPs (wrong), add new gen-sim samples with cloud model and with r-hadrons that decay in geant (right)

--- Configuration/python/genSim_102X_Samples.py
+++ Configuration/python/genSim_102X_Samples.py
@@ -8,9 +8,14 @@ bg_mc_samples = {
 }
 
 signal_mc_samples = {
-    'stopToLB200_1000mm' : "/StopToLB_M_200_1000mm_13TeV_2018MC_withCloudModel/jalimena-GenSim-c69c98045b4066c4b21b3b374c9684cc/USER",
-    'stopToLB1000_1000mm': "/StopToLB_M_1000_1000mm_13TeV_2018MC_withCloudModel/jalimena-GenSim-5fcce4e8b93b0e2895d6c1ae16c2514c/USER",
-    'stopToLB1800_1000mm': "/StopToLB_M_1800_1000mm_13TeV_2018MC_withCloudModel/jalimena-GenSim-341f225603b71ea433029af453b2e534/USER",
+
+    'stopToLB1000_0p1mm' : "/StopToLB_M_1000_0p1mm_13TeV_2018MC_withCloudModel/jalimena-GenSim-2d3633c9b37070a7a773d8d5a58080dd/USER",
+    'stopToLB1000_1mm'   : "/StopToLB_M_1000_1mm_13TeV_2018MC_withCloudModel/jalimena-GenSim-0f895349610d76a6ea7b0e656c656ff6/USER",
+    'stopToLB1000_10mm'  : "/StopToLB_M_1000_10mm_13TeV_2018MC_withCloudModel/jalimena-GenSim-2f4dd0a24c7321dbf8a610eeade31a6c/USER",
+    'stopToLB1000_100mm' : "/StopToLB_M_1000_100mm_13TeV_2018MC_withCloudModel/jalimena-GenSim-e39772ad3ad93e329739e185352a7758/USER",
+    'stopToLB1000_1000mm': "/StopToLB_M_1000_1000mm_13TeV_2018MC_withCloudModel/jalimena-GenSim-b5612ec4a84bf7b31ddb6ab6fc557441/USER",
+    'stopToLB1000_10000mm':"/StopToLB_M_1000_10000mm_13TeV_2018MC_withCloudModel/jalimena-GenSim-3902e700902794915777ca09bb7726b8/USER",
+
 }
 
 # create composite dictionary of all samples
