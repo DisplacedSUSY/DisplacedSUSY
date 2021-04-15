f790fd4aeb64fec49b340092282941da88ef44aa
Juliette Alimena <juliette.alimena@cern.ch>
2021-04-15 11:05:47 -0500

    add some gen-sim samples with cloud model on and decay in geant

--- Configuration/python/genSim_102X_Samples.py
+++ Configuration/python/genSim_102X_Samples.py
@@ -16,6 +16,13 @@ signal_mc_samples = {
     'stopToLB1000_1000mm_withCloudModel' : "/StopToLB_M_1000_1000mm_13TeV_2018MC_withCloudModel/jalimena-GenSim-b5612ec4a84bf7b31ddb6ab6fc557441/USER",
     'stopToLB1000_10000mm_withCloudModel':"/StopToLB_M_1000_10000mm_13TeV_2018MC_withCloudModel/jalimena-GenSim-3902e700902794915777ca09bb7726b8/USER",
 
+    'stopToLB1800_0p1mm_withCloudModel'  : "/StopToLB_M_1800_0p1mm_13TeV_2018MC_withCloudModel/jalimena-GenSim-9083ee302d2e6daeba440727c7cc65ef/USER",
+    'stopToLB1800_1mm_withCloudModel'    : "/StopToLB_M_1800_1mm_13TeV_2018MC_withCloudModel/jalimena-GenSim-7fbba056b835b222ddf12f0df1933d76/USER",
+    'stopToLB1800_10mm_withCloudModel'   : "/StopToLB_M_1800_10mm_13TeV_2018MC_withCloudModel/jalimena-GenSim-6ccc677c3e5368ba5ea151eb4cfc7e57/USER",
+    'stopToLB1800_100mm_withCloudModel'  : "/StopToLB_M_1800_100mm_13TeV_2018MC_withCloudModel/jalimena-GenSim-a71d5d6055606f41aeb8748c415e6277/USER",
+    'stopToLB1800_1000mm_withCloudModel' : "/StopToLB_M_1800_1000mm_13TeV_2018MC_withCloudModel/jalimena-GenSim-d3d7611a576dc12d8e9887a2378c106b/USER",
+    'stopToLB1800_10000mm_withCloudModel':"/StopToLB_M_1800_10000mm_13TeV_2018MC_withCloudModel/jalimena-GenSim-d435ef04084e4af193921df3df24a2f0/USER",
+
 }
 
 # create composite dictionary of all samples
