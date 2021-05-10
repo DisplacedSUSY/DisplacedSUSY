5d4094429275df0cb5ae987ea35367c017d0a974
Juliette Alimena <juliette.alimena@cern.ch>
2021-05-10 06:32:28 -0500

    add other Higgs mass combinations for limits

--- EMuChannel/test/HToSS_limit_plot_cfg.py
+++ EMuChannel/test/HToSS_limit_plot_cfg.py
@@ -25,6 +25,13 @@ bareMasses = {
   800  : [50, 150, 250],
   1000 : [30, 150, 350]
 }
+#20: green
+#30: black
+#50: purple
+#150: red
+#250: magenta
+#350: blue
+#yellow
 lifetimes = [str(b*10**e) for e in range(0, 4) for b in range(1, 10)] + [str(10000)] #all lifetimes
 
 # description of all the plots to be made
@@ -49,6 +56,146 @@ plotDefinitions = [
                 'graphsToInclude' : ['exp', 'obs'],
                 'colorScheme' : 'black',
                 'legendEntry' : 'm_{H}, m_{S} = 125, 30 GeV',
+            },
+        ],
+    },
+    {
+        # this will be the name of the canvas in the output root file
+        'title' : 'limits_vs_lifetime_H300',
+        # current options are 'mass' and 'lifetime'
+        'xAxisType' : 'lifetime',
+        # xmin, xmax, label
+        'xAxisLabel' : 'c#tau [cm]',
+
+        'showTheory' : True,
+        'showTheoryError' : True,
+
+        #define all the curves to include on this canvas
+        'graphs' : [
+            {
+                'source' : 'combined_HToSS_2016_unblinded_29Apr2021',
+                'mass' : '300_20',
+                'm' : '300',
+                'graphsToInclude' : ['exp', 'obs'],
+                'colorScheme' : 'green',
+                'legendEntry' : 'm_{H}, m_{S} = 300, 20 GeV',
+            },
+            {
+                'source' : 'combined_HToSS_2016_unblinded_29Apr2021',
+                'mass' : '300_50',
+                'm' : '300',
+                'graphsToInclude' : ['exp', 'obs'],
+                'colorScheme' : 'purple',
+                'legendEntry' : 'm_{H}, m_{S} = 300, 50 GeV',
+            },
+            {
+                'source' : 'combined_HToSS_2016_unblinded_29Apr2021',
+                'mass' : '300_150',
+                'm' : '300',
+                'graphsToInclude' : ['exp', 'obs'],
+                'colorScheme' : 'red',
+                'legendEntry' : 'm_{H}, m_{S} = 300, 150 GeV',
+            },
+        ],
+    },
+    {
+        # this will be the name of the canvas in the output root file
+        'title' : 'limits_vs_lifetime_H400',
+        # current options are 'mass' and 'lifetime'
+        'xAxisType' : 'lifetime',
+        # xmin, xmax, label
+        'xAxisLabel' : 'c#tau [cm]',
+
+        'showTheory' : True,
+        'showTheoryError' : True,
+
+        #define all the curves to include on this canvas
+        'graphs' : [
+            {
+                'source' : 'combined_HToSS_2016_unblinded_29Apr2021',
+                'mass' : '400_50',
+                'm' : '400',
+                'graphsToInclude' : ['exp', 'obs'],
+                'colorScheme' : 'purple',
+                'legendEntry' : 'm_{H}, m_{S} = 400, 50 GeV',
+            },
+            {
+                'source' : 'combined_HToSS_2016_unblinded_29Apr2021',
+                'mass' : '400_150',
+                'm' : '400',
+                'graphsToInclude' : ['exp', 'obs'],
+                'colorScheme' : 'red',
+                'legendEntry' : 'm_{H}, m_{S} = 400, 150 GeV',
+            },
+        ],
+    },
+    {
+        # this will be the name of the canvas in the output root file
+        'title' : 'limits_vs_lifetime_H600',
+        # current options are 'mass' and 'lifetime'
+        'xAxisType' : 'lifetime',
+        # xmin, xmax, label
+        'xAxisLabel' : 'c#tau [cm]',
+
+        'showTheory' : True,
+        'showTheoryError' : True,
+
+        #define all the curves to include on this canvas
+        'graphs' : [
+            {
+                'source' : 'combined_HToSS_2016_unblinded_29Apr2021',
+                'mass' : '600_50',
+                'm' : '600',
+                'graphsToInclude' : ['exp', 'obs'],
+                'colorScheme' : 'purple',
+                'legendEntry' : 'm_{H}, m_{S} = 600, 50 GeV',
+            },
+            {
+                'source' : 'combined_HToSS_2016_unblinded_29Apr2021',
+                'mass' : '600_150',
+                'm' : '600',
+                'graphsToInclude' : ['exp', 'obs'],
+                'colorScheme' : 'red',
+                'legendEntry' : 'm_{H}, m_{S} = 600, 150 GeV',
+            },
+        ],
+    },
+    {
+        # this will be the name of the canvas in the output root file
+        'title' : 'limits_vs_lifetime_H800',
+        # current options are 'mass' and 'lifetime'
+        'xAxisType' : 'lifetime',
+        # xmin, xmax, label
+        'xAxisLabel' : 'c#tau [cm]',
+
+        'showTheory' : True,
+        'showTheoryError' : True,
+
+        #define all the curves to include on this canvas
+        'graphs' : [
+            {
+                'source' : 'combined_HToSS_2016_unblinded_29Apr2021',
+                'mass' : '800_50',
+                'm' : '800',
+                'graphsToInclude' : ['exp', 'obs'],
+                'colorScheme' : 'purple',
+                'legendEntry' : 'm_{H}, m_{S} = 800, 50 GeV',
+            },
+            {
+                'source' : 'combined_HToSS_2016_unblinded_29Apr2021',
+                'mass' : '800_150',
+                'm' : '800',
+                'graphsToInclude' : ['exp', 'obs'],
+                'colorScheme' : 'red',
+                'legendEntry' : 'm_{H}, m_{S} = 800, 150 GeV',
+            },
+            {
+                'source' : 'combined_HToSS_2016_unblinded_29Apr2021',
+                'mass' : '800_250',
+                'm' : '800',
+                'graphsToInclude' : ['exp', 'obs'],
+                'colorScheme' : 'magenta',
+                'legendEntry' : 'm_{H}, m_{S} = 800, 250 GeV',
             },
         ],
     },
