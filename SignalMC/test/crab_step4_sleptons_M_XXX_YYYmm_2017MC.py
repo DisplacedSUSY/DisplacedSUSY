8ffade9b80822f4e459d986037a1586f8b02cca4
Juliette Alimena <juliette.alimena@cern.ch>
2021-05-22 13:06:53 -0500

    finish adding 2017 slepton MC production configs

---
+++ SignalMC/test/crab_step4_sleptons_M_XXX_YYYmm_2017MC.py
@@ -0,0 +1,26 @@
+from CRABClient.UserUtilities import config
+config = config()
+
+
+config.General.requestName = 'sleptons_M_XXX_YYYmm_13TeV_2017MC_MiniAod'
+config.General.workArea = 'crab'
+config.General.transferOutputs = True
+config.General.transferLogs = True
+
+config.JobType.pluginName = 'Analysis'
+config.JobType.psetName = 'step4_PAT_2017MC.py'
+config.JobType.maxMemoryMB = 8000
+config.JobType.numCores = 4
+config.JobType.allowUndistributedCMSSW = True
+
+config.Data.inputDataset = '/Sleptons_M_XXX_YYYmm_13TeV_2017MC/bcardwel-Reco-/USER'
+config.Data.outputDatasetTag = 'MiniAod'
+config.Data.inputDBS = 'phys03'
+config.Data.splitting = 'FileBased'
+config.Data.unitsPerJob = 1
+config.Data.publication = True
+config.Data.ignoreLocality = True
+config.Data.outLFNDirBase = '/store/group/lpclonglived/DisplacedLeptons/'
+
+config.Site.whitelist = ["T1_US_FNAL","T2_US_Nebraska","T2_US_Vanderbilt"]
+config.Site.storageSite = 'T3_US_FNALLPC'
