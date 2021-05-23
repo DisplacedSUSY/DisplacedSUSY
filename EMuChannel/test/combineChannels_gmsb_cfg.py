251416fc09f98aa025e3c17cb72f72c0b8020982
Bryan Cardwell <bryan.cardwell@cern.ch>
2021-05-23 17:32:35 -0500

    Initial commit of combineChannels config for gmsb co-nlsp scenario

---
+++ EMuChannel/test/combineChannels_gmsb_cfg.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+inputDirs =[
+    'sleptons_ee_2018_23May2021',
+    'sleptons_mumu_2018_23May2021',
+    'staus_ee_2018_23May2021',
+    'staus_emu_2018_23May2021',
+    'staus_mumu_2018_23May2021',
+]
