0432473e28e2936f9d718656863afae764a9a377
Juliette Alimena <juliette.alimena@cern.ch>
2021-06-29 02:51:45 -0500

    add staus to pileup config

--- StandardAnalysis/test/pileup/localPileupConfig.py
+++ StandardAnalysis/test/pileup/localPileupConfig.py
@@ -49,6 +49,10 @@ for mH in massesH:
 #processes = ['sleptons']
 #masses = [50] + [m for m in range(100, 901, 100)]
 #lifetimes = [10**e for e in range(-2, 5)]
-#datasets = ["{}{}_{}mm".format(p, m, l) for p in processes for m in masses for l in lifetimes]
 
-datasets = [lt.replace(".", "p") for lt in datasets]
+#processes = ['staus']
+#masses = [50] + [m for m in range(100, 501, 100)]
+#lifetimes = [10**e for e in range(-1, 4)]
+
+#datasets = ["{}{}_{}mm".format(p, m, l) for p in processes for m in masses for l in lifetimes]
+#datasets = [lt.replace(".", "p") for lt in datasets]
