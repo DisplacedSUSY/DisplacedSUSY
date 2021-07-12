cfa820776e7c9392c34ac6664f0e969637bfaaea
Bryan Cardwell <bryan.cardwell@cern.ch>
2021-07-12 11:20:10 -0500

    Update combineDatacard configs with completed sleptons datacards

--- EEChannel/test/combineYearsLimit_cfg.py
+++ EEChannel/test/combineYearsLimit_cfg.py
@@ -6,7 +6,7 @@ inputDirs = [
     #'ee_ld_2016_03Feb2021',
     #'ee_ld_2017_18_03Feb2021',
     #'sleptons_ee_2016_30Jun2021',
-    #'sleptons_ee_2017_18_from2018_30Jun2021',
+    #'sleptons_ee_2017_18_12Jul2021',
     #'staus_ee_2016_from2018Samples_25May2021',
     #'staus_ee_2017_18_from2018Samples_25May2021',
     #'ee_HToSS_2016_unblinded_29Apr2021',
--- MuMuChannel/test/combineYearsLimit_cfg.py
+++ MuMuChannel/test/combineYearsLimit_cfg.py
@@ -6,7 +6,7 @@ inputDirs = [
     #'mumu_ld_2016_03Feb2021',
     #'mumu_ld_2017_18_03Feb2021',
     #'sleptons_mumu_2016_30Jun2021',
-    #'sleptons_mumu_2017_18_from2018_30Jun2021',
+    #'sleptons_mumu_2017_18_12Jul2021',
     #'staus_mumu_2016_from2018Samples_25May2021',
     #'staus_mumu_2017_18_from2018Samples_25May2021',
     #'mumu_HToSS_2016_unblinded_29Apr2021',
